7d823a47fcbe66df2f414e038663cb63e0ae2be8
Satont <satontworldwide@gmail.com>
2022-06-06 19:48:37 +0000

    fix: add missed cooldown for keywords parser

--- apps/bots/src/libs/keywordsParser.ts
+++ apps/bots/src/libs/keywordsParser.ts
@@ -9,12 +9,17 @@ export class KeywordsParser {
     if (!keywordsKeys?.length) return;
 
     const keywords = await Promise.all(keywordsKeys.map(key => redis.hgetall(key))) as unknown as Keyword[];
-
     const responses: string[] = [];
 
     message = message.toLowerCase();
     for (const keyword of keywords.filter(k => k.enabled)) {
-      if (message.includes(keyword.text.toLowerCase())) responses.push(keyword.response);
+      const cooldownKey = `cooldowns:keywords:${keyword.id}`;
+      const isOnCooldown = await redis.get(cooldownKey);
+      if (message.includes(keyword.text.toLowerCase())) {
+        if (keyword.cooldown && isOnCooldown) continue;
+        responses.push(keyword.response);
+        redis.set(cooldownKey, 'true').then(() => redis.expire(cooldownKey, keyword.cooldown));
+      }
     }
 
     return responses;
