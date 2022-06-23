95f3b93bd4d4e315dd60a21f9e1a112fb605063b
Satont <satontworldwide@gmail.com>
2022-06-23 23:18:41 +0000

    feat(fronend): add handle error to login page
    [skip-docker]

--- apps/frontend/src/pages/Login.vue
+++ apps/frontend/src/pages/Login.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import { useTitle } from '@vueuse/core';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
-import { fetchAndSetUser } from '@/functions/fetchAndSetUser';
-
-
 const router = useRouter();
+
+const error = ref('');
 
 onMounted(async () => {
   const title = useTitle();
@@ -15,6 +14,7 @@ onMounted(async () => {
   const params = new URLSearchParams(window.location.search.substring(1));
 
   const code = params.get('code');
+  const isError = params.get('error');
   if (code) {
     const request = await fetch(
       '/api/auth/token?' +
@@ -34,5 +34,34 @@ onMounted(async () => {
 
     router.push('/dashboard');
   }
+
+  if (isError) {
+    error.value = isError;
+  }
 });
 </script>
+
+<template>
+  <div
+    v-if="error"
+    class="flex h-screen select-none"
+  >
+    <div class="m-auto text-center space-y-2 text-white">
+      <h1 class="font-bold text-6xl">
+        Oops!
+      </h1>
+      <p
+        class="text-lg"
+      >
+        That seems like you denied us to use your account data. Sorry, but we can't provide our service then.
+      </p>
+
+      <button
+        class="inline-block px-10 py-2.5 bg-[#9146FF] text-white text-lg leading-tight uppercase rounded shadow hover:bg-[#772CE8]  focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+        @click="router.push('/')"
+      >
+        Home
+      </button>
+    </div>
+  </div>
+</template>
