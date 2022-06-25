7717c7fce9e269c91880e3d318a3a6adedcdf6b5
Satont <satontworldwide@gmail.com>
2022-06-25 00:32:25 +0000

    feat(frontend): remove duplicated classes in footer
    [skip-docker]

--- apps/frontend/src/components/layout/Footer.vue
+++ apps/frontend/src/components/layout/Footer.vue
@@ -3,13 +3,19 @@
     <div class="mx-6 flex justify-end space-x-1 mt-1">
       <a
         href="/"
-        class="inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow hover:bg-[#202122] focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+        class="href"
       >@tsuwari</a>
       <a
         href="https://satont.dev"
         target="_blank"
-        class="inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow hover:bg-[#202122] focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+        class="href"
       >@satont</a>
     </div>
   </footer>
 </template>
+
+<style scoped>
+  .href {
+    @apply inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow hover:bg-[#202122] focus:outline-none focus:ring-0 transition duration-150 ease-in-out
+  }
+</style>
