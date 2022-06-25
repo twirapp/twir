44be2899b02b6de2e3b1e437a067db00143dcbac
Satont <satontworldwide@gmail.com>
2022-06-25 00:35:08 +0000

    feat(frontend): remove href class from links in footer
    [skip-docker]

--- apps/frontend/src/components/layout/Footer.vue
+++ apps/frontend/src/components/layout/Footer.vue
@@ -1,21 +1,19 @@
 <template>
   <footer class="w-full text-white my-2">
-    <div class="mx-6 flex justify-end space-x-1 mt-1">
+    <div class="contacts mx-6 flex justify-end space-x-1 mt-1">
       <a
         href="/"
-        class="href"
       >@tsuwari</a>
       <a
         href="https://satont.dev"
         target="_blank"
-        class="href"
       >@satont</a>
     </div>
   </footer>
 </template>
 
 <style scoped>
-  .href {
+  .contacts a {
     @apply inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow hover:bg-[#202122] focus:outline-none focus:ring-0 transition duration-150 ease-in-out
   }
 </style>
