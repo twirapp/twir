3a2d8fba903dce4e278b607b57741c3baecd941a
Satont <satontworldwide@gmail.com>
2022-07-06 17:57:02 +0000

    upd

--- apps/dota/src/index.ts
+++ apps/dota/src/index.ts
@@ -51,55 +51,65 @@ function converUsers(users: Record<string, { richPresence: RP }>) {
 client.on('appLaunched', async (appid) => {
   console.log('Dota launched', appid);
   const accs = [911977148, 1102609846, 70388657, 86738694].map(SteamID.fromIndividualAccountID).map(id => id.getSteamID64());
-  client.requestRichPresence(570, accs, 'english', (error, data) => {
+  const type = proto.root.lookupType('CMsgClientToGCFindTopSourceTVGames');
+  const newMsg = type.encode({
+    search_key: '',
+    league_id: 0,
+    hero_id: 0,
+    start_game: 90,
+    game_list_index: 0,
+    lobby_ids: [],
+  });
+  /* client.requestRichPresence(570, accs, 'english', (error, data) => {
     const users = converUsers(data.users);
 
     const lobbyIds = new Set(users.filter(u => u.richPresence.lobbyId).map(u => u.richPresence.lobbyId));
 
     if (!lobbyIds.size) return;
-    const type = proto.root.lookupType('CMsgClientToGCFindTopSourceTVGames');
-    const newMsg = type.encode({
-      search_key: '',
-      league_id: 0,
-      hero_id: 0,
-      start_game: 90,
-      game_list_index: 0,
-      lobby_ids: [],
-    });
 
-    console.info(type.fromObject({
-      search_key: '',
-      league_id: 0,
-      hero_id: 0,
-      start_game: 90,
-      game_list_index: 0,
-      lobby_ids: [],
-    }));
     console.info('Sending to GC', accs, [...lobbyIds.values()]);
 
     client.sendToGC(570, 8009, {}, Buffer.from(newMsg.finish()));
     setInterval(() => client.sendToGC(570, 8009, {}, Buffer.from(newMsg.finish())), 5000);
-  });
+  }); */
+  client.sendToGC(570, 8009, {}, Buffer.from(newMsg.finish()));
+  setInterval(() => client.sendToGC(570, 8009, {}, Buffer.from(newMsg.finish())), 5000);
 });
 
-client.on('receivedFromGC', (app, msg, payload) => {
-  const type = proto.root.lookupType('CMsgGCToClientFindTopSourceTVGamesResponse');
+client.on('disconnected', (e, w) => console.log(e, w));
 
-  const data = type.decode(payload).toJSON() as {
-    game_list?: Array<{
-      lobby_type: number,
-      game_mode: number,
-      average_mmr: number,
-      players: Array<{ account_id: number, hero_id: number }>,
-      weekend_tourney_bracket_round?: string,
-      weekend_tourney_skill_level?: string,
-      match_id?: string
-    }>
-  };
-  console.log('receivedFromGC', app, msg, data);
-  if (data.game_list) {
-    const match = data.game_list.find(g => g.players.find(p => p.account_id == 86738694));
-    console.log(match, { depth: null });
+client.on('receivedFromGC', async (app, msg, payload) => {
+  if (msg === 4009) {
+    const proto = await new protobufjs.Root().load(resolve(dirname(fileURLToPath(import.meta.url)), '..', 'protos', 'dota2', 'gcsdk_gcmessages.proto'), {
+      keepCase: true,
+    });
+    const type = proto.root.lookupType('CMsgConnectionStatus');
+    console.log(type.decode(payload).toJSON());
+
+    const helloType = proto.root.lookupType('CMsgClientHello');
+    client.sendToGC(570, 4006, {}, Buffer.from(helloType.encode({}).finish()));
+    return;
+  }
+
+  if (msg === 8010) {
+    const type = proto.root.lookupType('CMsgGCToClientFindTopSourceTVGamesResponse');
+
+    const data = type.decode(payload).toJSON() as {
+      game_list?: Array<{
+        lobby_type: number,
+        game_mode: number,
+        average_mmr: number,
+        players: Array<{ account_id: number, hero_id: number }>,
+        weekend_tourney_bracket_round?: string,
+        weekend_tourney_skill_level?: string,
+        match_id?: string
+      }>
+    };
+    console.log('receivedFromGC', app, msg, data);
+    if (data.game_list) {
+      const match = data.game_list.find(g => g.players.find(p => p.account_id == 86738694));
+      console.log(match, { depth: null });
+    }
   }
 });
 
