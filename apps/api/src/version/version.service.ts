286665deb4a21a944896d2613814bc34fc66ce4c
Satont <satontworldwide@gmail.com>
2022-07-09 14:03:53 +0000

    fix(api): add missed github token to version call
    [skip-docker]

--- apps/api/src/version/version.service.ts
+++ apps/api/src/version/version.service.ts
@@ -1,9 +1,14 @@
 import { HttpException, Injectable } from '@nestjs/common';
+import { config } from '@tsuwari/config';
 
 @Injectable()
 export class VersionService {
   async getCommitSha() {
-    const request = await fetch('https://api.github.com/repos/satont/tsuwari/commits');
+    const request = await fetch('https://api.github.com/repos/satont/tsuwari/commits', {
+      headers: {
+        'Authorization': `token: ${config.GITHUB_TOKEN}`,
+      },
+    });
     if (!request.ok) {
       console.log(request);
       throw new HttpException('Request not ok', 500);
