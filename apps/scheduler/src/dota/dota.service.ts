f9e80102c94f7f3b7af34b72ad81177c6bfadf8a
Satont <satontworldwide@gmail.com>
2022-07-11 14:54:32 +0000

    feat(scheduler): request info about dota accs each minute
    [skip-docker]

--- apps/scheduler/src/dota/dota.service.ts
+++ apps/scheduler/src/dota/dota.service.ts
@@ -7,15 +7,15 @@ import _ from 'lodash';
 
 @Injectable()
 export class DotaService {
-  #logger = new Logger()
+  #logger = new Logger();
 
   constructor(private readonly prisma: PrismaService, @Inject('NATS') private nats: ClientProxy) { }
 
-  @Interval(config.isDev ? 10000 : 5 * 60 * 1000)
+  @Interval(config.isDev ? 10000 : 1 * 60 * 1000)
   async cacheDota() {
     const accounts = await this.prisma.dotaAccount.findMany({});
     const chunks = _.chunk(accounts.map(a => a.id), 50);
-    this.#logger.log(`Getting information about ${accounts.length} accs.`)
+    this.#logger.log(`Getting information about ${accounts.length} accs.`);
 
     for (const chunk of chunks) {
       await this.nats.emit('dota.cacheAccountsMatches', chunk).toPromise();
