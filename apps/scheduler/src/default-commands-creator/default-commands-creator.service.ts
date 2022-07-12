a6b8b29d7470aa9467ba4daa920852b1ff98e0fa
Satont <satontworldwide@gmail.com>
2022-07-12 23:32:24 +0000

    fix(scheduler): correctly create default commands
    [skip-docker]

--- apps/scheduler/src/default-commands-creator/default-commands-creator.service.ts
+++ apps/scheduler/src/default-commands-creator/default-commands-creator.service.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable, Logger } from '@nestjs/common';
 import { Interval } from '@nestjs/schedule';
 import { config } from '@tsuwari/config';
-import { PrismaService } from '@tsuwari/prisma';
+import { PrismaService, Channel } from '@tsuwari/prisma';
 import { ClientProxy, RedisService } from '@tsuwari/shared';
 import { lastValueFrom } from 'rxjs';
 
@@ -18,34 +18,17 @@ export class DefaultCommandsCreatorService {
   @Interval(config.isDev ? 1000 : 5 * 60 * 1000)
   async createDefaultCommands() {
     const defaultCommands = await lastValueFrom(this.nats.send('bots.getDefaultCommands', {}));
-    const defaultCommandsNames = defaultCommands.map(c => c.name);
 
-    const usersForUpdate = await this.prisma.channel.findMany({
-      where: {
-        commands: {
-          none: {
-            defaultName: {
-              in: defaultCommandsNames,
-            },
-            default: true,
-          },
-        },
-      },
-      include: {
-        commands: true,
-      },
-    });
+    for (const command of defaultCommands) {
+      const usersForUpdate: Channel[] = await this.prisma.$queryRaw`SELECT * FROM "channels" where id not in (select "channelId" from "channels_commands" where "channels_commands"."defaultName" = ${command.name})`;
+      if (!usersForUpdate.length) continue;
 
-    this.#logger.log(`Creating default commands for ${usersForUpdate.length} users.`);
+      this.#logger.log(`Creating default command ${command.name} for ${usersForUpdate.length} users.`);
 
-    for (const user of usersForUpdate) {
-      const existedCommands = user.commands.filter(c => c.default).map(c => c.defaultName);
-      const commandsForCreate = defaultCommands.filter(c => !existedCommands.includes(c.name));
-
-      for (const command of commandsForCreate) {
+      for (const channel of usersForUpdate) {
         const newCommand = await this.prisma.command.create({
           data: {
-            channelId: user.id,
+            channelId: channel.id,
             default: true,
             defaultName: command.name,
             description: command.description,
@@ -63,7 +46,7 @@ export class DefaultCommandsCreatorService {
           aliases: JSON.stringify([]),
         };
 
-        await this.redis.hmset(`commands:${user.id}:${command.name}`, commandForSet);
+        await this.redis.hmset(`commands:${channel.id}:${command.name}`, commandForSet);
       }
     }
   }
