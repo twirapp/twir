40d7479ee67204c5426346ddcdee111109e61047
Satont <satontworldwide@gmail.com>
2022-07-13 18:45:58 +0000

    feat(scheduler): amazing creating of default commands via knex
    [skip-docker]

--- apps/scheduler/src/default-commands-creator/default-commands-creator.service.ts
+++ apps/scheduler/src/default-commands-creator/default-commands-creator.service.ts
@@ -1,13 +1,15 @@
-import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { Interval } from '@nestjs/schedule';
 import { config } from '@tsuwari/config';
 import { PrismaService, Channel } from '@tsuwari/prisma';
 import { ClientProxy, RedisService } from '@tsuwari/shared';
+import * as Knex from 'knex';
 import { lastValueFrom } from 'rxjs';
 
 @Injectable()
-export class DefaultCommandsCreatorService {
+export class DefaultCommandsCreatorService implements OnModuleInit {
   #logger = new Logger(DefaultCommandsCreatorService.name);
+  #knex: Knex.Knex;
 
   constructor(
     private readonly prisma: PrismaService,
@@ -15,11 +17,73 @@ export class DefaultCommandsCreatorService {
     private readonly redis: RedisService,
   ) { }
 
+  async onModuleInit() {
+    const knex = Knex.default({
+      client: 'pg',
+      connection: config.DATABASE_URL,
+    });
+
+    this.#knex = knex;
+  }
+
   @Interval('defaultCommands', config.isDev ? 1000 : 1 * 60 * 1000)
   async createDefaultCommands(usersIds?: string[]) {
     const defaultCommands = await lastValueFrom(this.nats.send('bots.getDefaultCommands', {}));
+    const defaultCommandsNames = defaultCommands.map(c => c.name);
 
-    for (const command of defaultCommands) {
+    const channels: Array<{
+      id: string,
+      commands: string[],
+    }> = await this.#knex
+      .select('channels.id', this.#knex.raw('array_remove(array_agg("channels_commands"."defaultName"),null) as commands'))
+      .from('channels')
+      .leftJoin('channels_commands', function () {
+        this
+          .on('channels_commands.channelId', '=', 'channels.id')
+          .andOnIn('channels_commands.defaultName', defaultCommandsNames);
+      })
+      .groupBy('channels.id')
+      .modify(function (b) {
+        if (usersIds) {
+          b.whereIn('channels.id', usersIds);
+        }
+      })
+      .having(this.#knex.raw(`count("defaultName") < ${defaultCommandsNames.length}`));
+
+
+    if (channels.length) {
+      this.#logger.log(`Creating default commands for ${channels.length} channels.`);
+    }
+
+    for (const channel of channels) {
+      const commandsForCreate = defaultCommands.filter(c => !channel.commands.includes(c.name));
+
+      for (const command of commandsForCreate) {
+        const newCommand = await this.prisma.command.create({
+          data: {
+            channelId: channel.id,
+            default: true,
+            defaultName: command.name,
+            description: command.description,
+            visible: command.visible,
+            name: command.name,
+            permission: command.permission,
+            cooldown: 0,
+            cooldownType: 'GLOBAL',
+          },
+        });
+
+        const commandForSet = {
+          ...newCommand,
+          responses: JSON.stringify([]),
+          aliases: JSON.stringify([]),
+        };
+
+        await this.redis.hmset(`commands:${channel.id}:${command.name}`, commandForSet);
+      }
+    }
+
+    /* for (const command of defaultCommands) {
       // const usersForUpdate: Channel[] = await this.prisma.$queryRaw`SELECT * FROM "channels" where id not in (select "channelId" from "channels_commands" where "channels_commands"."defaultName" = ${command.name})`;
       const usersForUpdate = usersIds
         ? await this.prisma.channel.findMany({
@@ -79,6 +143,6 @@ export class DefaultCommandsCreatorService {
 
         await this.redis.hmset(`commands:${channel.id}:${command.name}`, commandForSet);
       }
-    }
+    } */
   }
 }
