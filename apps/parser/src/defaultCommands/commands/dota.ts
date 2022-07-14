637fc22410d4fa9cc28e95155a6d5ad7fff320a4
Satont <satontworldwide@gmail.com>
2022-07-14 09:46:19 +0000

    fix(parser): another fix for wl
    [skip-docker]

--- apps/parser/src/defaultCommands/commands/dota.ts
+++ apps/parser/src/defaultCommands/commands/dota.ts
@@ -171,22 +171,22 @@ export const dota: DefaultCommand[] = [
     handler: async (state) => {
       if (!state.channelId) return;
 
-      const stream = await redis.get(`streams:${state.channelId}`);
+      /* const stream = await redis.get(`streams:${state.channelId}`);
 
       if (!stream) {
         return 'Stream is offline';
-      }
+      } */
 
-      const parsedStream = JSON.parse(stream) as HelixStreamData;
-
+      //const parsedStream = JSON.parse(stream) as HelixStreamData;
+      const date = new Date('2022-07-14 08:10:24');
       const accounts = await getAccounts(state.channelId);
       if (typeof accounts === 'string') return accounts;
 
       const games = await prisma.dotaMatch.findMany({
         where: {
-          startedAt: {
-            gte: new Date(new Date(parsedStream.started_at).getTime() - 10 * 60 * 1000),
-          },
+          /* startedAt: {
+            gte: new Date(date.getTime() - 10 * 60 * 1000),
+          }, */
           players: {
             hasSome: accounts.map(a => Number(a.id)),
           },
@@ -205,7 +205,7 @@ export const dota: DefaultCommand[] = [
           finished: true,
         },
       }).then(gms => gms.filter(g => !g.players_heroes.some(h => h === 0)));
-
+      console.log(date, games.length);
       const matchesRequest = await Promise.all(games.map(g => dotaApi.get('IDOTA2Match_570/GetMatchDetails/v1', { params: { match_id: g.match_id } })))
         .then(requests => requests.filter(r => r.status === 200));
 
@@ -246,7 +246,7 @@ export const dota: DefaultCommand[] = [
           const isPlayerRadiant = player.team_number === 0;
           let isWinner: boolean;
 
-          if ((isPlayerRadiant && match.result.radiant_win) || (!isPlayerRadiant && match.result.radiant_win)) {
+          if ((isPlayerRadiant && match.result.radiant_win) || (!isPlayerRadiant && !match.result.radiant_win)) {
             isWinner = true;
           } else {
             isWinner = false;
@@ -275,6 +275,7 @@ export const dota: DefaultCommand[] = [
         result.push(msg);
       }
 
+      console.log(result);
       return result.length ? result : 'W 0 — L 0';
     },
   },
