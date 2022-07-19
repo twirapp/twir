4cd887d170649b61bec13764f0ce9ccbf639fd50
Satont <satontworldwide@gmail.com>
2022-07-19 14:49:26 +0000

    feat(api): custom throttle message
    [skip-docker]

--- apps/api/src/guards/Throttle.guard.ts
+++ apps/api/src/guards/Throttle.guard.ts
@@ -4,6 +4,9 @@ import Express from 'express';
 
 @Injectable()
 export class ThrottlerGuard extends OriginalGuard {
+  headerPrefix = '';
+  errorMessage = 'Too many requests.';
+
   protected getTracker(req: Express.Request): string {
     return req.user.id;
   }
