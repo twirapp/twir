f81c458d9e59bd10a6b25c270771be71a6659433
Satont <satontworldwide@gmail.com>
2022-07-22 13:47:53 +0000

    feat(frontend): add vite port
    [skip-docker]

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     },
   },
   server: {
-    port: 3005,
+    port: Number(process.env.VITE_PORT ?? 3005),
     proxy: {
       '/api': {
         target: process.env.VITE_API_URL ?? 'http://localhost:3002',
