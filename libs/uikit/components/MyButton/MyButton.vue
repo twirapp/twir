75433f11e0bc6430b3c23e6848e78c458ab167e1
Artem Melnyk <melkam464@gmail.com>
2022-08-04 20:02:24 +0300

    feat: init uikit with storybook

---
+++ libs/uikit/components/MyButton/MyButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button type="button">
+    {{ text }}
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MyButton',
+  props: {
+    text: {
+      type: String,
+      default: 'Hello World',
+    },
+  },
+});
+</script>
