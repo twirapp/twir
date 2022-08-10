4b30d6e8efbf701a5e8fd24679060ce4cf925f71
Artem Melnyk <melkam464@gmail.com>
2022-08-10 16:00:19 +0200

    feat(uikit): icons, logos and multipath exports

---
+++ libs/uikit/src/icons/index.ts
@@ -0,0 +1,26 @@
+/// <reference types="vite-svg-loader" />
+
+export { default as ArrowLarge } from '@/icons/ArrowLargeIcon.svg?component';
+export { default as ArrowMedium } from '@/icons/ArrowMediumIcon.svg?component';
+export { default as Bell } from '@/icons/BellIcon.svg?component';
+export { default as Check } from '@/icons/CheckIcon.svg?component';
+export { default as CommandLine } from '@/icons/CommandLineIcon.svg?component';
+export { default as Danger } from '@/icons/DangerIcon.svg?component';
+export { default as Home } from '@/icons/HomeIcon.svg?component';
+export { default as Layout } from '@/icons/LayoutIcon.svg?component';
+export { default as SuccessCircle } from '@/icons/SuccessCircleIcon.svg?component';
+export { default as Users } from '@/icons/UsersIcon.svg?component';
+export { default as Warning } from '@/icons/WarningIcon.svg?component';
+
+export type IconName =
+  | 'ArrowLarge'
+  | 'ArrowMedium'
+  | 'Bell'
+  | 'Check'
+  | 'CommandLine'
+  | 'Danger'
+  | 'Home'
+  | 'Layout'
+  | 'SuccessCircle'
+  | 'Users'
+  | 'Warning';
