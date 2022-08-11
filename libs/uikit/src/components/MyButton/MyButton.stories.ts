e383dcb386e51dcaf30a879e4eceb83d535f4d97
Artem Melnyk <melkam464@gmail.com>
2022-08-11 23:36:20 +0200

    feat(uikit): fonts, styles and tailwind theme

--- libs/uikit/src/components/MyButton/MyButton.stories.ts
+++ libs/uikit/src/components/MyButton/MyButton.stories.ts
@@ -1,11 +1,43 @@
+import { Meta, Story } from '@storybook/vue3';
+
+import { Size, Type } from './props.types.js';
+
 import MyButton from '@/components/MyButton/MyButton.vue';
 
 export default {
   title: 'MyButton',
   component: MyButton,
-};
+  template: '<MyButton v-bind="args" />',
+  argTypes: {
+    text: { type: 'string', required: true },
+    type: {
+      type: {
+        name: 'enum',
+        value: ['button', 'submit', 'reset'] as Type[],
+      },
+      defaultValue: 'button' as Type,
+      control: 'select',
+    },
+    size: {
+      type: {
+        name: 'enum',
+        value: ['lg', 'md', 'sm'] as Size[],
+      },
+      control: 'select',
+      defaultValue: 'md' as Size,
+    },
+  },
+} as Meta;
 
-export const Primary = () => ({
+const Template: Story<{ text: string }> = (args) => ({
   components: { MyButton },
-  template: '<MyButton />',
+  setup() {
+    return { args };
+  },
+  template: '<MyButton v-bind="args" :type="args.type" />',
 });
+
+export const Primary = Template.bind({});
+Primary.args = {
+  text: 'Hello',
+};
