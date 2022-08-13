44de8c38578150708bf160e8648af8bea66b94a2
Artem Melnyk <melkam464@gmail.com>
2022-08-13 14:37:45 +0200

    feat(ui-components): TswIcon and TswArrowIcon

---
+++ libs/ui/components/src/components/TswArrowIcon/TswArrowIcon.vue
@@ -0,0 +1,65 @@
+<template>
+  <TswIcon
+    :name="iconName"
+    :size="size"
+    :class="classes"
+    :strokeWidth="strokeWidth"
+    :stroke="stroke"
+  />
+</template>
+
+<script lang="ts" setup>
+import type { IconName } from '@tsuwari/ui-icons/icons';
+import { computed } from 'vue';
+
+import type { ArrowDirection, ArrowSize } from '@/components/TswArrowIcon/props.types';
+import TswIcon from '@/components/TswIcon/TswIcon.vue';
+
+const props = withDefaults(
+  defineProps<{
+    direction: ArrowDirection;
+    arrowSize?: ArrowSize;
+    stroke?: string;
+    strokeWidth?: number;
+    size?: string;
+  }>(),
+  {
+    direction: 'right',
+    stroke: 'white',
+    arrowSize: 'md',
+    strokeWidth: 1.5,
+    size: undefined,
+  },
+);
+
+const iconName = computed<IconName>(() => {
+  switch (props.arrowSize) {
+    case 'lg':
+      return 'ArrowLarge';
+    case 'md':
+      return 'ArrowMedium';
+    default:
+      return 'ArrowMedium';
+  }
+});
+
+const classes = computed(() => [`arrow-${props.direction}`].join(' '));
+</script>
+
+<style lang="postcss">
+.arrow-right {
+  transform: rotate(0deg);
+}
+
+.arrow-bottom {
+  transform: rotate(90deg);
+}
+
+.arrow-left {
+  transform: rotate(180deg);
+}
+
+.arrow-top {
+  transform: rotate(-90deg);
+}
+</style>
---
+++ libs/ui/components/src/components/TswIcon/TswIcon.stories.ts
@@ -0,0 +1,56 @@
+import { Meta, Story } from '@storybook/vue3';
+import { IconName } from '@tsuwari/ui-icons/icons';
+
+import iconNamesArray from '@/components/TswIcon/icon-names-array';
+import TswIcon from '@/components/TswIcon/TswIcon.vue';
+
+export default {
+  title: 'TswIcon',
+  component: TswIcon,
+  argTypes: {
+    name: {
+      type: {
+        name: 'enum',
+        value: iconNamesArray,
+      },
+      control: 'select',
+    },
+    size: {
+      type: 'string',
+    },
+    fill: {
+      type: 'string',
+      control: 'color',
+    },
+    stroke: {
+      type: 'string',
+      control: 'color',
+    },
+    strokeWidth: {
+      type: 'number',
+    },
+  },
+} as Meta;
+
+const Template: Story<{
+  name: IconName;
+  size?: string;
+  fill?: string;
+  stroke?: string;
+  strokeWidth?: number;
+}> = (args) => ({
+  components: { TswIcon },
+  setup() {
+    return { args };
+  },
+  template: '<TswIcon v-bind="args" />',
+});
+
+export const Icon = Template.bind({});
+Icon.args = {
+  name: 'Home',
+  size: '36px',
+  stroke: 'white',
+  fill: 'none',
+  strokeWidth: 1.5,
+};
