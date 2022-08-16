ab91a8bb5193ba9d90c6d9046e1942a00721ef1b
Satont <satontworldwide@gmail.com>
2022-08-16 01:00:02 +0000

    fix(api): import TwitchApiService provider

--- apps/api/src/v1/bot/bot.module.ts
+++ apps/api/src/v1/bot/bot.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TwitchApiService } from '@tsuwari/shared';
 
 import { BotController } from './bot.controller.js';
 import { BotService } from './bot.service.js';
 
 @Module({
   controllers: [BotController],
-  providers: [BotService],
+  providers: [TwitchApiService, BotService],
 })
 export class BotModule {}
