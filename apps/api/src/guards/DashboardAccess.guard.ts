ccbb1a4e830d6db387f3bcb51e0bceadaa42926a
Satont <satontworldwide@gmail.com>
2022-08-16 00:51:23 +0000

    fix(api): remove unused decorators code

--- apps/api/src/guards/DashboardAccess.guard.ts
+++ apps/api/src/guards/DashboardAccess.guard.ts
@@ -1,24 +1,15 @@
-import { Injectable, CanActivate, ExecutionContext, HttpException } from '@nestjs/common';
+import { CanActivate, ExecutionContext, HttpException, Injectable } from '@nestjs/common';
 import { PrismaService } from '@tsuwari/prisma';
 import Express from 'express';
-import { Socket } from 'socket.io';
-
 
 @Injectable()
 export class DashboardAccessGuard implements CanActivate {
-  constructor(private readonly prisma: PrismaService) { }
+  constructor(private readonly prisma: PrismaService) {}
 
-  async canActivate(
-    context: ExecutionContext,
-  ) {
-    const type = context.getType();
-
-    if (type === 'ws') {
-      return this.#handleWs(context);
-    }
-
+  async canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest() as Express.Request;
-    if (!request.params?.channelId || !request.user?.id) throw new HttpException('DashboardId not specified.', 400);
+    if (!request.params?.channelId || !request.user?.id)
+      throw new HttpException('DashboardId not specified.', 400);
 
     if (request.user.id === request.params.channelId) {
       return true;
@@ -30,29 +21,6 @@ export class DashboardAccessGuard implements CanActivate {
         where: {
           channelId: request.params.channelId,
           userId: request.user.id,
-        },
-      }),
-    ]);
-
-    return requestUser?.isBotAdmin || !!dashBoardAccess;
-  }
-
-  async #handleWs(context: ExecutionContext) {
-    const client = context.switchToWs().getClient() as Socket;
-    const query = client.handshake.query;
-    if (!query?.channelId || !query?.userId) throw new Error('DashboardId not specified.');
-
-    if (Array.isArray(query.userId) || Array.isArray(query.channelId)) throw new Error('UserId or channelId cannot be array.');
-    if (query.channelId === query.userId) {
-      return true;
-    }
-
-    const [requestUser, dashBoardAccess] = await Promise.all([
-      this.prisma.user.findFirst({ where: { id: query.userId } }),
-      this.prisma.dashboardAccess.count({
-        where: {
-          channelId: query.channelId,
-          userId: query.userId,
         },
       }),
     ]);
