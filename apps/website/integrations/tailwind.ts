76005f6d81241c4f5f1592f14022702d56f06e97
Artem Melnyk <melkam464@gmail.com>
2022-08-18 19:08:55 +0200

    feat(website): init astro project and created custom tailwind integration

---
+++ apps/website/integrations/tailwind.ts
@@ -0,0 +1,30 @@
+import { fileURLToPath } from 'node:url';
+
+import load from '@proload/core';
+import type { AstroIntegration } from 'astro';
+import autoprefixerPlugin from 'autoprefixer';
+import tailwindPlugin, { Config as TailwindConfig } from 'tailwindcss';
+
+export default function tailwindIntegration(): AstroIntegration {
+  return {
+    name: 'tailwind',
+    hooks: {
+      'astro:config:setup': async ({ config, injectScript }) => {
+        const userConfig = await load('tailwind', {
+          mustExist: false,
+          cwd: fileURLToPath(config.root),
+        });
+
+        if (!userConfig?.value) {
+          throw new Error(`Could not find a Tailwind config. Does the file exist?`);
+        }
+
+        const tailwindConfig = userConfig.value as TailwindConfig;
+        config.style.postcss.plugins.push(tailwindPlugin(tailwindConfig));
+        config.style.postcss.plugins.push(autoprefixerPlugin);
+
+        injectScript('page-ssr', `import '../src/styles/tailwind.base.css';`);
+      },
+    },
+  };
+}
---
+++ apps/website/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./src/**/*.{vue,js,ts,jsx,tsx,astro}'],
+  theme: {
+    extends: {},
+  },
+  plugins: [],
+};
