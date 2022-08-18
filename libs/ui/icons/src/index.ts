36c465be50617b8d0fe2ad74a45f87d7348c691d
Artem Melnyk <melkam464@gmail.com>
2022-08-18 13:07:50 +0200

    feat(ui-icons): adding icons and fixing types

--- libs/ui/icons/src/index.ts
+++ libs/ui/icons/src/index.ts
@@ -2,19 +2,39 @@
 
 import { FunctionalComponent, SVGAttributes } from 'vue';
 
+import ArrowInCircle from '@/ArrowInCircleIcon.svg?component';
 import ArrowLarge from '@/ArrowLargeIcon.svg?component';
 import ArrowMedium from '@/ArrowMediumIcon.svg?component';
 import Bell from '@/BellIcon.svg?component';
 import Check from '@/CheckIcon.svg?component';
 import CommandLine from '@/CommandLineIcon.svg?component';
+import Cross from '@/CrossIcon.svg?component';
 import Danger from '@/DangerIcon.svg?component';
+import Eye from '@/EyeIcon.svg?component';
+import EyeOff from '@/EyeOffIcon.svg?component';
 import Home from '@/HomeIcon.svg?component';
+import Key from '@/KeyIcon.svg?component';
 import Layout from '@/LayoutIcon.svg?component';
+import Menu from '@/MenuIcon.svg?component';
+import Message from '@/MessageIcon.svg?component';
+import QuestionMark from '@/QuestionMarkIcon.svg?component';
+import Selector from '@/SelectorIcon.svg?component';
 import SuccessCircle from '@/SuccessCircleIcon.svg?component';
+import Sword from '@/SwordIcon.svg?component';
+import Timer from '@/TimerIcon.svg?component';
 import Users from '@/UsersIcon.svg?component';
+import Variable from '@/VariableIcon.svg?component';
 import Warning from '@/WarningIcon.svg?component';
 
 export type IconName =
+  | 'Timer'
+  | 'Selector'
+  | 'Menu'
+  | 'Cross'
+  | 'QuestionMark'
+  | 'Variable'
+  | 'Message'
+  | 'Sword'
   | 'ArrowLarge'
   | 'ArrowMedium'
   | 'Bell'
@@ -25,13 +45,27 @@ export type IconName =
   | 'Layout'
   | 'SuccessCircle'
   | 'Users'
-  | 'Warning';
+  | 'Warning'
+  | 'Eye'
+  | 'EyeOff'
+  | 'ArrowInCircle'
+  | 'Key';
 
 type Icons = {
   [K in IconName]: FunctionalComponent<SVGAttributes>;
 };
 
 const icons: Icons = {
+  Timer,
+  Message,
+  Variable,
+  QuestionMark,
+  Selector,
+  Menu,
+  Key,
+  Cross,
+  ArrowInCircle,
+  Sword,
   ArrowLarge,
   ArrowMedium,
   Bell,
@@ -43,6 +77,8 @@ const icons: Icons = {
   SuccessCircle,
   Users,
   Warning,
+  Eye,
+  EyeOff,
 };
 
 export default icons;
--- libs/ui/icons/src/logos/index.ts
+++ libs/ui/icons/src/logos/index.ts
@@ -1,5 +1,27 @@
 /// <reference types="vite-svg-loader" />
 
-export { default as TsuwariLogo } from '@/logos/TsuwariLogo.svg?component';
+import { FunctionalComponent, SVGAttributes } from 'vue';
 
-export type LogoName = 'TuswariLogo';
+import Dota2 from '@/logos/Dota2Logo.svg?component';
+import Faceit from '@/logos/FaceitLogo.svg?component';
+import LastFm from '@/logos/LastFmLogo.svg?component';
+import Spotify from '@/logos/SpotifyLogo.svg?component';
+import Tsuwari from '@/logos/TsuwariLogo.svg?component';
+import VK from '@/logos/VKLogo.svg?component';
+
+export type LogoName = 'Tsuwari' | 'VK' | 'Spotify' | 'Faceit' | 'Dota2' | 'LastFm';
+
+type Logos = {
+  [K in LogoName]: FunctionalComponent<SVGAttributes>;
+};
+
+const logos: Logos = {
+  Tsuwari,
+  Dota2,
+  Faceit,
+  LastFm,
+  Spotify,
+  VK,
+};
+
+export default logos;
