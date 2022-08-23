92ad1feeddb96564cc27ba53301fc7987ac3a3d9
Artem Melnyk <melkam464@gmail.com>
2022-08-23 10:55:28 +0200

    feat(web-landing): reviews card

--- apps/web/landing/src/components/ReviewCard.vue
+++ apps/web/landing/src/components/ReviewCard.vue
@@ -1,14 +1,26 @@
 <template>
-  <div class="inline-flex flex-col overflow-hidden rounded-[10px] bg-black-17 max-w-sm">
-    <div class="px-5 py-5 inline-flex border-b border-black-25">
+  <div class="inline-flex flex-col overflow-hidden rounded-[10px] bg-black-17">
+    <div class="px-5 py-5 inline-flex border-b border-black-25 items-center">
       <img :src="avatarUrl" class="w-11 h-11 rounded-full mr-[14px]" />
-      <div>
-        <span class="text-lg leading-[130%]">@{{ username }}</span>
-        <span></span>
+      <div class="inline-grid gap-y-[6px] justify-items-start">
+        <span
+          class="text-lg leading-[130%] whitespace-nowrap overflow-hidden text-ellipsis max-w-full"
+        >
+          @{{ username }}
+        </span>
+        <span class="inline-grid grid-flow-col gap-x-[1px]">
+          <TswIcon
+            v-for="i in 5"
+            :key="i"
+            name="Star"
+            size="18px"
+            :class="i > rating ? 'fill-gray-50' : 'fill-purple-70'"
+          />
+        </span>
       </div>
     </div>
     <div class="px-5 pt-3 pb-5">
-      <p class="leading-normal text-gray-70">
+      <p class="comment">
         {{ comment }}
       </p>
     </div>
@@ -16,5 +28,19 @@
 </template>
 
 <script lang="ts" setup>
-defineProps<{username: string, rating: number, comment: string, avatarUrl: string}>();
+import { TswIcon } from '@tsuwari/ui-components';
+
+defineProps<{ username: string; rating: number; comment: string; avatarUrl: string }>();
 </script>
+
+<style lang="postcss" scoped>
+.comment {
+  @apply leading-normal text-gray-70;
+
+  max-height: 72px;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+</style>
