e7284d40abb20f8021a2d709620f23d42aeee5c6
Melnyk Artem <melkam464@gmail.com>
2022-08-29 20:03:39 +0200

    feat(web): landing ssg and dynamic language change

--- apps/web/src/hooks/usePageContext.ts
+++ apps/web/src/hooks/usePageContext.ts
@@ -4,8 +4,8 @@ import type { PageContext } from '../types/pageContext.js';
 
 const PAGE_CONTEXT_INJECTION_KEY = Symbol();
 
-export function usePageContext() {
-  return inject(PAGE_CONTEXT_INJECTION_KEY);
+export function usePageContext(): PageContext {
+  return inject(PAGE_CONTEXT_INJECTION_KEY) as PageContext;
 }
 
 export function setPageContext(app: App, pageContext: PageContext) {
---
+++ apps/web/src/pages/index/Layout.vue
@@ -0,0 +1,13 @@
+<template>
+  <Header :menuItems="navMenuItems" />
+  <main>
+    <slot />
+  </main>
+  <Footer :menuItems="navMenuItems" :socials="socials" />
+</template>
+
+<script lang="ts" setup>
+import Footer from '@/components/sections/Footer.vue';
+import Header from '@/components/sections/Header.vue';
+import { socials, navMenuItems } from '@/data';
+</script>
