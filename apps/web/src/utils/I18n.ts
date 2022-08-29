8cf678ec7df7888ddd5b6b4c92f3a2bfc52a54a1
Melnyk Artem <melkam464@gmail.com>
2022-08-29 23:29:57 +0200

    fix(web): i18n setup

--- apps/web/src/utils/I18n.ts
+++ apps/web/src/utils/I18n.ts
@@ -2,9 +2,14 @@ import type { WritableComputedRef } from 'vue';
 import { createI18n } from 'vue-i18n';
 
 import type { Locale } from '@/types/locale';
+import { loadLocaleMessages, LocaleType } from '@/utils/locales.js';
 
-export function setupI18n(locale: Locale = 'en') {
+export async function setupI18n(locale: Locale = 'en', localeType: LocaleType) {
   const i18n = createI18n({ locale, legacy: false });
+
   (i18n.global.locale as WritableComputedRef<Locale>).value = locale;
+  const messages = await loadLocaleMessages(localeType, locale);
+  i18n.global.setLocaleMessage<any>(locale, messages);
+
   return i18n;
 }
