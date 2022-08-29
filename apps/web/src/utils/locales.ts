7dcbf4e42b885098378143ae99ed677892a6bb7d
Melnyk Artem <melkam464@gmail.com>
2022-08-29 23:21:08 +0200

    feat(web): native vite-plugin-ssr navigation and fix in language loading

--- apps/web/src/utils/locales.ts
+++ apps/web/src/utils/locales.ts
@@ -2,3 +2,15 @@ import type { Locale } from '@/types/locale';
 
 export const locales: Locale[] = ['en', 'ru'];
 export const defaultLocale = 'en';
+
+interface LocaleTypes {
+  landing: ReturnType<() => typeof import('@/locales/landing/en.json')>;
+  app: ReturnType<() => typeof import('@/locales/app/en.json')>;
+}
+
+export async function loadLocaleMessages<L extends keyof LocaleTypes>(
+  localeType: L,
+  locale: Locale,
+): Promise<LocaleTypes[L]> {
+  return (await import(`../locales/${localeType}/${locale}.json`)).default;
+}
