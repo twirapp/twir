59888cffe167a6516149dc52bdee46faf96ece05
Melnyk Artem <melkam464@gmail.com>
2022-09-07 20:18:27 +0200

    feat(web): header top bar, burger button, animations for first screen

---
+++ apps/web/src/components/landing/layout/MobileMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="mobile-menu hidden"
+    :style="{
+      top: `${headerHeight}px`,
+      height: `${windowHeight - headerHeight}px`,
+    }"
+  ></div>
+</template>
+
+<script lang="ts" setup>
+import { useStore } from '@nanostores/vue';
+import { useWindowSize } from '@vueuse/core';
+
+import { headerHeightStore } from '@/stores/landing/header.js';
+
+const headerHeight = useStore(headerHeightStore);
+const { height: windowHeight } = useWindowSize();
+</script>
