b4816867c8d3615dda30694eb2f2b551821cf348
Artem Melnyk <melkam464@gmail.com>
2022-09-10 16:56:54 +0200

    build(web): vue-i18n build optimizations

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -8,6 +8,11 @@ import ssr from 'vite-plugin-ssr/plugin';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  define: {
+    __VUE_I18N_FULL_INSTALL__: false,
+    __VUE_I18N_LEGACY_API__: false,
+    __INTLIFY_PROD_DEVTOOLS__: false,
+  },
   plugins: [
     vue(),
     ssr({
@@ -17,7 +22,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
-      'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.prod.js',
     },
   },
   server: {
