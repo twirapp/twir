5ba68c88469147d3532950f7ee267c8c317fe933
Satont <satontworldwide@gmail.com>
2022-09-12 17:45:52 +0000

    del node parser and add dockerfile for go parser

--- apps/parser/src/defaultCommands/index.ts
+++
@@ -1,5 +0,0 @@
-export { permit } from './commands/permit.js';
-export { channelInfo } from './commands/channelInfo.js';
-export { dota } from './commands/dota.js';
-export { spam } from './commands/spam.js';
-export { nuke } from './commands/nuke.js';
