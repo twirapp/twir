8a99dfce2e671eb7154c49f8019a525c28ed3c93
Melnyk Artem <melkam464@gmail.com>
2022-09-14 13:48:48 +0200

    feat(web): responsive hero section and footer

--- apps/web/src/components/landing/layout/Footer.vue
+++ apps/web/src/components/landing/layout/Footer.vue
@@ -1,25 +1,52 @@
 <template>
-  <footer class="bg-black-15 px-6">
+  <footer class="bg-black-15 max-md:bg-black-10 min-md:px-6">
     <div class="container max-w-[1020px]">
-      <div class="py-8 w-full flex justify-between items-center border-b border-black-25">
-        <div class="flex-1 flex">
-          <div class="inline-flex items-center mr-auto">
+      <div
+        class="
+          min-md:py-8
+          w-full
+          flex
+          max-md:flex-col
+          justify-between
+          items-center
+          min-md:border-b min-md:border-black-25
+        "
+      >
+        <div
+          class="
+            flex-1 flex
+            max-md:w-full
+            max-md:py-[18px]
+            max-md:justify-center
+            max-md:border-b
+            max-md:border-b-black-25
+          "
+        >
+          <div class="inline-flex items-center min-md:mr-auto">
             <img src="@/assets/NewLogo.svg" class="w-[28px] h-[28px]" />
             <span class="text-[22px] ml-3 font-medium">Tsuwari</span>
           </div>
         </div>
-        <NavMenu menuClass="inline-grid grid-flow-col gap-x-6" menuItemClass="leading-[130%]" />
-        <div class="flex-1 flex">
-          <ul class="inline-grid grid-flow-col gap-x-3 ml-auto">
+        <NavMenu
+          menuClass="inline-grid grid-flow-col gap-x-6 max-md:py-6 max-sm:grid-flow-row max-sm:justify-items-center max-sm:text-sm max-sm:gap-y-[18px]"
+          menuItemClass="leading-[130%]"
+        />
+        <div
+          class="
+            flex-1 flex
+            max-md:bg-black-15 max-md:w-full max-md:justify-center max-md:py-[10px]
+          "
+        >
+          <ul class="inline-grid grid-flow-col gap-x-3 min-md:ml-auto">
             <li v-for="item in socials" :key="item.id">
               <a :href="item.href">
-                <TswIcon :name="item.type" size="24px" class="stroke-gray-70" :strokeWidth="1.4" />
+                <TswIcon :name="item.type" size="24px" class="stroke-gray-70" :strokeWidth="1.5" />
               </a>
             </li>
           </ul>
         </div>
       </div>
-      <div class="text-sm text-gray-60 w-full text-center py-[18px]">
+      <div class="text-xs text-gray-60 w-full text-center py-4">
         {{ t('sections.footer.rights', { year: new Date().getFullYear() }) }}
       </div>
     </div>
