59eb2a9647277b074d9cebc61d817644bc65db9f
Satont <satontworldwide@gmail.com>
2022-09-16 20:31:16 +0300

    keywords done

--- apps/api/src/v1/keywords/keywords.service.ts
+++ apps/api/src/v1/keywords/keywords.service.ts
@@ -1,68 +1,55 @@
 import { HttpException, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaService } from '@tsuwari/prisma';
 import { Keyword, keywordsSchema, RedisORMService, Repository } from '@tsuwari/redis';
 import { RedisService } from '@tsuwari/shared';
+import { ChannelKeyword } from '@tsuwari/typeorm/entities/ChannelKeyword';
 
+import { typeorm } from '../../index.js';
 import { CreateKeywordDto } from './dto/create.js';
 
 @Injectable()
 export class KeywordsService implements OnModuleInit {
   #repository: Repository<Keyword>;
 
-  constructor(
-    private readonly prisma: PrismaService,
-    private readonly redis: RedisService,
-    private readonly redisOrm: RedisORMService,
-  ) { }
+  constructor(private readonly redis: RedisService, private readonly redisOrm: RedisORMService) {}
 
   onModuleInit() {
     this.#repository = this.redisOrm.fetchRepository(keywordsSchema);
   }
 
   async getList(channelId: string) {
-    return this.prisma.keyword.findMany({
-      where: {
-        channelId,
-      },
+    return typeorm.getRepository(ChannelKeyword).find({
+      where: { channelId },
     });
   }
 
   async delete(channelId: string, keywordId: string) {
-    const keyword = await this.prisma.keyword.findFirst({
-      where: {
-        channelId,
-        id: keywordId,
-      },
+    const repository = typeorm.getRepository(ChannelKeyword);
+    const keyword = await repository.findOneBy({
+      channelId,
+      id: keywordId,
     });
 
     if (!keyword) throw new HttpException(`Keyword with id ${keywordId} not found`, 404);
 
-    await this.prisma.keyword.delete({
-      where: {
-        id: keyword.id,
-      },
+    await repository.delete({
+      id: keyword.id,
     });
 
     await this.#repository.remove(`${channelId}:${keyword.id}`);
   }
 
   async create(channelId: string, data: CreateKeywordDto) {
-    const isExists = await this.prisma.keyword.findUnique({
-      where: {
-        channelId_text: {
-          channelId,
-          text: data.text,
-        },
-      },
+    const repository = typeorm.getRepository(ChannelKeyword);
+    const isExists = await repository.findOneBy({
+      channelId,
+      text: data.text,
     });
 
     if (isExists) throw new HttpException(`Keyword with text ${data.text} already exists`, 400);
 
-    const keyword = await this.prisma.keyword.create({
-      data: {
-        channelId,
-        ...data,
-      },
+    const keyword = await repository.save({
+      channelId,
+      ...data,
     });
 
     await this.#repository.createAndSave(keyword, `${channelId}:${keyword.id}`);
@@ -70,22 +57,17 @@ export class KeywordsService implements OnModuleInit {
   }
 
   async patch(channelId: string, keywordId: string, data: CreateKeywordDto) {
-    const isExists = await this.prisma.keyword.findUnique({
-      where: {
-        id: keywordId,
-      },
+    const repository = typeorm.getRepository(ChannelKeyword);
+    const isExists = await repository.findOneBy({
+      id: keywordId,
     });
 
     if (!isExists) throw new HttpException(`Keyword with id ${keywordId} not exists`, 404);
 
-    const keyword = await this.prisma.keyword.update({
-      where: {
-        id: keywordId,
-      },
-      data,
-    });
+    await repository.update({ id: keywordId }, data);
+    const newKeyword = await repository.findOneBy({ id: keywordId });
 
-    await this.#repository.createAndSave(keyword, `${channelId}:${keyword.id}`);
-    return keyword;
+    await this.#repository.createAndSave(newKeyword!, `${channelId}:${newKeyword!.id}`);
+    return newKeyword;
   }
 }
