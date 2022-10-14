87a50dc1b620b68160680caebe87400c0983b558
Satont <satontworldwide@gmail.com>
2022-10-14 16:50:23 +0300

    upd

--- devbox/pre-exit.sh
+++ devbox/pre-exit.sh
@@ -1,6 +1,8 @@
 finish()
 {
-  pkill -SIGINT -F /tmp/tsuwari-redis.pid
+  #pkill -SIGINT -F /tmp/tsuwari-redis.pid
+  docker stop tsuwari-redis-stack
+  docker rm tsuwari-redis-stack
   nats-server -sl quit=/tmp/tsuwari-nats.pid
   pg_ctl -D devbox/data/postgres stop
 }
