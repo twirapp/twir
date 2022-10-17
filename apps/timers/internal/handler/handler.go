997e5f02058fcb125c06807a7a4dc754ee22b4f3
Satont <satontworldwide@gmail.com>
2022-10-17 04:20:33 +0300

    upd

--- apps/timers/internal/handler/handler.go
+++ apps/timers/internal/handler/handler.go
@@ -18,24 +18,6 @@ import (
 	"gorm.io/gorm"
 )
 
-type Stream struct {
-	ID           string    `json:"id"`
-	UserID       string    `json:"user_id"`
-	UserLogin    string    `json:"user_login"`
-	UserName     string    `json:"user_name"`
-	GameID       string    `json:"game_id"`
-	GameName     string    `json:"game_name"`
-	TagIDs       []string  `json:"tag_ids"`
-	IsMature     bool      `json:"is_mature"`
-	Type         string    `json:"type"`
-	Title        string    `json:"title"`
-	ViewerCount  int       `json:"viewer_count"`
-	StartedAt    time.Time `json:"started_at"`
-	Language     string    `json:"language"`
-	ThumbnailURL string    `json:"thumbnail_url"`
-	Messages     int       `json:"parsedMessages"`
-}
-
 type Handler struct {
 	redis  *redis.Client
 	twitch *twitch.Twitch
