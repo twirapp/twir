97099a816f002cdfeb52b2696548afcb18ccebbd
Satont <satontworldwide@gmail.com>
2022-10-20 05:00:16 +0300

    chore(devbox): remove not needed exports

--- devbox/post-install.sh
+++ devbox/post-install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-export LC_ALL="en_US.UTF-8"
-export LANG="en_US.UTF-8"
-
 go install github.com/mitranim/gow@latest
 go install mvdan.cc/gofumpt@latest
 go install github.com/segmentio/golines@latest
