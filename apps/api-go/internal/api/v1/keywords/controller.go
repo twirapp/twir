b8c1c354bf628685f9f5ee4aa54103bff29e835a
Satont <satontworldwide@gmail.com>
2022-10-21 18:29:34 +0300

    upd

--- apps/api-go/internal/api/v1/keywords/controller.go
+++ apps/api-go/internal/api/v1/keywords/controller.go
@@ -8,5 +8,30 @@ import (
 func Setup(router fiber.Router, services types.Services) fiber.Router {
 	middleware := router.Group("keywords")
 
+	middleware.Get("", get(services))
+	middleware.Post("", post(services))
+	middleware.Delete(":keywordId", delete(services))
+	middleware.Put(":keywordId", delete(services))
+
 	return middleware
+}
+
+func get(services types.Services) func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+	}
+}
+
+func post(services types.Services) func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+	}
+}
+
+func delete(services types.Services) func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+	}
+}
+
+func put(services types.Services) func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+	}
 }
