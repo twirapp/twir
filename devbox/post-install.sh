aa68cb4f423d240d3945129c4a40e1bbc0d22ecb
Satont <satontworldwide@gmail.com>
2022-10-22 03:51:58 +0300

    upd

--- devbox/post-install.sh
+++ devbox/post-install.sh
@@ -2,5 +2,6 @@
 go install github.com/mitranim/gow@latest
 go install mvdan.cc/gofumpt@latest
 go install github.com/segmentio/golines@latest
+pnpm add -g nodemon
 
 echo "Adminer available at http://localhost:8080/?pgsql=postgres:5432&username=tsuwari"
