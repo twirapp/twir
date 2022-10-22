ccc08db6285d34e7bcf705b58dd5a1d645bbab94
Satont <satontworldwide@gmail.com>
2022-10-22 04:24:48 +0300

    add streams

---
+++ apps/api-go/internal/api/v1/streams/controller.go
@@ -0,0 +1,22 @@
+package streams
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/satont/tsuwari/apps/api-go/internal/types"
+)
+
+func Setup(router fiber.Router, services types.Services) fiber.Router {
+	middleware := router.Group("streams")
+
+	return middleware
+}
+
+func get(services types.Services) func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		stream, err := handleGet(c.Params("channelId"), services)
+		if err != nil {
+			return err
+		}
+		return c.JSON(stream)
+	}
+}
--- libs/gomodels/channels_streams.go
+++ libs/gomodels/channels_streams.go
@@ -8,20 +8,20 @@ import (
 
 type ChannelsStreams struct {
 	ID             string          `gorm:"primary_key;column:id;type:TEXT;"            json:"id"`
-	UserId         string          `gorm:"column:userId;type:TEXT;"                    json:"userId"`
-	UserLogin      string          `gorm:"column:userLogin;type:TEXT;"                 json:"userLogin"`
-	UserName       string          `gorm:"column:userName;type:TEXT;"                  json:"userName"`
-	GameId         string          `gorm:"column:gameId;type:TEXT;default:TEXT;"       json:"gameId"`
-	GameName       string          `gorm:"column:gameName;type:TEXT;"                  json:"gameName"`
-	CommunityIds   pq.StringArray  `gorm:"column:communityIds;type:text[];default:[];" json:"communityIds"`
+	UserId         string          `gorm:"column:userId;type:TEXT;"                    json:"user_id"`
+	UserLogin      string          `gorm:"column:userLogin;type:TEXT;"                 json:"user_login"`
+	UserName       string          `gorm:"column:userName;type:TEXT;"                  json:"user_name"`
+	GameId         string          `gorm:"column:gameId;type:TEXT;default:TEXT;"       json:"game_id"`
+	GameName       string          `gorm:"column:gameName;type:TEXT;"                  json:"game_name"`
+	CommunityIds   pq.StringArray  `gorm:"column:communityIds;type:text[];default:[];" json:"community_ids"`
 	Type           string          `gorm:"column:type;type:TEXT;"                      json:"type"`
 	Title          string          `gorm:"column:title;type:TEXT;"                     json:"title"`
-	ViewerCount    int             `gorm:"column:viewerCount;type:INT;"                json:"viewerCount"`
-	StartedAt      time.Time       `gorm:"column:startedAt;type:TIMESTAMP;"            json:"startedAt"`
+	ViewerCount    int             `gorm:"column:viewerCount;type:INT;"                json:"viewer_count"`
+	StartedAt      time.Time       `gorm:"column:startedAt;type:TIMESTAMP;"            json:"started_at"`
 	Language       string          `gorm:"column:language;type:TEXT;"                  json:"language"`
-	ThumbnailUrl   string          `gorm:"column:thumbnailUrl;type:TEXT;"              json:"thumbnailUrl"`
-	TagIds         *pq.StringArray `gorm:"column:tagIds;type:text[];default:[];"       json:"tagIds"`
-	IsMature       bool            `gorm:"column:isMature;type:BOOL;"                  json:"isMature"`
+	ThumbnailUrl   string          `gorm:"column:thumbnailUrl;type:TEXT;"              json:"thumbnail_url"`
+	TagIds         *pq.StringArray `gorm:"column:tagIds;type:text[];default:[];"       json:"tag_ids"`
+	IsMature       bool            `gorm:"column:isMature;type:BOOL;"                  json:"is_mature"`
 	ParsedMessages int             `gorm:"column:parsedMessages;type:INT;"             json:"parsedMessages"`
 	Channel        *Channels       `gorm:"foreignKey:UserId"                           json:"channel"`
 }
