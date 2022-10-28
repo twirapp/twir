1859ea37e41bd17485efeb104cdcd7b98d4a3b6e
Satont <satontworldwide@gmail.com>
2022-10-29 02:55:00 +0300

    upd

--- devbox/post-install.sh
+++ devbox/post-install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
+
 go install github.com/mitranim/gow@latest
 go install mvdan.cc/gofumpt@latest
 go install github.com/segmentio/golines@latest
-pnpm add -g nodemon
+go install github.com/twitchdev/twitch-cli@latest
 
 echo "Adminer available at http://localhost:8080/?pgsql=postgres:5432&username=tsuwari"
