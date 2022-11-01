40e14d9e1f386ca1bd109d8b6b4023bd78b9c3db
Satont <satontworldwide@gmail.com>
2022-11-01 17:07:26 +0300

    feat(devbox): install packages only if needed

--- devbox/post-install.sh
+++ devbox/post-install.sh
@@ -2,9 +2,11 @@
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 
-go install github.com/mitranim/gow@latest
-go install mvdan.cc/gofumpt@latest
-go install github.com/segmentio/golines@latest
-go install github.com/twitchdev/twitch-cli@latest
+which gow || go install github.com/mitranim/gow@latest
+which gofumpt || go install mvdan.cc/gofumpt@latest
+which golines || go install github.com/segmentio/golines@latest
+which twitch-cli || go install github.com/twitchdev/twitch-cli@latest
+
+which nodemon || pnpm add -g nodemon
 
 echo "Adminer available at http://localhost:8080/?pgsql=postgres:5432&username=tsuwari"
