7ef4c53c413407383982953cc2c85804168d7c93
Satont <satontworldwide@gmail.com>
2022-11-06 18:34:58 +0300

    chore: add gotools to bin

--- devbox/post-install.sh
+++ devbox/post-install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-export GOPATH=/home/satont/Projects/tsuwari/devbox/go
-export PATH=$PATH:$GOPATH/bin
+export GOBIN=$PWD/devbox/go
+export GOTOOLDIR=$PWD/devbox/go
+export GOPATH=$PWD/.devbox/nix/profile
+export PATH=$PATH:$GOPATH/bin:$GOBIN:GOTOOLDIR
 
 which gow || go install github.com/mitranim/gow@latest
 which gofumpt || go install mvdan.cc/gofumpt@latest
