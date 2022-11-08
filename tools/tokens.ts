5d6609a92966a56b2a104e5ec398fcda5583824a
Satont <satontworldwide@gmail.com>
2022-11-08 13:20:48 +0000

    chore: update tokens gen tool
    [skip-docker]

--- tools/tokens.ts
+++ tools/tokens.ts
@@ -1,9 +1,115 @@
-import { exec } from 'node:child_process';
+import { ChildProcess, exec } from 'node:child_process';
 import { readFile, writeFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
-import { promisify } from 'node:util';
 
-const promisedExec = promisify(exec);
+function updateTwitchCliConfig(proc: ChildProcess) {
+  return new Promise<boolean>((resolve) => {
+    if (proc.stdin === null || proc.stdout === null) {
+      return resolve(false);
+    }
+
+    proc.stdin.write(
+      `doppler run --command='twitch-cli configure -i $TWITCH_CLIENTID -s $TWITCH_CLIENTSECRET'\n`,
+    );
+
+    proc.stdout.on('error', (error) => {
+      console.error(error);
+      resolve(false);
+    });
+    proc.stdout.on('data', (message) => {
+      if (typeof message === 'string' && message.trim() === 'Updated configuration.') {
+        return resolve(true);
+      }
+      resolve(false);
+    });
+  });
+}
+
+type TwitchTokens = { BOT_ACCESS_TOKEN: string; BOT_REFRESH_TOKEN: string };
+
+function getTwitchTokens(proc: ChildProcess) {
+  return new Promise<TwitchTokens>((resolve, reject) => {
+    if (!proc.stdin || !proc.stdout || !proc.stderr) {
+      return reject('Cannot get stdin or stdout from process');
+    }
+
+    const tokenRegex = /Token:\s([0-9a-z]+)/gi;
+
+    proc.stdin.write(
+      `doppler run --command='twitch-cli token --user-token --scopes="${scopes}"'\n`,
+    );
+
+    proc.stdout.on('data', (data) => {
+      console.log(data);
+    });
+    proc.stderr.on('data', (data) => {
+      if (typeof data !== 'string') return;
+
+      const result = data.match(tokenRegex);
+      if (result === null) {
+        return console.log(data);
+      }
+      if (result.length !== 2) {
+        return reject('Cannot find two tokens');
+      }
+
+      return resolve({
+        BOT_ACCESS_TOKEN: result[0].split(':')[1].trim(),
+        BOT_REFRESH_TOKEN: result[1].split(':')[1].trim(),
+      });
+    });
+  });
+}
+
+async function writeTokensToEnv(tokens: TwitchTokens) {
+  const envPath = resolve(process.cwd(), '.env');
+  const data = [
+    `BOT_ACCESS_TOKEN=${tokens.BOT_ACCESS_TOKEN}`,
+    `BOT_REFRESH_TOKEN=${tokens.BOT_REFRESH_TOKEN}`,
+  ].join('\n');
+
+  try {
+    let content = await readFile(envPath, 'utf-8');
+    content = content.trim();
+    if (content === '') {
+      return await writeToFile(data, true);
+    }
+
+    const tokensRegex = /BOT_.+_TOKEN *= *([0-9a-z]+)/gi;
+    const tokensRegexResult = content.match(tokensRegex);
+    console.log(tokensRegexResult);
+    console.log(content);
+
+    if (tokensRegexResult === null) {
+      content += '\n' + data;
+      return await writeToFile(content, true);
+    }
+
+    tokensRegexResult.forEach((res) => {
+      content = content.replace(res, '');
+    });
+    content += '\n' + data;
+    return await writeToFile(content.trim(), true);
+  } catch (e) {
+    await writeToFile(data, false);
+  }
+
+  async function writeToFile(content: string, exists = false) {
+    return await writeFile(envPath, content, { flag: !exists ? 'wx' : undefined });
+  }
+}
+
+(async () => {
+  const childProcess = exec('bash');
+
+  const isConfigured = await updateTwitchCliConfig(childProcess);
+  if (!isConfigured) throw new Error('Cannot configure twitch cli');
+  const tokens = await getTwitchTokens(childProcess);
+  await writeTokensToEnv(tokens);
+
+  childProcess.kill(0);
+  process.exit(0);
+})();
 
 const scopes = [
   'analytics:read:extensions',
@@ -58,40 +164,3 @@ const scopes = [
   'whispers:read',
   'whispers:edit',
 ].join(' ');
-
-await promisedExec(
-  `doppler run --command='twitch-cli configure -i $TWITCH_CLIENTID -s $TWITCH_CLIENTSECRET'`,
-);
-const result = await promisedExec(
-  `doppler run --command='twitch-cli token --user-token --scopes "${scopes}" --client-id $TWITCH_CLIENTID'`,
-);
-
-const regex = /([\sa-z]+)token:\s([0-9a-z]+)/gi;
-const firstregexResult = regex.exec(result.stderr);
-if (firstregexResult == null) {
-  console.error(result.stderr);
-  process.exit(1);
-}
-const secondRegexResult = regex.exec(result.stderr);
-const accessToken = firstregexResult[2];
-const refreshToken = secondRegexResult![2];
-
-const envPath = resolve(process.cwd(), '.env');
-
-const data = [`BOT_ACCESS_TOKEN=${accessToken}`, `BOT_REFRESH_TOKEN=${refreshToken}`].join('\n');
-
-const tokensRegex = new RegExp('.+TOKEN=([0-9a-z]+)', 'ig');
-try {
-  let content = await readFile(envPath, 'utf-8');
-  const tokensRegexResult = tokensRegex.exec(content);
-  if (tokensRegexResult != null) {
-    content = content.replaceAll(tokensRegex, '');
-  }
-  await writeToFile(`${content.replaceAll(/^\s*\n/gm, '')}\n${data}`, true);
-} catch {
-  await writeToFile(data);
-}
-console.info('✅ Done');
-function writeToFile(content: string, exists = false) {
-  return writeFile(envPath, content, { flag: !exists ? 'wx' : undefined });
-}
