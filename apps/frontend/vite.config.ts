97f667adc13a8a68d4ae2c7bc92f37daed534d37
Satont <42675886+Satont@users.noreply.github.com>
2022-11-17 16:33:08 +0300

    Update vite.config.ts

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 import svgLoader from 'vite-svg-loader';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   clearScreen: false,
   plugins: [
