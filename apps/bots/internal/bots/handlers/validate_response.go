eaebecb83c3b60e75b8e009527b62e1ba67322b6
Satont <satontworldwide@gmail.com>
2022-11-22 04:19:47 +0300

    fix(parser): add redutant check on prefix

--- apps/bots/internal/bots/handlers/validate_response.go
+++ apps/bots/internal/bots/handlers/validate_response.go
@@ -8,7 +8,9 @@ import (
 func ValidateResponseSlashes(response string) error {
 	if !strings.HasPrefix(response, "/") || strings.HasPrefix(response, "/me") || strings.HasPrefix(response, "/announce") {
 		return nil
+	} else if strings.HasPrefix(response, "/") {
+		return errors.New("Slash commands except /me and /announce is disallowed. This response wont be ever sended.")
 	} else {
-		return errors.New("slash commands except /me and /announce is disallowed. This response wont be ever sended")
+		return nil
 	}
 }
