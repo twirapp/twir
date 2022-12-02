058661693e729c8c18499778f611f0ba2b527186
Satont <satontworldwide@gmail.com>
2022-12-02 18:56:17 +0300

    fix(devbox): correct postinstall
    [skipd-docker]

--- devbox/post-install.sh
+++ devbox/post-install.sh
@@ -9,7 +9,7 @@ which gofumpt || go install mvdan.cc/gofumpt@latest
 which golines || go install github.com/segmentio/golines@latest
 which twitch-cli || go install github.com/twitchdev/twitch-cli@latest
 which protoc-gen-go || go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-which protoc-gen-go-grpc go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+which protoc-gen-go-grpc || go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
 which nodemon || pnpm add -g nodemon
 
