734d7ac55f3a366d87bd554b4513c381bd5d5d1f
Satont <satontworldwide@gmail.com>
2022-12-07 21:35:55 +0000

    upd Chatting

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -7,31 +7,27 @@ import Sidebar from './components/sidebar/menu.vue';
 import Profile from './components/sidebar/profile.vue';
 
 const drawer = ref(false);
-const display = useDisplay();
 
-const isMobile = computed(() => {
-  return display.xs.value || display.sm.value;
-});
+const { mobile } = useDisplay();
 
 onMounted(() => {
-  if (!isMobile.value) drawer.value = true;
+  if (!mobile.value) drawer.value = true;
 });
 
-watch(isMobile, (_, v) => {
-  console.log(isMobile);
+watch(mobile, (_, v) => {
   drawer.value = !v;
 });
 </script>
 
 <template>
   <v-layout>
-    <v-navigation-drawer v-model="drawer" color="grey-darken-5" :expand-on-hover="!isMobile" :rail="!isMobile"> 
+    <v-navigation-drawer v-model="drawer" color="grey-darken-5" :expand-on-hover="!mobile" :rail="!mobile"> 
       <Profile />
       <v-divider></v-divider>
       <Sidebar />
     </v-navigation-drawer>
     <v-app-bar color="grey-darken-5">
-      <v-app-bar-nav-icon v-if="isMobile" variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon v-if="mobile" variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
     </v-app-bar>
     <v-main>
       <div style="padding: 8px;">
