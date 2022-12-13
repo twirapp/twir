10c89db86e2e09bbdf430c854cad00690ebcee82
Satont <satontworldwide@gmail.com>
2022-12-13 00:01:36 +0000

    upd Chatting

---
+++ apps/frontend/src/components/drawers/moderationEdit.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+import { mdiPlus, mdiClose } from '@mdi/js';
+import { ChannelModerationSetting } from '@tsuwari/typeorm/entities/ChannelModerationSetting';
+import { ref } from 'vue';
+
+import confirmDeletion from '@/components/confirmDeletion.vue';
+
+const props = defineProps<{
+  settings: ChannelModerationSetting
+}>();
+
+const emits = defineEmits<{
+  (event: 'cancel'): () => void
+}>();
+
+const settings = ref(props.settings);
+
+function onDelete() {
+  console.log(settings);
+  emits('cancel');
+}
+</script>
+
+<template>
+  <div>
+    <v-list-item>
+      <div class="d-flex justify-space-between">
+        <h1>Settings of {{ settings.type }}</h1>
+        <div class="d-flex d-inline">
+          <v-btn size="small" class="mt-1 mr-2" @click="$emit('cancel')">
+            Cancel
+          </v-btn>
+          <confirmDeletion :cb="() => onDelete()">
+            <v-btn color="red" size="small" class="mt-1 mr-2">
+              Delete
+            </v-btn>
+          </confirmDeletion>
+          <v-btn color="green" size="small" class="mt-1">
+            Save
+          </v-btn>
+        </div>
+      </div>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list-item>
+      <v-form class="mt-2">
+        <div
+          class="d-flex flex-column"
+        >
+          qwe
+        </div>
+      </v-form>
+    </v-list-item>
+  </div>
+</template>
--- apps/frontend/src/pages/Moderation.vue
+++ apps/frontend/src/pages/Moderation.vue
@@ -1,9 +1,17 @@
 <script lang="ts" setup>
-import { mdiLinkBoxVariant, mdiCapsLock, mdiClipboard, mdiCalculatorVariant, mdiEmoticon, mdiWrapDisabled } from '@mdi/js';
+import { 
+  mdiLinkBoxVariant, 
+  mdiCapsLock, 
+  mdiClipboard, 
+  mdiCalculatorVariant, 
+  mdiEmoticon, 
+  mdiWrapDisabled, 
+  mdiPencil, 
+} from '@mdi/js';
 import chunk from 'lodash.chunk';
 import { useDisplay } from 'vuetify';
 
-import Card from '@/components/moderation/card.vue';
+import { moderationSettings } from '@/data/moderationSettings';
 
 const { smAndDown } = useDisplay();
 
@@ -15,6 +23,15 @@ const types = chunk([
   { key: 'blacklists', icon: mdiClipboard, iconColor: 'red' }, 
   { key: 'longMessage', icon: mdiWrapDisabled, iconColor: 'cyan' },
 ], 2);
+
+const descriptions = {
+  'links': `Remove messages containing any links you haven't whitelisted.`,
+  'caps': `Remove messages containing excessive amounts of capital letters.`,
+  'symbols': `Remove messages containing disruptive or excessive use of symbols.`,
+  'longMessage': `Remove lengthy messages.`,
+  'emotes': 'Remove messages containing an excessive amount of emotes.',
+  'blacklists': 'Remove blacklisted words.',
+} as { [x: string]: string };
 </script>
 
 <template>
@@ -25,7 +42,26 @@ const types = chunk([
       justify="center"
     >
       <v-col v-for="(item, itemsIndex) of items" :key="itemsIndex" :cols="smAndDown ? 12 : 4">
-        <Card :type="(item.key as any)" :icon="item.icon" :icon-color="item.iconColor" />
+        <v-card class="ma-2 d-flex flex-column">
+          <v-card-title>
+            <div class="d-flex justify-space-between">
+              <div>
+                <v-icon :icon="item.icon" :color="item.iconColor"></v-icon>
+                {{ item.key.charAt(0).toUpperCase() + item.key.substring(1) }}
+              </div>
+              <div style="height: 10px;" class="d-flex justify-space-between">
+                <v-btn :icon="mdiPencil" variant="flat" size="x-small" />
+                <!-- <v-switch style="margin-top:-10px" color="indigo" /> -->
+              </div>
+            </div>
+          </v-card-title>
+          <v-divider />
+          <v-card-text>
+            <v-row align="center" no-gutters>
+              {{ descriptions[item.key as string] ?? '' }}
+            </v-row>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </div>
