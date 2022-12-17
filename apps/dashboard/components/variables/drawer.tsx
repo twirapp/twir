144cbe937fade62da93798c838ca44083a9b87fc
Satont <satontworldwide@gmail.com>
2022-12-17 22:41:34 +0000

    upd Chatting

---
+++ apps/dashboard/components/variables/drawer.tsx
@@ -0,0 +1,84 @@
+import {
+  Badge,
+  Drawer,
+  Flex,
+  ScrollArea,
+  Select,
+  Textarea,
+  TextInput,
+  useMantineColorScheme,
+  useMantineTheme,
+} from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { useViewportSize } from '@mantine/hooks';
+import Editor, { Monaco, useMonaco } from '@monaco-editor/react';
+import { ChannelCustomvar } from '@tsuwari/typeorm/entities/ChannelCustomvar';
+import { useEffect, useRef } from 'react';
+
+type Props = {
+  opened: boolean;
+  variable: ChannelCustomvar;
+  setOpened: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const VariableDrawer: React.FC<Props> = (props) => {
+  const theme = useMantineTheme();
+  const form = useForm<ChannelCustomvar>({});
+  const viewPort = useViewportSize();
+  const editorRef = useRef(null);
+
+  function handleEditorDidMount(editor: any) {
+    editorRef.current = editor;
+  }
+
+  useEffect(() => {
+    form.setValues(props.variable);
+  }, [props.variable]);
+
+  return (
+    <Drawer
+      opened={props.opened}
+      onClose={() => props.setOpened(false)}
+      title={<Badge size="xl">{props.variable.name}</Badge>}
+      padding="xl"
+      size="xl"
+      position="right"
+      transition="slide-left"
+      overlayColor={theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2]}
+      overlayOpacity={0.55}
+      overlayBlur={3}
+    >
+      <ScrollArea.Autosize maxHeight={viewPort.height - 120} type="auto" offsetScrollbars={true}>
+        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+          <Flex direction="column" gap="md" justify="flex-start" align="flex-start" wrap="wrap">
+            <TextInput label="Name" required {...form.getInputProps('name')} />
+            <Select
+              label="Variable type"
+              data={[
+                { value: 'SCRIPT', label: 'Script' },
+                { value: 'TEXT', label: 'Text' },
+              ]}
+              {...form.getInputProps('type')}
+            />
+            {form.values.type === 'SCRIPT' && (
+              <Editor
+                height="50vh"
+                defaultLanguage="javascript"
+                theme={theme.colorScheme === 'dark' ? 'vs-dark' : 'light'}
+                defaultValue="// some comment"
+                onMount={handleEditorDidMount}
+                value={form.values.evalValue}
+                onChange={(v) => {
+                  form.values.evalValue = v ?? '';
+                }}
+              />
+            )}
+            {form.values.type === 'TEXT' && (
+              <Textarea label="Response" autosize={true} {...form.getInputProps('response')} />
+            )}
+          </Flex>
+        </form>
+      </ScrollArea.Autosize>
+    </Drawer>
+  );
+};
---
+++ apps/dashboard/pages/variables.tsx
@@ -0,0 +1,63 @@
+import { Badge, Button, Table } from '@mantine/core';
+import { useLocalStorage } from '@mantine/hooks';
+import { Dashboard } from '@tsuwari/shared';
+import { ChannelCustomvar } from '@tsuwari/typeorm/entities/ChannelCustomvar';
+import { useState } from 'react';
+import useSWR from 'swr';
+
+import { VariableDrawer } from '../components/variables/drawer';
+import { swrFetcher } from '../services/swrFetcher';
+
+export default function () {
+  const [editDrawerOpened, setEditDrawerOpened] = useState(false);
+  const [editableVariable, setEditableVariable] = useState<ChannelCustomvar>({} as any);
+  const [selectedDashboard] = useLocalStorage<Dashboard>({
+    key: 'selectedDashboard',
+    serialize: (v) => JSON.stringify(v),
+    deserialize: (v) => JSON.parse(v),
+  });
+
+  const { data: variables } = useSWR<ChannelCustomvar[]>(
+    selectedDashboard ? `/api/v1/channels/${selectedDashboard.channelId}/variables` : null,
+    swrFetcher,
+  );
+
+  return (
+    <div>
+      <Table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {variables &&
+            variables.map((element, idx) => (
+              <tr key={element.id}>
+                <td>
+                  <Badge>{element.name}</Badge>
+                </td>
+                <td>
+                  <Button
+                    onClick={() => {
+                      setEditableVariable(variables[idx] as any);
+                      setEditDrawerOpened(true);
+                    }}
+                  >
+                    Edit
+                  </Button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+
+      <VariableDrawer
+        opened={editDrawerOpened}
+        setOpened={setEditDrawerOpened}
+        variable={editableVariable}
+      />
+    </div>
+  );
+}
