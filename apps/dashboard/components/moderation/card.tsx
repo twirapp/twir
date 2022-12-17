d63644420a76d5addab8aba7d59507bd496cc372
Satont <satontworldwide@gmail.com>
2022-12-17 20:47:45 +0000

    upd Chatting

--- apps/dashboard/components/moderation/card.tsx
+++ apps/dashboard/components/moderation/card.tsx
@@ -1,65 +1,14 @@
 import { ActionIcon, Card, Divider, Flex, Grid, Group, Text } from '@mantine/core';
-import {
-  IconLambda,
-  IconLetterCaseUpper,
-  IconLink,
-  IconMoodSmile,
-  IconPencil,
-  IconPlaylistX,
-  IconTextWrapDisabled,
-  TablerIcon,
-} from '@tabler/icons';
-import type {
-  ChannelModerationSetting,
-  SettingsType,
-} from '@tsuwari/typeorm/entities/ChannelModerationSetting';
+import { IconPencil } from '@tabler/icons';
+import type { ChannelModerationSetting } from '@tsuwari/typeorm/entities/ChannelModerationSetting';
+
+import { typesMapping } from './mapping';
 
 type Props = React.PropsWithChildren<{
   settings: ChannelModerationSetting;
   setEditableSettings: React.Dispatch<React.SetStateAction<ChannelModerationSetting>>;
   setEditDrawerOpened: React.Dispatch<React.SetStateAction<boolean>>;
 }>;
-
-const typesMapping: Record<
-  keyof typeof SettingsType,
-  {
-    icon: TablerIcon;
-    iconColor?: string;
-    name?: string;
-    description: string;
-  }
-> = {
-  links: {
-    icon: IconLink,
-    iconColor: 'cyan',
-    description: `Remove messages containing any links you haven't whitelisted.`,
-  },
-  caps: {
-    icon: IconLetterCaseUpper,
-    iconColor: 'orange',
-    description: `Remove messages containing excessive amounts of capital letters.`,
-  },
-  emotes: {
-    icon: IconMoodSmile,
-    iconColor: 'yellow',
-    description: 'Remove messages containing an excessive amount of emotes.',
-  },
-  longMessage: {
-    icon: IconTextWrapDisabled,
-    name: 'Long Messages',
-    description: `Remove lengthy messages.`,
-  },
-  blacklists: {
-    icon: IconPlaylistX,
-    name: 'Deny List',
-    description: 'Remove denied words from chat.',
-  },
-  symbols: {
-    icon: IconLambda,
-    iconColor: 'green',
-    description: `Remove messages containing disruptive or excessive use of symbols.`,
-  },
-};
 
 export const ModerationCard: React.FC<Props> = (props) => {
   return (
@@ -78,7 +27,12 @@ export const ModerationCard: React.FC<Props> = (props) => {
                     props.settings.type.charAt(0).toUpperCase() + props.settings.type.slice(1)}
                 </Text>
               </Group>
-              <ActionIcon onClick={() => props.setEditDrawerOpened(true)}>
+              <ActionIcon
+                onClick={() => {
+                  props.setEditDrawerOpened(true);
+                  props.setEditableSettings(props.settings);
+                }}
+              >
                 <IconPencil size={18}></IconPencil>
               </ActionIcon>
             </Flex>
--- apps/dashboard/components/moderation/drawer.tsx
+++ apps/dashboard/components/moderation/drawer.tsx
@@ -1,8 +1,21 @@
-import { Badge, Drawer, Flex, ScrollArea, useMantineTheme } from '@mantine/core';
+import {
+  Badge,
+  Drawer,
+  Flex,
+  Grid,
+  Group,
+  NumberInput,
+  ScrollArea,
+  Switch,
+  TextInput,
+  useMantineTheme,
+} from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { useViewportSize } from '@mantine/hooks';
 import { ChannelModerationSetting } from '@tsuwari/typeorm/entities/ChannelModerationSetting';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+import { typesMapping } from './mapping';
 
 type Props = {
   opened: boolean;
@@ -14,9 +27,14 @@ export const ModerationDrawer: React.FC<Props> = (props) => {
   const theme = useMantineTheme();
   const form = useForm<ChannelModerationSetting>({});
   const viewPort = useViewportSize();
+  const [title, setTitle] = useState('');
 
   useEffect(() => {
     form.setValues(props.settings);
+    const titleValue = props.settings.type
+      ? typesMapping[props.settings.type]?.name ?? props.settings.type
+      : '';
+    setTitle(titleValue);
   }, [props.settings]);
 
   return (
@@ -24,11 +42,7 @@ export const ModerationDrawer: React.FC<Props> = (props) => {
       <Drawer
         opened={props.opened}
         onClose={() => props.setOpened(false)}
-        title={
-          <Badge size="xl">
-            {props.settings.type?.charAt(0).toUpperCase() + props.settings.type?.slice(1)}
-          </Badge>
-        }
+        title={<Badge size="xl">{title.charAt(0).toUpperCase() + title.slice(1)}</Badge>}
         padding="xl"
         size="xl"
         position="right"
@@ -40,7 +54,22 @@ export const ModerationDrawer: React.FC<Props> = (props) => {
         <ScrollArea.Autosize maxHeight={viewPort.height - 120} type="auto" offsetScrollbars={true}>
           <form onSubmit={form.onSubmit((values) => console.log(values))}>
             <Flex direction="column" gap="md" justify="flex-start" align="flex-start" wrap="wrap">
-              qwe
+              <Grid>
+                <Grid.Col xs={12} sm={8} md={4} lg={4} xl={4}>
+                  <TextInput label="Timeout message" />
+                </Grid.Col>
+                <Grid.Col xs={12} sm={3} md={4} lg={4} xl={4}>
+                  <NumberInput label="Timeout time" />
+                </Grid.Col>
+              </Grid>
+              <NumberInput label="Warning message" />
+              <Group grow>
+                {props.settings.type === 'links' && (
+                  <Switch label="Moderate clips" labelPosition="left" />
+                )}
+                <Switch label="Moderate vips" labelPosition="left" />
+                <Switch label="Moderate subscribers" labelPosition="left" />
+              </Group>
             </Flex>
           </form>
         </ScrollArea.Autosize>
