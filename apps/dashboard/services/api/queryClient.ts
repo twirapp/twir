f0ece4883426931b421a83a0aa9a1f77820a6064
Satont <satontworldwide@gmail.com>
2022-12-23 20:33:47 +0000

    upd Chatting

--- apps/dashboard/services/api/queryClient.ts
+++ apps/dashboard/services/api/queryClient.ts
@@ -7,6 +7,9 @@ import { printError } from '@/services/api/error';
 
 export const queryClient = new QueryClient({
   defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
     mutations: {
       onError: (error: any) => {
         if (error instanceof FetcherError) {
