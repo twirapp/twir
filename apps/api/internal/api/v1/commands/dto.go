062aac72500b2e2f12c2888ddbbc1f28cd7daf39
Satont <satontworldwide@gmail.com>
2022-12-25 04:33:02 +0000

    feat(api): adjust commands text limit

--- apps/api/internal/api/v1/commands/dto.go
+++ apps/api/internal/api/v1/commands/dto.go
@@ -1,19 +1,19 @@
 package commands
 
 type responsesDto struct {
-	Text  string `validate:"required,min=1,max=400" json:"text"`
+	Text  string `validate:"required,min=1,max=500" json:"text"`
 	Order uint8  `validate:"gte=0"                                             json:"order"`
 }
 
 type commandDto struct {
 	ID *string `json:"id,omitempty"`
 
-	Name               string         `validate:"required,min=1,max=50"       json:"name"`
+	Name               string         `validate:"required,min=1,max=100"       json:"name"`
 	Cooldown           uint32         `validate:"lte=86400"                   json:"cooldown"`
 	CooldownType       string         `validate:"required"                    json:"cooldownType"`
-	Description        *string        `validate:"omitempty,max=400"           json:"description,omitempty"`
+	Description        *string        `validate:"omitempty,max=500"           json:"description,omitempty"`
 	Permission         string         `validate:"required"                    json:"permission"`
-	Aliases            []string       `validate:"max=20,dive,required,max=50" json:"aliases"`
+	Aliases            []string       `validate:"max=20,dive,required" json:"aliases"`
 	Visible            *bool          `validate:"omitempty,required"          json:"visible,omitempty"`
 	Enabled            *bool          `validate:"omitempty,required"          json:"enabled,omitempty"`
 	Responses          []responsesDto `validate:"dive"                        json:"responses"`
