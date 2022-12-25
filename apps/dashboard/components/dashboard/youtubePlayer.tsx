d92a0dbe0ea6b5bda39fe29198d5e3b5f5c86d43
Satont <satontworldwide@gmail.com>
2022-12-25 08:31:02 +0000

    upd Chatting

--- apps/dashboard/components/dashboard/youtubePlayer.tsx
+++ apps/dashboard/components/dashboard/youtubePlayer.tsx
@@ -1,7 +1,8 @@
-import { ActionIcon, Button, Card, Divider, Flex, Grid, Group, Text } from '@mantine/core';
+import { ActionIcon, Button, Card, Divider, Flex, Grid, Group, Text, Transition } from '@mantine/core';
+import { useListState } from '@mantine/hooks';
 import { IconPlayerPlay, IconPlayerSkipForward } from '@tabler/icons';
-import Plyr, { APITypes, PlyrSource, PlyrOptions } from 'plyr-react';
-import React, { useRef, useState } from 'react';
+import Plyr, { APITypes, PlyrOptions } from 'plyr-react';
+import React, { useEffect, useRef, useState } from 'react';
 import 'plyr-react/plyr.css';
 
 const plyrOptions: PlyrOptions = {
@@ -23,29 +24,40 @@ const plyrOptions: PlyrOptions = {
   debug: false,
 };
 
+type Track = Plyr.Source & {
+  title: string,
+  orderedBy: string
+}
+
 export const YoutubePlayer: React.FC = () => {
   const plyrRef = useRef<APITypes>(null) as React.MutableRefObject<APITypes>;
 
-  const [source, setSource] = useState<PlyrSource>( {
-    type: 'video',
-    sources: [
-      {
-        src: 'WLcHVVS90zQ',
-        provider: 'youtube',
-      },
-    ],
-  });
+  const [currentTrack, setCurrentTrack] = useState<Track>();
+  const [songs, songsHandlers] = useListState<Track>([
+    {
+      src: 'WLcHVVS90zQ',
+      provider: 'youtube',
+      title: 'Test',
+      orderedBy: 'Satont',
+    },
+    {
+      src: 'FCtasDPQ9e8',
+      provider: 'youtube',
+      title: 'Test 2',
+      orderedBy: 'mellkam',
+    },
+  ]);
+
+  useEffect(() => {
+    if (songs.at(0)) {
+      setCurrentTrack(songs.at(0));
+    } else {
+      setCurrentTrack(undefined);
+    }
+  }, [songs]);
 
   const setVideo = () => {
-    plyrRef.current.plyr.source = {
-      type: 'video',
-      sources: [
-        {
-          src: 'FCtasDPQ9e8',
-          provider: 'youtube',
-        },
-      ],
-    };
+    songsHandlers.shift();
   };
 
   return <Grid grow>
@@ -54,29 +66,37 @@ export const YoutubePlayer: React.FC = () => {
         <Card.Section p={'xs'}>
           <Flex gap="xs" direction="row" justify="space-between">
             <Text size="md">YouTube</Text>
+            <Button onClick={setVideo}>test change source</Button>
           </Flex>
         </Card.Section>
         <Divider />
         <Card.Section>
+          <Text hidden={!!songs.length}>no songs</Text>
           <Plyr
             ref={plyrRef as any}
-            source={source}
+            source={{
+              type: 'video',
+              sources: currentTrack ? [currentTrack] : [],
+            }}
             options={plyrOptions}
+            hidden={!songs.length}
           />
           {/*<video ref={ref} className="plyr-react plyr" {...rest} />*/}
         </Card.Section>
-        <Card.Section p={'xs'}>
-          <Flex direction={'row'} justify={'space-between'}>
-            <Flex direction={'column'}>
-              <Text size={'lg'}>qweqweqweqweqweqweqweqweqweqweqwe</Text>
-              <Text size={'xs'} color={'lime'}>Ordered by: mellkam</Text>
+        <Transition mounted={!!currentTrack} transition="slide-down" duration={1500} timingFunction="ease">
+          {(styles) => <Card.Section p={'xs'} style={styles}>
+            <Flex direction={'row'} justify={'space-between'}>
+              <Flex direction={'column'}>
+                <Text size={'lg'}>{currentTrack?.title}</Text>
+                <Text size={'xs'} color={'lime'}>Ordered by: {currentTrack?.orderedBy}</Text>
+              </Flex>
+              <Group>
+                <ActionIcon><IconPlayerPlay /></ActionIcon>
+                <ActionIcon><IconPlayerSkipForward /></ActionIcon>
+              </Group>
             </Flex>
-            <Group>
-              <ActionIcon><IconPlayerPlay /></ActionIcon>
-              <ActionIcon><IconPlayerSkipForward /></ActionIcon>
-            </Group>
-          </Flex>
-        </Card.Section>
+          </Card.Section>}
+        </Transition>
       </Card>
     </Grid.Col>
   </Grid>;
