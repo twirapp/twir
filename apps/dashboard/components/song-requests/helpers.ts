c786b37a16f18b19035838e1d235b729fd321958
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-01-02 17:21:03 +0000

    fix(dashboard/songrequests): correct convert milliseconds to time

--- apps/dashboard/components/song-requests/helpers.ts
+++ apps/dashboard/components/song-requests/helpers.ts
@@ -5,12 +5,22 @@ export const formatDuration = (seconds: number) => {
   return [minutes, seconds % 60].map(format).join(':');
 };
 
-export function millisToMinutesAndSeconds(millis: number) {
-  const minutes = Math.floor(millis / 60000);
-  const seconds = ((millis % 60000) / 1000);
-  return minutes + ':' + (seconds < 10 ? '0' : '') + seconds.toFixed(0);
-}
+export const convertMillisToTime = (millis: number) => {
+  let seconds = Math.floor(millis / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
 
+  seconds = seconds % 60;
+  minutes = minutes % 60;
+
+  return `${hours ? `${padTo2Digits(hours)}:` : ''}${padTo2Digits(minutes)}:${padTo2Digits(
+    seconds,
+  )}`;
+};
+
+export const padTo2Digits = (num: number) => {
+  return num.toString().padStart(2, '0');
+};
 
 export const moveItem = <T>(items: T[], from: number, to: number) => {
   const values = [...items];
--- apps/dashboard/components/song-requests/list.tsx
+++ apps/dashboard/components/song-requests/list.tsx
@@ -1,11 +1,10 @@
 import { ActionIcon, createStyles, Flex, Table } from '@mantine/core';
-import { useListState } from '@mantine/hooks';
 import { IconGripVertical, IconTrash } from '@tabler/icons';
 import { RequestedSong } from '@tsuwari/typeorm/entities/RequestedSong';
 import { useCallback, useContext, useEffect, useState } from 'react';
 import { DragDropContext, Draggable, DraggableLocation, Droppable } from 'react-beautiful-dnd';
 
-import { millisToMinutesAndSeconds } from './helpers';
+import { convertMillisToTime } from './helpers';
 
 import { PlayerContext } from '@/components/song-requests/context';
 
@@ -13,7 +12,6 @@ const useStyles = createStyles((theme) => ({
   item: {
     backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
   },
-
   dragHandle: {
     ...theme.fn.focusStyles(),
     width: 40,
@@ -35,24 +33,31 @@ export const VideosList: React.FC = () => {
     setIsBrowser(process.browser);
   }, []);
 
-
   const items = videos.map((video, index) => (
     <Draggable key={video.id} index={index} draggableId={video.id}>
       {(provided) => (
-        <tr className={classes.item} ref={provided.innerRef} {...provided.draggableProps} hidden={index === 0}>
+        <tr
+          className={classes.item}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          hidden={index === 0}
+        >
           <td>
             <div className={classes.dragHandle} {...provided.dragHandleProps}>
-              <IconGripVertical size={18} stroke={1.5}/>
+              <IconGripVertical size={18} stroke={1.5} />
             </div>
           </td>
           <td>{index}</td>
-          <td><a href={'https://youtu.be/' + video.videoId}>{video.title}</a></td>
+          <td>
+            <a href={'https://youtu.be/' + video.videoId}>{video.title}</a>
+          </td>
           <td>{video.orderedByName}</td>
-          <td>{millisToMinutesAndSeconds(video.duration)}</td>
+          <td>{convertMillisToTime(video.duration)}</td>
           <td>
             <Flex>
-              <ActionIcon variant={'filled'} color={'red'} onClick={() => skipVideo(index)}><IconTrash size={14}
-              /></ActionIcon>
+              <ActionIcon variant={'filled'} color={'red'} onClick={() => skipVideo(index)}>
+                <IconTrash size={14} />
+              </ActionIcon>
             </Flex>
           </td>
         </tr>
@@ -61,33 +66,37 @@ export const VideosList: React.FC = () => {
   ));
 
   return (
-    <>{isBrowser
-      ? <DragDropContext
-        onDragEnd={({ destination, source }) => {
-          reorderVideos(destination!, source);
-        }}
-      >
-        <Table>
-          <thead>
-          <tr>
-            <th style={{ width: 40 }}></th>
-            <th>#</th>
-            <th>Title</th>
-            <th>Requested by</th>
-            <th>Duration</th>
-            <th>Actions</th>
-          </tr>
-          </thead>
-          <Droppable droppableId="dnd-list" direction="vertical">
-            {(provided) => (
-              <tbody {...provided.droppableProps} ref={provided.innerRef}>
-              {items}
-              {provided.placeholder}
-              </tbody>
-            )}
-          </Droppable>
-        </Table>
-      </DragDropContext>
-      : <div></div>}
-    </>);
+    <>
+      {isBrowser ? (
+        <DragDropContext
+          onDragEnd={({ destination, source }) => {
+            reorderVideos(destination!, source);
+          }}
+        >
+          <Table>
+            <thead>
+              <tr>
+                <th style={{ width: 40 }}></th>
+                <th>#</th>
+                <th>Title</th>
+                <th>Requested by</th>
+                <th>Duration</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <Droppable droppableId="dnd-list" direction="vertical">
+              {(provided) => (
+                <tbody {...provided.droppableProps} ref={provided.innerRef}>
+                  {items}
+                  {provided.placeholder}
+                </tbody>
+              )}
+            </Droppable>
+          </Table>
+        </DragDropContext>
+      ) : (
+        <div></div>
+      )}
+    </>
+  );
 };
