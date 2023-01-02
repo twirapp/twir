70fc24927933e2c961ef3b74ff160d2c88fb411e
Satont <satontworldwide@gmail.com>
2023-01-02 05:10:08 +0300

    fix(dashboard): correct width of app shell

--- apps/dashboard/pages/_app.tsx
+++ apps/dashboard/pages/_app.tsx
@@ -1,11 +1,4 @@
-import {
-  AppShell,
-  ColorScheme,
-  ColorSchemeProvider,
-  MantineProvider,
-  ScrollArea,
-  useMantineTheme,
-} from '@mantine/core';
+import { AppShell, ColorScheme, ColorSchemeProvider, MantineProvider, useMantineTheme } from '@mantine/core';
 import { useColorScheme, useHotkeys, useLocalStorage } from '@mantine/hooks';
 import { ModalsProvider } from '@mantine/modals';
 import { NotificationsProvider } from '@mantine/notifications';
@@ -46,7 +39,7 @@ const app = function App(props: AppProps) {
     <>
       <Head>
         <title>Tsuwari</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
       </Head>
       <QueryClientProvider client={queryClient}>
         <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
@@ -54,7 +47,7 @@ const app = function App(props: AppProps) {
             <NotificationsProvider position={'top-center'} limit={5}>
               <SpotlightProvider
                 actions={[]}
-                searchIcon={<IconSearch size={18} />}
+                searchIcon={<IconSearch size={18}/>}
                 searchPlaceholder="Search..."
                 shortcut={['mod+k']}
                 nothingFoundMessage="Nothing found..."
@@ -73,12 +66,13 @@ const app = function App(props: AppProps) {
                         background:
                           colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
                         padding: 0,
+                        width: '100%',
                       },
                     }}
                     navbarOffsetBreakpoint="sm"
                     asideOffsetBreakpoint="sm"
-                    navbar={<SideBar opened={sidebarOpened} setOpened={setSidebarOpened} />}
-                    header={<NavBar setOpened={setSidebarOpened} opened={sidebarOpened} />}
+                    navbar={<SideBar opened={sidebarOpened} setOpened={setSidebarOpened}/>}
+                    header={<NavBar setOpened={setSidebarOpened} opened={sidebarOpened}/>}
                   >
                     <AppProvider colorScheme={colorScheme}><Component
                       styles={{
