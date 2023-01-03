76e6fcae08705370fb027111a65da115d331cbef
Satont <satontworldwide@gmail.com>
2023-01-03 14:15:47 +0300

    fix(grpc): correct path to ts proto plugin
    [skip-docker]

--- libs/grpc/gen.ts
+++ libs/grpc/gen.ts
@@ -1,6 +1,8 @@
 import { exec } from 'node:child_process';
 import { existsSync, mkdirSync, readFileSync, rmSync, statSync } from 'node:fs';
 import { readdir } from 'node:fs/promises';
+import { platform } from 'node:os';
+import { resolve } from 'node:path';
 import { promisify } from 'util';
 
 const promisedExec = promisify(exec);
@@ -22,11 +24,18 @@ rmSync('generated', { recursive: true, force: true });
           mkdirSync(`generated/${name}`, { recursive: true });
         }
 
+        const protocPath = resolve(
+          __dirname,
+          'node_modules',
+          '.bin',
+          `protoc-gen-ts_proto${platform() === 'win32' ? '.CMD' : ''}`,
+        );
+
         const requests = await Promise.all([
           promisedExec(`
         protoc --go_out=./generated/${name} --go_opt=paths=source_relative --go-grpc_out=./generated/${name} --go-grpc_opt=paths=source_relative --experimental_allow_proto3_optional --proto_path=./protos ${name}.proto`),
           promisedExec(
-            `protoc --plugin=protoc-gen-ts_proto=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./generated/${name} --ts_proto_opt=outputServices=nice-grpc,outputServices=generic-definitions,useExactTypes=false,esModuleInterop=true --experimental_allow_proto3_optional --proto_path=./protos ${name}.proto`,
+            `protoc --plugin=protoc-gen-ts_proto=${protocPath} --ts_proto_out=./generated/${name} --ts_proto_opt=outputServices=nice-grpc,outputServices=generic-definitions,useExactTypes=false,esModuleInterop=true --experimental_allow_proto3_optional --proto_path=./protos ${name}.proto`,
           ),
         ]);
 
