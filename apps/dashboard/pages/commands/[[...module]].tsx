341fe66c08478fcc2a0919c977cbf691305c3a31
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-01-04 11:18:39 +0000

    fix: horizontal scroll on commands page

--- apps/dashboard/pages/commands/[[...module]].tsx
+++ apps/dashboard/pages/commands/[[...module]].tsx
@@ -1,4 +1,14 @@
-import { ActionIcon, Badge, Button, Flex, Group, Switch, Table, Text, TextInput } from '@mantine/core';
+import {
+  ActionIcon,
+  Badge,
+  Button,
+  Flex,
+  Group,
+  Switch,
+  Table,
+  Text,
+  TextInput,
+} from '@mantine/core';
 import { useDebouncedState, useViewportSize } from '@mantine/hooks';
 import { IconPencil, IconSearch, IconTrash } from '@tabler/icons';
 import { ChannelCommand } from '@tsuwari/typeorm/entities/ChannelCommand';
@@ -39,8 +49,8 @@ const Commands: NextPage = () => {
           <Text size="lg">{t('title')}</Text>
           <TextInput
             placeholder={'search...'}
-            rightSection={<IconSearch size={18}/>}
-            onChange={event => setSearchInput(event.target.value)}
+            rightSection={<IconSearch size={18} />}
+            onChange={(event) => setSearchInput(event.target.value)}
           />
         </Group>
         <Button
@@ -53,75 +63,85 @@ const Commands: NextPage = () => {
           {t('create')}
         </Button>
       </Flex>
-      <Table>
+      <Table style={{ tableLayout: 'fixed', width: '100%' }}>
         <thead>
-        <tr>
-          <th>{t('name')}</th>
-          {viewPort.width > 550 && <th>{t('responses')}</th>}
-          <th>{t('table.head.status')}</th>
-          <th>{t('table.head.actions')}</th>
-        </tr>
+          <tr>
+            <th>{t('name')}</th>
+            {viewPort.width > 550 && <th>{t('responses')}</th>}
+            <th>{t('table.head.status')}</th>
+            <th>{t('table.head.actions')}</th>
+          </tr>
         </thead>
 
         <tbody>
-        {commands &&
-          commands.length &&
-          commands
-            .filter((c) => {
-              const isActiveTab = c.module.toLowerCase() === (router.query.module ? router.query.module[0] : 'custom');
-              const nameIncludes = c.name.includes(searchInput);
-              const aliasesIncludes = c.aliases.some(a => a.includes(searchInput));
-              return isActiveTab && (nameIncludes || aliasesIncludes);
-            })
-            .map((command) => (
-              <tr key={command.id}>
-                <td>
-                  <Badge>{command.name}</Badge>
-                </td>
-                {viewPort.width > 550 && <td>
-                  {command.module != 'CUSTOM' && <Badge>{t('builtInBadge')}</Badge>}
-                  {command.module === 'CUSTOM' &&
-                    (command.responses?.map((r, i) => (
-                      <p key={i} style={{ margin: 0 }}>
-                        {r.text}
-                      </p>
-                    )) || <Badge>No Response</Badge>)}
-                </td>}
-                <td>
-                  <Switch
-                    checked={command.enabled}
-                    onChange={() => patcher.mutate({ id: command.id, data: { enabled: !command.enabled } })}
-                  />
-                </td>
-                <td>
-                  <Flex direction="row" gap="xs">
-                    <ActionIcon
-                      onClick={() => {
-                        setEditableCommand(commands.find((c) => c.id === command.id)!);
-                        setEditDrawerOpened(true);
-                      }}
-                      variant="filled"
-                      color="blue"
-                    >
-                      <IconPencil size={14}/>
-                    </ActionIcon>
-                    {command.module === 'CUSTOM' && (
+          {commands &&
+            commands.length &&
+            commands
+              .filter((c) => {
+                const isActiveTab =
+                  c.module.toLowerCase() ===
+                  (router.query.module ? router.query.module[0] : 'custom');
+                const nameIncludes = c.name.includes(searchInput);
+                const aliasesIncludes = c.aliases.some((a) => a.includes(searchInput));
+                return isActiveTab && (nameIncludes || aliasesIncludes);
+              })
+              .map((command) => (
+                <tr key={command.id}>
+                  <td>
+                    <Badge>{command.name}</Badge>
+                  </td>
+                  {viewPort.width > 550 && (
+                    <td>
+                      {command.module != 'CUSTOM' && <Badge>{t('builtInBadge')}</Badge>}
+                      {command.module === 'CUSTOM' &&
+                        (command.responses?.map((r) => (
+                          <Text
+                            title={r.text!}
+                            lineClamp={1}
+                            style={{ textOverflow: 'ellipsis', overflow: 'hidden' }}
+                          >
+                            {r.text}
+                          </Text>
+                        )) || <Badge>No Response</Badge>)}
+                    </td>
+                  )}
+                  <td>
+                    <Switch
+                      checked={command.enabled}
+                      onChange={() =>
+                        patcher.mutate({ id: command.id, data: { enabled: !command.enabled } })
+                      }
+                    />
+                  </td>
+                  <td>
+                    <Flex direction="row" gap="xs">
                       <ActionIcon
-                        onClick={() =>
-                          confirmDelete({
-                            onConfirm: () => deleter.mutate(command.id),
-                          })
-                        }
+                        onClick={() => {
+                          setEditableCommand(commands.find((c) => c.id === command.id)!);
+                          setEditDrawerOpened(true);
+                        }}
                         variant="filled"
-                        color="red"
+                        color="blue"
                       >
-                        <IconTrash size={14}/>
+                        <IconPencil size={14} />
                       </ActionIcon>
-                    )}
-                  </Flex>
-                </td>
-              </tr>
-            ))}
+                      {command.module === 'CUSTOM' && (
+                        <ActionIcon
+                          onClick={() =>
+                            confirmDelete({
+                              onConfirm: () => deleter.mutate(command.id),
+                            })
+                          }
+                          variant="filled"
+                          color="red"
+                        >
+                          <IconTrash size={14} />
+                        </ActionIcon>
+                      )}
+                    </Flex>
+                  </td>
+                </tr>
+              ))}
         </tbody>
       </Table>
 
