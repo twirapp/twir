86bf55d8f7c3e7623cb3f0e3cedae6be73fad442
Satont <satontworldwide@gmail.com>
2023-02-02 16:21:42 +0300

    chore(tokens): add tokens encoder cli
    [skip-docker]

---
+++ apps/tokens/cmd/encode/encode.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/satont/tsuwari/libs/crypto"
+)
+
+func main() {
+	args := os.Args
+	if len(args) < 4 {
+		log.Fatal("Wrong number of arguments")
+	}
+
+	cipherKey := args[1]
+
+	accessToken, err := crypto.Encrypt(args[2], cipherKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+	refreshToken, err := crypto.Encrypt(args[3], cipherKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("accessToken: ", accessToken)
+	fmt.Println("refreshToken: ", refreshToken)
+}
