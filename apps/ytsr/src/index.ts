eca32887b6db87466a85cd6c81b19aae0ecd9701
Satont <satontworldwide@gmail.com>
2023-02-20 18:16:54 +0300

    fix(ytsr): correctly search video by link

--- apps/ytsr/src/index.ts
+++ apps/ytsr/src/index.ts
@@ -1,7 +1,8 @@
 import * as YTSR from '@tsuwari/grpc/generated/ytsr/ytsr';
 import { PORTS } from '@tsuwari/grpc/servers/constants';
 import { createServer } from 'nice-grpc';
-import ytsrLib from 'ytsr';
+import ytdl from 'ytdl-core';
+import ytsrLib, { Video } from 'ytsr';
 
 import { durationToMilliseconds } from './utils/convertDuration.js';
 
@@ -11,42 +12,52 @@ export const grpcServer = createServer({
 
 const linkRegexp = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=|embed\/|v\/|.+\?v=)?([\w-]{11})(?:\S+)?/g;
 
+
 const ytsrService: YTSR.YtsrServiceImplementation = {
   async search(request: YTSR.SearchRequest, context): Promise<YTSR.DeepPartial<YTSR.SearchResponse>> {
-    const videos: Array<ytsrLib.Video> = [];
-
-    const searchAndPush = async (input: string) => {
-      const search = await ytsrLib(input, { limit: 1 });
-      if (search.items.length && search.items[0]?.type === 'video') {
-        videos.push(search.items[0]);
-      }
-    };
+    const videos: Array<YTSR.Song> = [];
 
     const linkMatches = [...request.search.matchAll(linkRegexp)];
     if (linkMatches.length) {
       await Promise.all(linkMatches.map(async (match) => {
-        await searchAndPush(match[1]!);
+        const song = await ytdl.getInfo(match[0]);
+        if (!song) return;
+        videos.push({
+          title: song.videoDetails.title,
+          isLive: song.videoDetails.isLiveContent,
+          duration: Number(song.videoDetails.lengthSeconds) * 1000,
+          thumbnailUrl: song.videoDetails.thumbnails[0]?.url,
+          author: {
+            name: song.videoDetails.author.name,
+            avatarUrl: song.videoDetails.author.thumbnails?.at(0)?.url,
+            channelId: song.videoDetails.author.id,
+          },
+          id: song.videoDetails.videoId,
+          views: Number(song.videoDetails.viewCount),
+        });
       }));
     } else {
-      await searchAndPush(request.search);
-    }
-
-    return {
-      songs: videos.map((item) => {
-        return {
+      const search = await ytsrLib(request.search, { limit: 1 });
+      if (search.items.length && search.items?.at(0)?.type === 'video') {
+        const item = search.items.at(0) as ytsrLib.Video;
+        videos.push({
           title: item.title,
           isLive: item.isLive,
           views: item.views ?? 0,
           id: item.id,
-          author: item.author ? {
-            name: item.author.name,
-            avatarUrl: item.author.bestAvatar?.url ?? undefined,
-            channelId: item.author.channelID,
-          } : undefined,
-          thumbnailUrl: item.bestThumbnail.url ?? undefined,
+          thumbnailUrl: item.bestThumbnail?.url ?? undefined,
           duration: durationToMilliseconds(item.duration ?? '0:00'),
-        };
-      }),
+          author: {
+            name: item.author?.name || '',
+            channelId: item.author?.channelID || '',
+            avatarUrl: item.author?.bestAvatar?.url || '',
+          },
+        });
+      }
+    }
+
+    return {
+      songs: videos,
     };
   },
 };
