a33522090b0364b898db9f8fe696e325c905b334
Satont <satontworldwide@gmail.com>
2023-02-21 23:33:02 +0300

    chore: fix migrations name of column

--- libs/typeorm/src/migrations/1676998385847-events_rename_obsTargetToTarget.ts
+++ libs/typeorm/src/migrations/1676998385847-events_rename_obsTargetToTarget.ts
@@ -4,7 +4,7 @@ export class eventsRenameObsTargetToTarget1676998385847 implements MigrationInte
     name = 'eventsRenameObsTargetToTarget1676998385847';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "channels_events_operations" RENAME COLUMN "obsTarget" TO "target"`);
+        await queryRunner.query(`ALTER TABLE "channels_events_operations" RENAME COLUMN "obsTargetName" TO "target"`);
         await queryRunner.query(`ALTER TYPE "public"."channels_events_operations_type_enum" RENAME TO "channels_events_operations_type_enum_old"`);
         await queryRunner.query(`CREATE TYPE "public"."channels_events_operations_type_enum" AS ENUM('TIMEOUT', 'TIMEOUT_RANDOM', 'BAN', 'UNBAN', 'BAN_RANDOM', 'VIP', 'UNVIP', 'UNVIP_RANDOM', 'MOD', 'UNMOD', 'UNMOD_RANDOM', 'SEND_MESSAGE', 'CHANGE_TITLE', 'CHANGE_CATEGORY', 'FULFILL_REDEMPTION', 'CANCEL_REDEMPTION', 'ENABLE_SUBMODE', 'DISABLE_SUBMODE', 'ENABLE_EMOTEONLY', 'DISABLE_EMOTEONLY', 'CREATE_GREETING', 'OBS_SET_SCENE', 'OBS_TOGGLE_SOURCE', 'OBS_TOGGLE_AUDIO', 'OBS_AUDIO_SET_VOLUME', 'OBS_AUDIO_INCREASE_VOLUME', 'OBS_AUDIO_DECREASE_VOLUME', 'OBS_DISABLE_AUDIO', 'OBS_ENABLE_AUDIO', 'OBS_START_STREAM', 'OBS_STOP_STREAM', 'CHANGE_VARIABLE')`);
         await queryRunner.query(`ALTER TABLE "channels_events_operations" ALTER COLUMN "type" TYPE "public"."channels_events_operations_type_enum" USING "type"::"text"::"public"."channels_events_operations_type_enum"`);
