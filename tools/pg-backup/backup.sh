c8417f4fdd4281d8efdea71c0d205f3a63b7d7eb
Satont <satontworldwide@gmail.com>
2023-02-22 03:34:58 +0300

    chore(backup): fix args 3

--- tools/pg-backup/backup.sh
+++ tools/pg-backup/backup.sh
@@ -49,11 +49,12 @@ echo "Creating dump of ${POSTGRES_DATABASE} database from ${POSTGRES_HOST}..."
 SRC_FILE=dump.sql.gz
 DEST_FILE=${POSTGRES_DATABASE}_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz
 
-pg_dump -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DATABASE | gzip > $SRC_FILE
+pg_dump -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d "$POSTGRES_DATABASE" | gzip > $SRC_FILE
 
 echo "Uploading dump to $S3_BUCKET"
 
-cat $SRC_FILE | aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/$DEST_FILE || exit 2
+# shellcheck disable=SC2002
+cat $SRC_FILE | aws "$AWS_ARGS" s3 cp - s3://"$S3_BUCKET"/"$S3_PREFIX"/"$DEST_FILE" || exit 2
 
 echo "SQL backup finished"
 
