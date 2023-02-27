c7bba4d2b5719e638e09f15e8e13f84110ad6380
Satont <satontworldwide@gmail.com>
2023-02-27 16:31:23 +0300

    fix(parser): try to get game by `get games` endpoint, then try to search with indexOf alg

--- apps/parser/internal/commands/channel/game/game.go
+++ apps/parser/internal/commands/channel/game/game.go
@@ -65,24 +65,45 @@ var SetCommand = types.DefaultCommand{
 			return nil
 		}
 
-		games, err := twitchClient.SearchCategories(&helix.SearchCategoriesParams{
-			Query: *ctx.Text,
+		gameReq, err := twitchClient.GetGames(&helix.GamesParams{
+			Names: []string{*ctx.Text},
 		})
-
-		if err != nil || len(games.Data.Categories) == 0 || games.StatusCode != 200 {
-			result.Result = append(result.Result, "game not found on twitch")
-			return result
+		if err != nil {
+			return nil
 		}
 
-		categoryId := games.Data.Categories[0].ID
-		categoryName := games.Data.Categories[0].Name
+		categoryId := ""
+		categoryName := ""
 
-		for _, category := range games.Data.Categories {
-			if strings.Contains(strings.ToLower(category.Name), strings.ToLower(*ctx.Text)) {
-				categoryId = category.ID
-				categoryName = category.Name
-				break
+		if len(gameReq.Data.Games) > 0 {
+			categoryId = gameReq.Data.Games[0].ID
+			categoryName = gameReq.Data.Games[0].Name
+		} else {
+			games, err := twitchClient.SearchCategories(&helix.SearchCategoriesParams{
+				Query: *ctx.Text,
+			})
+
+			if err != nil {
+				return nil
 			}
+
+			if len(games.Data.Categories) > 0 {
+				categoryId = games.Data.Categories[0].ID
+				categoryName = games.Data.Categories[0].Name
+
+				for _, category := range games.Data.Categories {
+					if strings.Index(strings.ToLower(category.Name), strings.ToLower(*ctx.Text)) == 0 {
+						categoryId = category.ID
+						categoryName = category.Name
+						break
+					}
+				}
+			}
+		}
+
+		if categoryId == "" || categoryName == "" {
+			result.Result = append(result.Result, "game not found on twitch")
+			return result
 		}
 
 		req, err := twitchClient.EditChannelInformation(&helix.EditChannelInformationParams{
