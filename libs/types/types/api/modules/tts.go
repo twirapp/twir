e77158f181e551a71bc5a3b8410f203eecbb33c8
Satont <42675886+Satont@users.noreply.github.com>
2023-02-28 16:18:11 +0300

    fix: remove required from tts validations

--- libs/types/types/api/modules/tts.go
+++ libs/types/types/api/modules/tts.go
@@ -6,7 +6,7 @@ type TTSSettings struct {
 	Volume                             int    `validate:"gte=0,lte=100" json:"volume"`
 	Pitch                              int    `validate:"gte=0,lte=100" json:"pitch"`
 	Voice                              string `validate:"required" json:"voice"`
-	AllowUsersChooseVoiceInMainCommand bool   `validate:"required" json:"allow_users_choose_voice_in_main_command"`
+	AllowUsersChooseVoiceInMainCommand bool   `json:"allow_users_choose_voice_in_main_command"`
 	MaxSymbols                         int    `json:"max_symbols"`
 }
 
