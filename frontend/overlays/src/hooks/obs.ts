59350ca4b76e97c4458c707f55780c799649d91f
Satont <satontworldwide@gmail.com>
2023-03-05 14:41:35 +0300

    Revert "feat(overlays): add scene name to sources list"
    This reverts commit 0f5754e9302e882d56950246f9cb1a7ec2296ce5.

--- frontend/overlays/src/hooks/obs.ts
+++ frontend/overlays/src/hooks/obs.ts
@@ -117,7 +117,7 @@ export const useObs = () => {
       if (!item) return;
       const sceneName = mappedScenesNames[index];
       result[sceneName] = item.sceneItems.filter(i => !i.isGroup).map((i) => ({
-        name: `[${sceneName}] ${i.sourceName}`,
+        name: i.sourceName as string,
         type: i.inputKind?.toString() || null,
       }));
 
@@ -132,7 +132,7 @@ export const useObs = () => {
         result[sceneName] = [
           ...result[sceneName],
           ...group.sceneItems.filter(i => !i.isGroup).map((i) => ({
-            name: `[${sceneName}] ${i.sourceName}`,
+            name: i.sourceName as string,
             type: i.inputKind?.toString() || null,
           })),
         ];
