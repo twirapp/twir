c670e4a6e24904c6477cc094fbd2db048fa23639
Satont <satontworldwide@gmail.com>
2023-05-01 09:40:57 +0300

    fix(scheduler): correctly stat online user, and improve code readability

--- apps/scheduler/internal/timers/online.go
+++ apps/scheduler/internal/timers/online.go
@@ -27,77 +27,97 @@ func NewOnlineUsers(ctx context.Context, services *types.Services) {
 				ticker.Stop()
 				break
 			case <-ticker.C:
-				var streams []model.ChannelsStreams
+				var streams []*model.ChannelsStreams
 				err := services.Gorm.Find(&streams).Error
 				if err != nil {
 					zap.S().Error(err)
-				} else {
-					wg := &sync.WaitGroup{}
-					for _, stream := range streams {
-						wg.Add(1)
-						twitchClient, err := twitch.NewUserClient(stream.UserId, *services.Config, services.Grpc.Tokens)
-						if err != nil {
-							zap.S().Error(err)
-						} else {
-							go func(userId string) {
-								defer wg.Done()
-								var chatters []helix.ChatChatter
-								cursor := ""
-								for {
-									req, err := twitchClient.GetChannelChatChatters(&helix.GetChatChattersParams{
-										BroadcasterID: stream.UserId,
-										ModeratorID:   stream.UserId,
-										After:         cursor,
-									})
-									if err != nil {
-										zap.S().Error(err)
-									} else {
-										chatters = append(chatters, req.Data.Chatters...)
-										if req.Data.Pagination.Cursor == "" {
-											break
-										}
-									}
-								}
-
-								err = services.Gorm.Transaction(func(tx *gorm.DB) error {
-									tx.Delete(&model.UsersOnline{}).Where(`"channelId" = ?`, userId)
-									for _, chatter := range chatters {
-										var user model.Users
-										err := tx.Where("id = ?", chatter.UserID).First(&user).Error
-										if err != nil {
-											if err == gorm.ErrRecordNotFound {
-												user = model.Users{
-													ID:     chatter.UserID,
-													ApiKey: uuid.New().String(),
-												}
-												err = tx.Create(&user).Error
-												if err != nil {
-													return err
-												}
-											} else {
-												return err
-											}
-										} else {
-											tx.Save(&model.UsersOnline{
-												ID:        uuid.New().String(),
-												ChannelId: userId,
-												UserId:    null.StringFrom(chatter.UserID),
-												UserName:  null.StringFrom(chatter.UserLogin),
-											})
-										}
-									}
-									return nil
-								})
-
-								if err != nil {
-									zap.S().Error(err)
-								}
-							}(stream.UserId)
-						}
-					}
-					wg.Wait()
+					return
 				}
 
+				streamsWg := &sync.WaitGroup{}
+				for _, stream := range streams {
+					streamsWg.Add(1)
+
+					twitchClient, err := twitch.NewUserClient(stream.UserId, *services.Config, services.Grpc.Tokens)
+					if err != nil {
+						zap.S().Error(err)
+						continue
+					}
+
+					go func(broadcasterId string) {
+						defer streamsWg.Done()
+						var chatters []helix.ChatChatter
+						cursor := ""
+						for {
+							req, err := twitchClient.GetChannelChatChatters(&helix.GetChatChattersParams{
+								BroadcasterID: stream.UserId,
+								ModeratorID:   stream.UserId,
+								After:         cursor,
+							})
+							if err != nil {
+								zap.S().Error(err)
+							} else {
+								chatters = append(chatters, req.Data.Chatters...)
+								if req.Data.Pagination.Cursor == "" {
+									break
+								}
+							}
+						}
+
+						err = services.Gorm.Transaction(func(tx *gorm.DB) error {
+							err = tx.Where(`"channelId" = ?`, broadcasterId).Delete(&model.UsersOnline{}).Error
+
+							if err != nil {
+								return err
+							}
+
+							for _, chatter := range chatters {
+								user := &model.Users{}
+								err = tx.Where("id = ?", chatter.UserID).Find(user).Error
+								if err != nil {
+									return err
+								}
+
+								if user.ID == "" {
+									user = &model.Users{
+										ID:     chatter.UserID,
+										ApiKey: uuid.New().String(),
+										Stats: &model.UsersStats{
+											ID:        uuid.New().String(),
+											ChannelID: broadcasterId,
+											UserID:    chatter.UserID,
+										},
+									}
+									err = tx.Create(user).Error
+									if err != nil {
+										return err
+									}
+
+									continue
+								}
+
+								err = tx.Save(&model.UsersOnline{
+									ID:        uuid.New().String(),
+									ChannelId: broadcasterId,
+									UserId:    null.StringFrom(chatter.UserID),
+									UserName:  null.StringFrom(chatter.UserLogin),
+								}).Error
+
+								if err != nil {
+									return err
+								}
+
+								continue
+							}
+							return nil
+						})
+
+						if err != nil {
+							zap.S().Error(err)
+						}
+					}(stream.UserId)
+				}
+				streamsWg.Wait()
 			}
 		}
 	}()
