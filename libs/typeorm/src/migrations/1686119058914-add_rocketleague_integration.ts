3e3500f9c420603f83888637532645d24a257eb3
danilluk1 <danluki02@yandex.ru>
2023-06-07 17:01:07 +0500

    feat: change to rl migration

--- libs/typeorm/src/migrations/1686119058914-add_rocketleague_integration.ts
+++ libs/typeorm/src/migrations/1686119058914-add_rocketleague_integration.ts
@@ -8,6 +8,7 @@ export class addRocketleagueIntegration1686119058914 implements MigrationInterfa
         await queryRunner.query(`CREATE TYPE "public"."integrations_service_enum" AS ENUM('LASTFM', 'VK', 'FACEIT', 'SPOTIFY', 'DONATIONALERTS', 'STREAMLABS', 'DONATEPAY', 'DONATELLO', 'VALORANT', 'DONATE_STREAM', 'ROCKETLEAGUE')`);
         await queryRunner.query(`ALTER TABLE "integrations" ALTER COLUMN "service" TYPE "public"."integrations_service_enum" USING "service"::"text"::"public"."integrations_service_enum"`);
         await queryRunner.query(`DROP TYPE "public"."integrations_service_enum_old"`);
+        await queryRunner.query(`INSERT INTO "integrations" ("service") VALUES ($1)`, ['ROCKETLEAGUE']);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
