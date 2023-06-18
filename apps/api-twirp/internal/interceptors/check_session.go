44c30458a43878fd0672a335fa404544c67091c7
Satont <satontworldwide@gmail.com>
2023-06-18 14:58:08 +0300

    upd

--- apps/api-twirp/internal/interceptors/check_session.go
+++ apps/api-twirp/internal/interceptors/check_session.go
@@ -8,6 +8,7 @@ import (
 func (s *Service) SessionInterceptor(next twirp.Method) twirp.Method {
 	return func(ctx context.Context, req interface{}) (interface{}, error) {
 		user := s.sessionManager.Get(ctx, "user")
+		// TODO: check api key and attach user to context from api key
 		if user == nil {
 			return nil, twirp.Unauthenticated.Error("not authenticated")
 		}
