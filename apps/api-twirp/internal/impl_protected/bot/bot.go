ffeaecaceb15183c1674bcd03c688797c77882de
Satont <satontworldwide@gmail.com>
2023-06-19 06:45:03 +0300

    upd

--- apps/api-twirp/internal/impl_protected/bot/bot.go
+++ apps/api-twirp/internal/impl_protected/bot/bot.go
@@ -2,13 +2,17 @@ package bot
 
 import (
 	"context"
-	"fmt"
-	"github.com/kr/pretty"
+	"github.com/nicklaw5/helix/v2"
+	"github.com/samber/lo"
 	"github.com/satont/tsuwari/apps/api-twirp/internal/impl_deps"
+	model "github.com/satont/tsuwari/libs/gomodels"
 	"github.com/satont/tsuwari/libs/grpc/generated/api/bots"
 	"github.com/satont/tsuwari/libs/grpc/generated/api/meta"
+	"github.com/satont/tsuwari/libs/twitch"
+	"github.com/twitchtv/twirp"
 	"google.golang.org/protobuf/types/known/emptypb"
-	"time"
+	"net/http"
+	"sync"
 )
 
 type Bot struct {
@@ -16,15 +20,68 @@ type Bot struct {
 }
 
 func (c *Bot) BotInfo(ctx context.Context, meta *meta.BaseRequestMeta) (*bots.BotInfo, error) {
-	dashboard := ctx.Value("dashboardId")
-	pretty.Println(dashboard)
+	dashboardId, ok := ctx.Value("dashboardId").(string)
+	if !ok || dashboardId == "" {
+		return nil, twirp.NewError(twirp.ErrorCode(http.StatusBadRequest), "no dashboardId provided")
+	}
 
-	return &bots.BotInfo{
-		IsMod:   false,
-		BotId:   "123",
-		BotName: fmt.Sprintf("%v", time.Now().UnixMilli()),
-		Enabled: false,
-	}, nil
+	dbUser := &model.Users{}
+	err := c.Db.WithContext(ctx).Where("id = ?", dashboardId).Preload("Channel").Find(dbUser).Error
+	if err != nil {
+		return nil, err
+	}
+	if dbUser.ID == "" || dbUser.Channel == nil {
+		return nil, twirp.NotFoundError("user not found")
+	}
+
+	twitchClient, err := twitch.NewUserClient(dashboardId, *c.Config, c.Grpc.Tokens)
+	if err != nil {
+		return nil, err
+	}
+
+	var wg sync.WaitGroup
+	result := &bots.BotInfo{}
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+
+		if dashboardId == dbUser.Channel.BotID {
+			result.IsMod = true
+			return
+		}
+
+		mods, err := twitchClient.GetModerators(&helix.GetModeratorsParams{
+			BroadcasterID: dashboardId,
+			UserIDs:       []string{dbUser.Channel.BotID},
+		})
+		if err != nil {
+			return
+		}
+
+		result.IsMod = lo.If(len(mods.Data.Moderators) == 0, false).Else(true)
+	}()
+
+	go func() {
+		defer wg.Done()
+		infoReq, err := twitchClient.GetUsers(&helix.UsersParams{
+			IDs: []string{dbUser.Channel.BotID},
+		})
+		if err != nil {
+			return
+		}
+
+		if len(infoReq.Data.Users) == 0 {
+			return
+		}
+
+		result.BotId = infoReq.Data.Users[0].ID
+		result.BotName = infoReq.Data.Users[0].Login
+	}()
+
+	wg.Wait()
+
+	return result, nil
 }
 
 func (c *Bot) BotJoinPart(ctx context.Context, request *bots.BotJoinPartRequest) (*emptypb.Empty, error) {
