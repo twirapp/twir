228be87818e4d8cda9ce077011235da45b42eae0
Satont <satontworldwide@gmail.com>
2023-06-20 11:35:32 +0300

    upd

--- apps/api-twirp/internal/impl_protected/integrations/donatello.go
+++ apps/api-twirp/internal/impl_protected/integrations/donatello.go
@@ -2,11 +2,18 @@ package integrations
 
 import (
 	"context"
+	model "github.com/satont/tsuwari/libs/gomodels"
 	"github.com/satont/tsuwari/libs/grpc/generated/api/integrations_donatello"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
-func (c *Integrations) IntegrationsDonatelloGet(ctx context.Context, empty *emptypb.Empty) (*integrations_donatello.GetResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func (c *Integrations) IntegrationsDonatelloGet(ctx context.Context, _ *emptypb.Empty) (*integrations_donatello.GetResponse, error) {
+	integration, err := c.getChannelIntegrationByService(ctx, model.IntegrationServiceDonatello)
+	if err != nil {
+		return nil, err
+	}
+
+	return &integrations_donatello.GetResponse{
+		IntegrationId: integration.ID,
+	}, nil
 }
