6c5958da6b2560bca69fd0d80556686c08a353bb
Satont <satontworldwide@gmail.com>
2023-06-20 11:27:36 +0300

    upd

--- apps/api-twirp/internal/impl_protected/integrations/donate_stream.go
+++ apps/api-twirp/internal/impl_protected/integrations/donate_stream.go
@@ -2,16 +2,36 @@ package integrations
 
 import (
 	"context"
+	model "github.com/satont/tsuwari/libs/gomodels"
 	"github.com/satont/tsuwari/libs/grpc/generated/api/integrations_donate_stream"
 	"google.golang.org/protobuf/types/known/emptypb"
+	"time"
 )
 
-func (c *Integrations) IntegrationsDonateStreamGet(ctx context.Context, empty *emptypb.Empty) (*integrations_donate_stream.DonateStreamResponse, error) {
-	//TODO implement me
-	panic("implement me")
+const donateStreamConfirmationKey = "donate_stream_confirmation"
+
+func (c *Integrations) IntegrationsDonateStreamGet(ctx context.Context, _ *emptypb.Empty) (*integrations_donate_stream.DonateStreamResponse, error) {
+	integration, err := c.getChannelIntegrationByService(ctx, model.IntegrationServiceDonateStream)
+	if err != nil {
+		return nil, err
+	}
+
+	return &integrations_donate_stream.DonateStreamResponse{
+		IntegrationId: integration.ID,
+	}, nil
 }
 
 func (c *Integrations) IntegrationsDonateStreamPostSecret(ctx context.Context, request *integrations_donate_stream.DonateStreamPostSecretRequest) (*emptypb.Empty, error) {
-	//TODO implement me
-	panic("implement me")
+	integration, err := c.getChannelIntegrationByService(ctx, model.IntegrationServiceDonateStream)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = c.Redis.
+		Set(ctx, donateStreamConfirmationKey+integration.IntegrationID, request.Secret, 1*time.Hour).
+		Err(); err != nil {
+		return nil, err
+	}
+
+	return &emptypb.Empty{}, nil
 }
--- apps/api-twirp/internal/impl_protected/integrations/integrations.go
+++ apps/api-twirp/internal/impl_protected/integrations/integrations.go
@@ -1,9 +1,28 @@
 package integrations
 
 import (
+	"context"
 	"github.com/satont/tsuwari/apps/api-twirp/internal/impl_deps"
+	model "github.com/satont/tsuwari/libs/gomodels"
 )
 
 type Integrations struct {
 	*impl_deps.Deps
+}
+
+func (c *Integrations) getChannelIntegrationByService(
+	ctx context.Context,
+	service model.IntegrationService,
+) (*model.ChannelsIntegrations, error) {
+	integration := &model.Integrations{}
+	if err := c.Db.WithContext(ctx).Where("service = ?", service).First(integration).Error; err != nil {
+		return nil, err
+	}
+
+	channelIntegration := &model.ChannelsIntegrations{}
+	if err := c.Db.WithContext(ctx).Where(`"integrationId" = ?`, integration.ID).First(channelIntegration).Error; err != nil {
+		return nil, err
+	}
+
+	return channelIntegration, nil
 }
