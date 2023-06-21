3351c6ad3aa9270dc9e139805eaa77353488b557
Satont <satontworldwide@gmail.com>
2023-06-21 05:19:28 +0300

    upd

--- frontend/overlays/src/pages/tts.tsx
+++ frontend/overlays/src/pages/tts.tsx
@@ -1,7 +1,7 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import useWebSocket from 'react-use-websocket';
 
-import { useWebSocket } from '../hooks/websocket';
 
 declare global {
   interface Window {
@@ -10,52 +10,40 @@ declare global {
 }
 
 export const TTS: React.FC = () => {
+	const [url, setUrl] = useState<string | null>(null);
   const { apiKey } = useParams();
-  const { ws, close, connect } = useWebSocket(apiKey);
+  const { lastMessage } = useWebSocket(url, {
+		shouldReconnect: () => true,
+		onOpen: () => console.log('Opened'),
+		reconnectInterval: 500,
+	});
+
+	useEffect(() => {
+		if (!lastMessage) return;
+		const parsedData = JSON.parse(lastMessage.data);
+		console.log(parsedData);
+
+		if (parsedData.eventName === 'say') {
+			queueRef.current.push(parsedData.data);
+
+			if (queueRef.current.length === 1) {
+				processQueue();
+			}
+		}
+
+		if (parsedData.eventName === 'skip') {
+			currentAudioBuffer.current?.stop();
+		}
+	}, [lastMessage]);
+
+	useEffect(() => {
+		if (!apiKey) return;
+
+		setUrl(`${window.location.protocol === 'https:' ? 'wss' : 'ws'}://${window.location.host}/socket/tts?apiKey=${apiKey}`);
+	}, [apiKey]);
 
   const queueRef = useRef<Array<Record<string, string>>>([]);
   const currentAudioBuffer = useRef<AudioBufferSourceNode | null>(null);
-
-  useEffect(() => {
-    if (!ws) return;
-
-    const onOpen = () => {
-      console.log('connected');
-    };
-
-    const onMessage = (message: MessageEvent) => {
-      const parsedData = JSON.parse(message.data);
-      console.log(parsedData);
-
-      if (parsedData.eventName === 'say') {
-        queueRef.current.push(parsedData.data);
-
-        if (queueRef.current.length === 1) {
-          processQueue();
-        }
-      }
-
-      if (parsedData.eventName === 'skip') {
-        currentAudioBuffer.current?.stop();
-      }
-    };
-
-    const onClose = () => {
-			console.log('disconnected');
-      connect();
-    };
-
-    ws.addEventListener('open', onOpen);
-    ws.addEventListener('message', onMessage);
-    ws.addEventListener('close', onClose);
-
-    return () => {
-      ws.removeEventListener('open', onOpen);
-      ws.removeEventListener('message', onMessage);
-      ws.removeEventListener('close', onClose);
-      close();
-    };
-  }, [ws]);
 
   const processQueue = useCallback(async () => {
     if (queueRef.current.length === 0) {
