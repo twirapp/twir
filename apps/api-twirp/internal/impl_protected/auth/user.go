3b049388557c89bf8795b36e04565765566244ed
Satont <satontworldwide@gmail.com>
2023-07-04 01:21:28 +0300

    kruto

--- apps/api-twirp/internal/impl_protected/auth/user.go
+++ apps/api-twirp/internal/impl_protected/auth/user.go
@@ -28,3 +28,9 @@ func (c *Auth) AuthUserProfile(ctx context.Context, _ *emptypb.Empty) (*auth.Pro
 		SelectedDashboardId: selectedDashboardId,
 	}, nil
 }
+
+func (c *Auth) AuthSetDashboard(ctx context.Context, req *auth.SetDashboard) (*emptypb.Empty, error) {
+	c.SessionManager.Put(ctx, "dashboardId", req.DashboardId)
+
+	return &emptypb.Empty{}, nil
+}
