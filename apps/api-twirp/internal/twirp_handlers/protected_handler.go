aac1974f2f404c3bde86d825cf1f927c3f6c22bc
Satont <satontworldwide@gmail.com>
2023-07-06 19:33:58 +0300

    upd

--- apps/api-twirp/internal/twirp_handlers/protected_handler.go
+++ apps/api-twirp/internal/twirp_handlers/protected_handler.go
@@ -7,9 +7,6 @@ import (
 	"github.com/satont/twir/apps/api-twirp/internal/wrappers"
 	"github.com/satont/twir/libs/grpc/generated/api"
 	"github.com/satont/twir/libs/grpc/generated/api/bots"
-	"github.com/satont/twir/libs/grpc/generated/api/commands"
-	"github.com/satont/twir/libs/grpc/generated/api/events"
-	"github.com/satont/twir/libs/grpc/generated/api/greetings"
 	"github.com/satont/twir/libs/grpc/generated/tokens"
 	"github.com/twitchtv/twirp"
 	"go.uber.org/fx"
@@ -38,33 +35,78 @@ func NewProtected(opts Opts) IHandler {
 					return &bots.BotInfo{}
 				},
 			},
-			interceptors.CacheOpts{
-				CacheMethod:       "CommandsGetAll",
-				CacheDuration:     24 * time.Hour,
-				ClearMethods:      []string{"CommandsCreate", "CommandsDelete", "CommandsUpdate", "CommandsEnableOrDisable"},
-				WithChannelHeader: true,
-				NewCastTo: func() any {
-					return &commands.CommandsGetAllResponse{}
-				},
-			},
-			interceptors.CacheOpts{
-				CacheMethod:       "EventsGetAll",
-				CacheDuration:     24 * time.Hour,
-				ClearMethods:      []string{"EventsCreate", "EventsDelete", "EventsUpdate", "EventsEnableOrDisable"},
-				WithChannelHeader: true,
-				NewCastTo: func() any {
-					return &events.GetAllResponse{}
-				},
-			},
-			interceptors.CacheOpts{
-				CacheMethod:       "GreetingsGetAll",
-				CacheDuration:     24 * time.Hour,
-				ClearMethods:      []string{"GreetingsCreate", "GreetingsDelete", "GreetingsUpdate", "GreetingsEnableOrDisable"},
-				WithChannelHeader: true,
-				NewCastTo: func() any {
-					return &greetings.GetAllResponse{}
-				},
-			},
+			// interceptors.CacheOpts{
+			// 	CacheMethod:       "CommandsGetAll",
+			// 	CacheDuration:     24 * time.Hour,
+			// 	ClearMethods:      []string{"CommandsCreate", "CommandsDelete", "CommandsUpdate", "CommandsEnableOrDisable"},
+			// 	WithChannelHeader: true,
+			// 	NewCastTo: func() any {
+			// 		return &commands.CommandsGetAllResponse{}
+			// 	},
+			// },
+			// interceptors.CacheOpts{
+			// 	CacheMethod:       "EventsGetAll",
+			// 	CacheDuration:     24 * time.Hour,
+			// 	ClearMethods:      []string{"EventsCreate", "EventsDelete", "EventsUpdate", "EventsEnableOrDisable"},
+			// 	WithChannelHeader: true,
+			// 	NewCastTo: func() any {
+			// 		return &events.GetAllResponse{}
+			// 	},
+			// },
+			// interceptors.CacheOpts{
+			// 	CacheMethod:       "GreetingsGetAll",
+			// 	CacheDuration:     24 * time.Hour,
+			// 	ClearMethods:      []string{"GreetingsCreate", "GreetingsDelete", "GreetingsUpdate", "GreetingsEnableOrDisable"},
+			// 	WithChannelHeader: true,
+			// 	NewCastTo: func() any {
+			// 		return &greetings.GetAllResponse{}
+			// 	},
+			// },
+			// interceptors.CacheOpts{
+			// 	CacheMethod:       "KeywordsGetAll",
+			// 	CacheDuration:     24 * time.Hour,
+			// 	ClearMethods:      []string{"KeywordsCreate", "KeywordsDelete", "KeywordsUpdate", "KeywordsEnableOrDisable"},
+			// 	WithChannelHeader: true,
+			// 	NewCastTo: func() any {
+			// 		return &keywords.GetAllResponse{}
+			// 	},
+			// },
+			// interceptors.CacheOpts{
+			// 	CacheMethod:       "TimersGetAll",
+			// 	CacheDuration:     24 * time.Hour,
+			// 	ClearMethods:      []string{"TimersCreate", "TimersDelete", "TimersUpdate", "TimersEnableOrDisable"},
+			// 	WithChannelHeader: true,
+			// 	NewCastTo: func() any {
+			// 		return &timers.GetResponse{}
+			// 	},
+			// },
+			// interceptors.CacheOpts{
+			// 	CacheMethod:       "VariablesGetAll",
+			// 	CacheDuration:     24 * time.Hour,
+			// 	ClearMethods:      []string{"VariablesCreate", "VariablesDelete", "VariablesUpdate", "VariablesEnableOrDisable"},
+			// 	WithChannelHeader: true,
+			// 	NewCastTo: func() any {
+			// 		return &variables.GetAllResponse{}
+			// 	},
+			// },
+			// interceptors.CacheOpts{
+			// 	CacheMethod:       "CommandsGroupGetAll",
+			// 	CacheDuration:     24 * time.Hour,
+			// 	ClearMethods:      []string{"CommandsGroupCreate", "CommandsGroupDelete", "CommandsGroupUpdate", "CommandsGroupEnableOrDisable"},
+			// 	WithChannelHeader: true,
+			// 	NewCastTo: func() any {
+			// 		return &commands_group.GetAllResponse{}
+			// 	},
+			// },
+			// interceptors.CacheOpts{
+			// 	CacheMethod:       "RolesGetAll",
+			// 	CacheDuration:     24 * time.Hour,
+			// 	ClearMethods:      []string{"RolesGroupCreate", "RolesGroupDelete", "RolesGroupUpdate", "RolesGroupEnableOrDisable"},
+			// 	WithChannelHeader: true,
+			// 	NewCastTo: func() any {
+			// 		return &roles.GetAllResponse{}
+			// 	},
+			// },
 		)),
 	)
 
