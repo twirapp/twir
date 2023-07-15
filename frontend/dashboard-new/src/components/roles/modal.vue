e773fe87324e7f45747270eff4bbf79393d5856d
Satont <satontworldwide@gmail.com>
2023-07-16 02:27:02 +0300

    kruto

--- frontend/dashboard-new/src/components/roles/modal.vue
+++ frontend/dashboard-new/src/components/roles/modal.vue
@@ -4,12 +4,18 @@ import {
 	NInput,
 	NForm,
 	NFormItem,
+	NDivider,
+	NGrid,
+	NGridItem,
+	NInputNumber,
+	NCheckbox,
+	NCheckboxGroup,
 } from 'naive-ui';
-import { ref } from 'vue';
+import { ref, onMounted, toRaw } from 'vue';
 
-import type { EditableRole } from '@/components/roles/types.js';
+import { type EditableRole, permissions } from '@/components/roles/types.js';
 
-defineProps<{
+const props = defineProps<{
 	role?: EditableRole | null
 }>();
 defineEmits<{
@@ -28,6 +34,11 @@ const formValue = ref<EditableRole>({
 		requiredWatchTime: 0,
 	},
 });
+
+onMounted(() => {
+	if (!props.role) return;
+	formValue.value = structuredClone(toRaw(props.role));
+});
 </script>
 
 <template>
@@ -35,5 +46,48 @@ const formValue = ref<EditableRole>({
     <n-form-item label="Name">
       <n-input v-model:value="formValue.name" />
     </n-form-item>
+
+    <n-divider>Access by stats</n-divider>
+
+    <n-grid :cols="12" :x-gap="5">
+      <n-grid-item :span="6">
+        <n-form-item label="Required watch time">
+          <n-input-number
+            v-model:value="formValue.settings!.requiredWatchTime"
+            :min="0" :max="99999999"
+          />
+        </n-form-item>
+      </n-grid-item>
+
+      <n-grid-item :span="6">
+        <n-form-item label="Required messages">
+          <n-input-number
+            v-model:value="formValue.settings!.requiredMessages"
+            :min="0"
+            :max="99999999"
+          />
+        </n-form-item>
+      </n-grid-item>
+
+      <n-grid-item :span="6">
+        <n-form-item label="Required used channels points">
+          <n-input-number
+            v-model:value="formValue.settings!.requiredUserChannelPoints"
+            :min="0"
+            :max="999999999999"
+          />
+        </n-form-item>
+      </n-grid-item>
+    </n-grid>
+
+    <n-divider>Permissions</n-divider>
+
+    <n-checkbox-group v-model:value="formValue.permissions">
+      <n-grid :cols="12" :x-gap="5">
+        <n-grid-item v-for="(permission) of Object.entries(permissions)" :key="permission[0]" :span="6">
+          <n-checkbox :value="permission[0]" :label="permission[1]" />
+        </n-grid-item>
+      </n-grid>
+    </n-checkbox-group>
   </n-form>
 </template>
--- frontend/dashboard-new/src/components/roles/types.ts
+++ frontend/dashboard-new/src/components/roles/types.ts
@@ -1,3 +1,34 @@
 import type { CreateRequest } from '@twir/grpc/generated/api/api/roles';
 
 export type EditableRole = CreateRequest & { id?: string };
+
+export const permissions = {
+	CAN_ACCESS_DASHBOARD: 'Can access dashboard',
+
+	UPDATE_CHANNEL_TITLE: 'Can update channel title',
+	UPDATE_CHANNEL_CATEGORY: 'Can update channel category',
+
+	VIEW_COMMANDS: 'Can view commands',
+	MANAGE_COMMANDS: 'Can manage commands',
+
+	VIEW_KEYWORDS: 'Can view keywords',
+	MANAGE_KEYWORDS: 'Can manage keywords',
+
+	VIEW_TIMERS: 'Can view timers',
+	MANAGE_TIMERS: 'Can manage timers',
+
+	VIEW_INTEGRATIONS: 'Can view integrations',
+	MANAGE_INTEGRATIONS: 'Can manage integrations',
+
+	VIEW_SONG_REQUESTS: 'Can view song requests',
+	MANAGE_SONG_REQUESTS: 'Can manage song requests',
+
+	VIEW_MODERATION: 'Can view moderation settings',
+	MANAGE_MODERATION: 'Can manage moderation settings',
+
+	VIEW_VARIABLES: 'Can view variables',
+	MANAGE_VARIABLES: 'Can manage variables',
+
+	VIEW_GREETINGS: 'Can view greetings',
+	MANAGE_GREETINGS: 'Can manage greetings',
+};
