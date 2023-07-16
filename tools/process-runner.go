9108ac3473754e8246ff71fd32b7df7c5ecd85c1
Satont <satontworldwide@gmail.com>
2023-07-16 03:24:43 +0300

    kruto

--- tools/process-runner.go
+++ tools/process-runner.go
@@ -33,7 +33,7 @@ func main() {
 		{Stack: "go", Name: "watched", Port: servers.WATCHED_SERVER_PORT},
 		{Stack: "go", Name: "websockets", Port: servers.WEBSOCKET_SERVER_PORT},
 		{Stack: "node", Name: "ytsr", Port: servers.YTSR_SERVER_PORT},
-		{Stack: "go", Name: "api-twirp", Port: 3002},
+		{Stack: "go", Name: "api", Port: 3002},
 		{Stack: "go", Name: "scheduler"},
 		{Stack: "frontend", Name: "dashboard", Port: 3006},
 		{Stack: "frontend", Name: "landing", Port: 3005},
