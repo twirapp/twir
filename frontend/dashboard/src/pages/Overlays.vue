48ff2a39aef2b094cbea189ef231bb7a1e095cca
Artem Melnyk <melkam464@gmail.com>
2023-07-19 01:30:54 +0000

    upd

--- frontend/dashboard/src/pages/Overlays.vue
+++ frontend/dashboard/src/pages/Overlays.vue
@@ -1,23 +1,27 @@
-<script setup lang='ts'>
-import {
-  NGrid,
-  NGridItem,
-} from 'naive-ui';
+<script setup lang="ts">
+import { NGrid, NGridItem } from 'naive-ui';
 
 import OBS from '@/components/overlays/obs.vue';
 import TTS from '@/components/overlays/tts.vue';
 </script>
 
 <template>
-  <div style="display: flex; align-items: center; justify-content: center;">
-    <n-grid cols="1 s:1 m:2 l:2" :x-gap="24" :y-gap="10" responsive="screen">
-      <n-grid-item :span="1">
-        <TTS />
-      </n-grid-item>
-      <n-grid-item :span="1">
-        <OBS />
-      </n-grid-item>
-    </n-grid>
-  </div>
+	<div
+		style="
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			max-width: 1000px;
+			margin: 0 auto;
+		"
+	>
+		<n-grid cols="1 m:2" :x-gap="16" :y-gap="16" responsive="screen">
+			<n-grid-item :span="1">
+				<TTS />
+			</n-grid-item>
+			<n-grid-item :span="1">
+				<OBS />
+			</n-grid-item>
+		</n-grid>
+	</div>
 </template>
-
