34293d18f1380aff44929f303242ee33a70d446d
Artem Melnyk <melkam464@gmail.com>
2023-07-29 17:38:09 +0200

    upd

--- frontend/landing/tailwind.config.js
+++ frontend/landing/tailwind.config.js
@@ -1,5 +1,9 @@
 export default {
-	content: [
-		'./**/*.{html,js,astro,vue,ts,tsx}',
-	],
+	content: ['./**/*.{html,js,astro,vue,ts,tsx}'],
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+		},
+	},
 };
