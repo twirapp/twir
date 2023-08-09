78cc1ee759bd6a771962075e04c44f26396248d8
Satont <satontworldwide@gmail.com>
2023-08-09 06:55:39 +0300

    chore(dashboard): add pwa

---
+++ frontend/dashboard/pwa-assets.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig, minimalPreset as preset } from '@vite-pwa/assets-generator/config';
+
+export default defineConfig({
+	preset,
+	images: [
+		'public/TwirInCircle.svg',
+	],
+});
