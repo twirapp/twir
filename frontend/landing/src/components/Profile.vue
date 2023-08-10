bf568a6bf5aa82cc1f77542f2ee6ad95e3595e1b
Satont <satontworldwide@gmail.com>
2023-08-10 19:30:21 +0300

    kruto

--- frontend/landing/src/components/Profile.vue
+++ frontend/landing/src/components/Profile.vue
@@ -1,57 +1,58 @@
 <script lang="ts" setup>
-import { Profile } from '@twir/grpc/generated/api/api/auth';
-import { computed, ref } from 'vue';
+import { useStore } from '@nanostores/vue';
+import { computed, onMounted, ref } from 'vue';
 
-import { browserProtectedClient, browserUnProtectedClient } from '../api/twirp-browser.js';
 import Loader from '../icons/Loader.svg?component';
 import TwitchIcon from '../icons/TwitchLogo.svg?component';
+import { profileStore, authLinkStore } from '../stores/auth.js';
 
-const profile = ref<Profile | null>(null);
-const authLink = ref<string | null>(null);
+const profile = useStore(profileStore);
+const authLink = useStore(authLinkStore);
 
 const isLoading = computed(
-	() => authLink.value === null && profile.value === null && isError.value === false,
+    () => authLinkStore.value === null && profileStore.value === null && isError.value === false,
 );
 const isError = ref(false);
 const error = ref<string | null>(null);
 
-browserUnProtectedClient
-	.authGetLink({
-		state: window.btoa(window.location.origin),
-	})
-	.then((r) => (authLink.value = r.response.link))
-	.catch((err) => {
-		isError.value = true;
-		console.error(err);
-		error.value = String(err);
-	});
+onMounted(async () => {
+  if (typeof window === 'undefined') return;
+  const { browserProtectedClient, browserUnProtectedClient } = await import('../api/twirp-browser.js');
 
-browserProtectedClient
-	.authUserProfile({})
-	.then((r) => (profile.value = r.response))
-	.catch((err) => {
-		isError.value = true;
-		console.error(err);
-		error.value = String(err);
-	});
+  browserUnProtectedClient
+		.authGetLink({
+			state: window.btoa(window.location.origin),
+		})
+		.then((r) => authLinkStore.set(r.response.link))
+		.catch((err) => {
+			isError.value = true;
+			console.error(err);
+			error.value = String(err);
+		});
+
+  browserProtectedClient
+		.authUserProfile({})
+		.then((r) => profileStore.set(r.response))
+		.catch((err) => {
+			isError.value = true;
+			console.error(err);
+			error.value = String(err);
+		});
+});
 </script>
 
 <template>
 	<Loader v-if="isLoading" class="w-6 h-6 animate-spin stroke-white/75 stroke-[1.5] m-2" />
-	<!--	<span v-else-if="isError" class="text-[#FD6675] inline-flex items-center gap-x-2 max-w-sm">-->
-	<!--		<ErrorIcon class="fill-[#FD6675] h-5 w-5 flex-shrink-0 items-center" />-->
-	<!--		<span class="line-clamp-1">{{ error }}</span>-->
-	<!--	</span>-->
-	<a v-if="profile" href="/dashboard" class="text-white/75 inline-flex gap-x-3 items-center">
-		{{ profile.displayName }}
-		<img :src="profile.avatar" class="rounded-full w-9 h-9 object-cover" />
-	</a>
 	<a
-		v-else
+		v-else-if="!profile && !isLoading"
 		class="flex flex-row px-4 py-2 items-center gap-2 bg-[#5D58F5] text-white rounded-lg font-medium cursor-pointer hover:bg-[#6964FF]"
 		:href="authLink"
 	>
 		Login
 		<TwitchIcon class="w-5 h-5 fill-white" />
+	</a>
+	<a v-else href="/dashboard" class="text-white/75 inline-flex gap-x-3 items-center">
+		{{ profile.displayName }}
+		<img :src="profile.avatar" class="rounded-full w-9 h-9 object-cover" />
 	</a>
 </template>
---
+++ frontend/landing/src/components/index/Reviews.vue
@@ -0,0 +1,134 @@
+<script lang="ts" setup>
+import { Fade, AutoPlay } from '@egjs/flicking-plugins';
+import Flicking from '@egjs/vue3-flicking';
+import { type TwitchUser } from '@twir/grpc/generated/api/api/twitch';
+import { onMounted, ref, computed } from 'vue';
+
+import RatingStarFilled from '../../assets/rating-star-filled.svg?url';
+
+const plugins = [new Fade(), new AutoPlay({ duration: 2000, direction: 'NEXT', stopOnHover: true })];
+
+const reviews = [
+{
+    id: '139336353',
+    username: '7ssk7',
+		roles: ['Streamer', 'pro player'],
+    comment: `I've been using the bot for a few years now, and I'm happy with it. There are convenient integrations with Volaroant, Spotify. It's easy to add and remove commands from the chat. I am pleased with its stability and functionality.`,
+    avatarUrl:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/66cb7060-1a8a-4fca-9ccd-f760b70af636-profile_image-70x70.png',
+    rating: 5,
+  },
+  {
+    id: '104435562',
+    username: 'qrushcsgo',
+		roles: ['Streamer', 'player'],
+    comment: `Good, handy bot for streaming. Easy to set all the settings and everything works clearly. Recommended 👍`,
+    avatarUrl:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/a477bccc-9b23-44d7-a379-fe64f67898c3-profile_image-70x70.png',
+    rating: 4,
+  },
+	{
+		id: '155644238',
+		username: 'le_xot',
+		roles: ['Streamer'],
+		comment: 'Twir combines a simple and clear interface, extensive customization and integration options, making this bot an indispensable assistant on my broadcasts.',
+		avatarUrl: 'https://static-cdn.jtvnw.net/jtv_user_pictures/423e40e6-9534-46ac-9ed8-5714657dd03b-profile_image-70x70.png',
+		rating: 5,
+	},
+];
+
+const twitchUsers = ref<TwitchUser[]>([]);
+
+onMounted(async () => {
+	if (typeof window === 'undefined') return;
+  const { browserUnProtectedClient } = await import('../../api/twirp-browser.js');
+
+	const request = await browserUnProtectedClient.twitchGetUsers({
+		ids: reviews.map(r => r.id),
+		names: [],
+	});
+	twitchUsers.value = request.response.users;
+});
+
+const mappedReviews = computed(() => {
+	if (!twitchUsers.value.length) return reviews;
+
+	return reviews.map(r => {
+		const twitchUser = twitchUsers.value.find(u => u.id === r.id);
+
+		return {
+			...r,
+			avatarUrl: twitchUser.profileImageUrl ?? r.avatarUrl,
+			username: twitchUser.login ?? r.username,
+		};
+	});
+});
+</script>
+
+<template>
+	<div class="container py-24">
+		<div class="flex justify-between">
+			<h1 class="text-white text-4xl font-bold">
+				Reviews from streamers and viewers
+			</h1>
+
+			<button class="inline-flex px-6 py-3 font-semibold text-white rounded-lg bg-[#5D58F5] hover:bg-[#6964FF] transition-[background,box-shadow] text-lg focus-visible:outline-none focus-visible:ring-[#5D58F5]/50 focus-visible:ring-4">
+				Leave feedback
+			</button>
+		</div>
+	</div>
+
+	<Flicking
+		:options="{
+			align: 'center',
+			moveType: 'snap',
+			circular: true,
+			circularFallback: 'bound',
+		}"
+		:plugins="plugins"
+		class="cursor-grab mb-20"
+	>
+		<div v-for="review of mappedReviews" :key="review.id" class="rounded-[12px] bg-[#24242780] border-[#393A3E] inline-flex flex-col border select-none slider-review-card">
+			<div class="flex flex-col gap-5 p-6">
+				<div class="flex gap-4">
+					<img :src="review.avatarUrl" class="w-11 h-11 rounded-full" />
+					<div class="flex flex-col">
+						<a
+							class="text-lg text-white font-semibold"
+							:href="'https://twitch.tv/' + review.username"
+							target="_blank"
+						>
+							@{{ review.username }}
+						</a>
+						<span class="text-sm text-stone-400">{{ review.roles.join(', ') }}</span>
+					</div>
+				</div>
+				<span class="text-base text-stone-400">
+					{{ review.comment }}
+				</span>
+				<div class="flex justify-between">
+					<div></div>
+					<div class="flex gap-1">
+						<img v-for="(_, index) of Array(review.rating)" :key="index" :src="RatingStarFilled" />
+						<img v-for="(_, index) of Array(5 - review.rating)" :key="index" :src="RatingStarFilled" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</Flicking>
+</template>
+
+<style scoped>
+@import '@egjs/vue3-flicking/dist/flicking.css';
+
+.slider-review-card {
+  width: 380px;
+  margin: 0 12px;
+	height: 230px;
+	opacity: 1 !important;
+
+  @media screen and (max-width: 565.98px) {
+    width: calc(100vw - 24px * 2);
+  }
+}
+</style>
