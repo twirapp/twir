67793bd5cf10ca32307fbd1c86aa979de5b71210
Artem Melnyk <melkam464@gmail.com>
2023-08-12 17:31:21 +0200

    upd

--- frontend/landing/src/components/index/heroStartButton.vue
+++ frontend/landing/src/components/index/heroStartButton.vue
@@ -9,7 +9,7 @@ const authLink = useStore(authLinkStore);
 
 <template>
 	<a
-		class="inline-flex justify-center items-center py-3 px-7 xs:py-4 font-semibold text-white rounded-lg bg-[#5D58F5] hover:bg-[#6964FF] transition-[background,box-shadow] text-base sm:text-lg focus-visible:outline-none focus-visible:ring-[#5D58F5]/50 focus-visible:ring-4 text-center"
+		class="inline-flex justify-center items-center py-3 px-7 xs:py-4 font-semibold text-white rounded-lg bg-[#5D58F5] hover:bg-[#6964FF] transition-[background,box-shadow] text-base sm:text-lg focus-visible:outline-none focus-visible:ring-[#5D58F5]/50 focus-visible:ring-4 text-center whitespace-nowrap"
 		:href="!profile ? authLink : '/dashboard'"
 	>
 		<template v-if="!profile">Start for free</template>
