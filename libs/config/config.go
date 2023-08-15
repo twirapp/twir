9717f814a90abb916e3083d1bf25c31da5471143
Satont <satontworldwide@gmail.com>
2023-08-15 06:50:43 +0300

    chore: update config lib

--- libs/config/config.go
+++ libs/config/config.go
@@ -13,21 +13,17 @@ type Config struct {
 	BotAccessToken  string `required:"false"    envconfig:"BOT_ACCESS_TOKEN"`
 	BotRefreshToken string `required:"false"    envconfig:"BOT_REFRESH_TOKEN"`
 
-	RedisUrl                 string  `required:"true"  default:"redis://localhost:6379/0"    envconfig:"REDIS_URL"`
-	TwitchClientId           string  `required:"true"                                        envconfig:"TWITCH_CLIENTID"`
-	TwitchClientSecret       string  `required:"true"                                        envconfig:"TWITCH_CLIENTSECRET"`
-	TwitchCallbackUrl        string  `required:"true"  default:"http://localhost:3005/login" envconfig:"TWITCH_CALLBACKURL"`
-	DatabaseUrl              string  `required:"true"                                        envconfig:"DATABASE_URL"`
-	AppEnv                   string  `required:"true"  default:"development"                 envconfig:"APP_ENV"`
-	SentryDsn                string  `required:"false"                                       envconfig:"SENTRY_DSN"`
-	FeedbackTelegramBotToken *string `required:"false"                                       envconfig:"FEEDBACK_TELEGRAM_BOT_TOKEN"`
-	FeedbackTelegramUserID   *string `required:"false"                                       envconfig:"FEEDBACK_TELEGRAM_USERID"`
-	JwtAccessSecret          string  `required:"false" default:"CoolSecretForAccess"         envconfig:"JWT_ACCESS_SECRET"`
-	JwtRefreshSecret         string  `required:"false" default:"CoolSecretForRefresh"        envconfig:"JWT_REFRESH_SECRET"`
-	HostName                 string  `required:"false" default:"localhost:3005" envconfig:"HOSTNAME"`
-	TokensCipherKey          string  `required:"false" default:"pnyfwfiulmnqlhkvixaeligpprcnlyke" envconfig:"TOKENS_CIPHER_KEY"`
-	TTSServiceUrl            string  `required:"false" default:"localhost:7000" envconfig:"TTS_SERVICE_URL"`
-	OdesliApiKey             string  `required:"false" envconfig:"ODESLI_API_KEY"`
+	RedisUrl           string `required:"true"  default:"redis://localhost:6379/0"    envconfig:"REDIS_URL"`
+	TwitchClientId     string `required:"true"                                        envconfig:"TWITCH_CLIENTID"`
+	TwitchClientSecret string `required:"true"                                        envconfig:"TWITCH_CLIENTSECRET"`
+	TwitchCallbackUrl  string `required:"true"  default:"http://localhost:3005/login" envconfig:"TWITCH_CALLBACKURL"`
+	DatabaseUrl        string `required:"true"                                        envconfig:"DATABASE_URL"`
+	AppEnv             string `required:"true"  default:"development"                 envconfig:"APP_ENV"`
+	SentryDsn          string `required:"false"                                       envconfig:"SENTRY_DSN"`
+	HostName           string `required:"false" default:"localhost:3005" envconfig:"HOSTNAME"`
+	TokensCipherKey    string `required:"false" default:"pnyfwfiulmnqlhkvixaeligpprcnlyke" envconfig:"TOKENS_CIPHER_KEY"`
+	TTSServiceUrl      string `required:"false" default:"localhost:7000" envconfig:"TTS_SERVICE_URL"`
+	OdesliApiKey       string `required:"false" envconfig:"ODESLI_API_KEY"`
 }
 
 func New() (*Config, error) {
