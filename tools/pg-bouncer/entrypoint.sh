88c4f1f685e291aea368435b7d4ec146a68cbb73
Satont <satontworldwide@gmail.com>
2023-08-15 09:09:06 +0300

    chore(stack): remove pg-pool

--- tools/pg-bouncer/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-export POSTGRESQL_USERNAME=$(cat /run/secrets/tsuwari_postgres_user)
-export POSTGRESQL_PASSWORD=$(cat /run/secrets/tsuwari_postgres_password)
-export POSTGRESQL_DATABASE=$(cat /run/secrets/tsuwari_postgres_db)
-export POSTGRESQL_HOST=postgres
