28d216946bcc2b126f1e17548a2070f487c5ec0e
Satont <satontworldwide@gmail.com>
2023-08-17 02:29:30 +0300

    chore(landing): fix api addr in docker

--- frontend/landing/src/api/twirp.ts
+++ frontend/landing/src/api/twirp.ts
@@ -3,7 +3,7 @@ import { ProtectedClient, UnProtectedClient } from '@twir/grpc/generated/api/api
 
 const host = process.env.HOSTNAME;
 const isDev = process.env.NODE_ENV === 'development';
-const baseUrl = `${host?.startsWith('localhost') ? 'http' : 'https'}://${isDev ? host : 'api'}/api/v1`;
+const baseUrl = `${host?.startsWith('localhost') ? 'http' : 'https'}://${isDev ? host : 'api:3002'}/api/v1`;
 
 const transport = new TwirpFetchTransport({
 	baseUrl,
