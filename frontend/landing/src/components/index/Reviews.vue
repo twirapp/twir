9c0935ec289c133ee76a4bff58f5b430caf55ef9
Artem Melnyk <51422045+MellKam@users.noreply.github.com>
2023-08-29 17:18:31 +0200

    upd (#421)

--- frontend/landing/src/components/index/Reviews.vue
+++ frontend/landing/src/components/index/Reviews.vue
@@ -7,46 +7,52 @@ import { onMounted, ref, computed, watch } from 'vue';
 const plugins = [new AutoPlay({ stopOnHover: true })];
 
 const reviews = [
-{
-    id: '139336353',
-    username: '7ssk7',
+	{
+		id: '139336353',
+		username: '7ssk7',
 		roles: ['Streamer', 'pro player'],
-    comment: `I've been using Twir for a few years now. There are useful integrations with Volaroant, Spotify. I am pleased with its stability and functionality.`,
-    avatarUrl:
-      'https://static-cdn.jtvnw.net/jtv_user_pictures/66cb7060-1a8a-4fca-9ccd-f760b70af636-profile_image-70x70.png',
-    rating: 5,
-  },
-  {
-    id: '104435562',
-    username: 'qrushcsgo',
+		comment: `I've been using Twir for a few years now. There are useful integrations with Volaroant, Spotify. I am pleased with its stability and functionality.`,
+		avatarUrl:
+			'https://static-cdn.jtvnw.net/jtv_user_pictures/66cb7060-1a8a-4fca-9ccd-f760b70af636-profile_image-70x70.png',
+		rating: 5,
+	},
+	{
+		id: '104435562',
+		username: 'qrushcsgo',
 		roles: ['Streamer', 'player'],
-    comment: `Good, handy bot for streaming. Easy to set all the settings and everything works clearly. Recommended 👍`,
-    avatarUrl:
-      'https://static-cdn.jtvnw.net/jtv_user_pictures/a477bccc-9b23-44d7-a379-fe64f67898c3-profile_image-70x70.png',
-    rating: 4,
-  },
+		comment: `Good, handy bot for streaming. Easy to set all the settings and everything works clearly. Recommended 👍`,
+		avatarUrl:
+			'https://static-cdn.jtvnw.net/jtv_user_pictures/a477bccc-9b23-44d7-a379-fe64f67898c3-profile_image-70x70.png',
+		rating: 4,
+	},
 	{
 		id: '48385787',
 		username: 'promotive',
 		roles: ['Streamer'],
-		comment: 'Started used Twir in 2017, now it\'s a robust, feature-rich bot for streamers — complete automation for advanced chat functionality.',
-		avatarUrl: 'https://static-cdn.jtvnw.net/jtv_user_pictures/6808c622-2cf0-4319-a2ac-d91ae5212928-profile_image-70x70.png',
+		comment:
+			'Started used Twir in 2017, now it\'s a robust, feature-rich bot for streamers — complete automation for advanced chat functionality.',
+		avatarUrl:
+			'https://static-cdn.jtvnw.net/jtv_user_pictures/6808c622-2cf0-4319-a2ac-d91ae5212928-profile_image-70x70.png',
 		rating: 5,
 	},
 	{
 		id: '155644238',
 		username: 'le_xot',
 		roles: ['Streamer'],
-		comment: 'Twir combines a simple and clear interface, extensive customization and integration options, making this bot an indispensable assistant on my broadcasts.',
-		avatarUrl: 'https://static-cdn.jtvnw.net/jtv_user_pictures/423e40e6-9534-46ac-9ed8-5714657dd03b-profile_image-70x70.png',
+		comment:
+			'Twir combines a simple and clear interface, extensive customization and integration options, making this bot an indispensable assistant on my broadcasts.',
+		avatarUrl:
+			'https://static-cdn.jtvnw.net/jtv_user_pictures/423e40e6-9534-46ac-9ed8-5714657dd03b-profile_image-70x70.png',
 		rating: 5,
 	},
 	{
 		id: '189703483',
 		username: 'daetojekara',
 		roles: ['Streamer', 'viewer'],
-		comment: 'Streamline broadcasts: bot controls, configures & monitors. Integrated 3rd-party tools enable dashboard-based basic setup.',
-		avatarUrl: 'https://static-cdn.jtvnw.net/jtv_user_pictures/b73f81e7-3fe1-415b-a543-4fe164d16e56-profile_image-70x70.png',
+		comment:
+			'Streamline broadcasts: bot controls, configures & monitors. Integrated 3rd-party tools enable dashboard-based basic setup.',
+		avatarUrl:
+			'https://static-cdn.jtvnw.net/jtv_user_pictures/b73f81e7-3fe1-415b-a543-4fe164d16e56-profile_image-70x70.png',
 		rating: 5,
 	},
 ];
@@ -55,10 +61,10 @@ const twitchUsers = ref<TwitchUser[]>([]);
 
 onMounted(async () => {
 	if (typeof window === 'undefined') return;
-  const { browserUnProtectedClient } = await import('../../api/twirp-browser.js');
+	const { browserUnProtectedClient } = await import('../../api/twirp-browser.js');
 
 	const request = await browserUnProtectedClient.twitchGetUsers({
-		ids: reviews.map(r => r.id),
+		ids: reviews.map((r) => r.id),
 		names: [],
 	});
 	twitchUsers.value = request.response.users;
@@ -67,8 +73,8 @@ onMounted(async () => {
 const mappedReviews = computed(() => {
 	if (!twitchUsers.value.length) return reviews;
 
-	return reviews.map(r => {
-		const twitchUser = twitchUsers.value.find(u => u.id === r.id);
+	return reviews.map((r) => {
+		const twitchUser = twitchUsers.value.find((u) => u.id === r.id);
 
 		return {
 			...r,
@@ -142,9 +148,9 @@ async function sendFeedBack() {
 	<div class="reviews-bg">
 		<div class="container py-24">
 			<div class="flex justify-between">
-				<h1 class="text-white text-4xl font-bold">
+				<h3 class="text-white text-4xl font-bold">
 					Reviews from streamers and viewers
-				</h1>
+				</h3>
 
 				<button
 					class="inline-flex px-6 py-3 font-semibold text-white rounded-lg bg-[#5D58F5] hover:bg-[#6964FF] transition-[background,box-shadow] text-lg focus-visible:outline-none focus-visible:ring-[#5D58F5]/50 focus-visible:ring-4"
@@ -167,7 +173,11 @@ async function sendFeedBack() {
 				:plugins="plugins"
 				class="cursor-grab mb-20"
 			>
-				<div v-for="review of mappedReviews" :key="review.id" class="panel gap-5 p-6 rounded-[12px] bg-[#24242780] border-[#393A3E] inline-flex flex-col border select-none slider-review-card">
+				<div
+					v-for="review of mappedReviews"
+					:key="review.id"
+					class="panel gap-5 p-6 rounded-[12px] bg-[#24242780] border-[#393A3E] inline-flex flex-col border select-none slider-review-card"
+				>
 					<div class="flex justify-between">
 						<div class="flex items-center gap-4">
 							<img :src="review.avatarUrl" class="w-11 h-11 rounded-full" />
@@ -235,10 +245,12 @@ async function sendFeedBack() {
 				</button>
 				<button
 					class="inline-flex px-3 py-1 font-semibold text-white rounded-lg bg-[#5D58F5] hover:bg-[#6964FF] transition-[background,box-shadow] text-lg focus-visible:outline-none focus-visible:ring-[#5D58F5]/50 focus-visible:ring-4"
-					@click="(e) => {
-						e.preventDefault();
-						sendFeedBack()
-					}"
+					@click="
+						(e) => {
+							e.preventDefault();
+							sendFeedBack();
+						}
+					"
 				>
 					Confirm
 				</button>
@@ -251,25 +263,25 @@ async function sendFeedBack() {
 @import '@egjs/vue3-flicking/dist/flicking.css';
 
 .slider-review-card {
-  width: 380px;
-  margin: 0 12px;
+	width: 380px;
+	margin: 0 12px;
 	height: 230px;
 	opacity: 1 !important;
 
-  @media screen and (max-width: 565.98px) {
-    width: calc(100vw - 24px * 2);
-  }
+	@media screen and (max-width: 565.98px) {
+		width: calc(100vw - 24px * 2);
+	}
 }
 
 .reviews-bg {
-	background: radial-gradient(ellipse at top, #270a3b, transparent), radial-gradient(ellipse at bottom, #000, transparent);
+	background: radial-gradient(ellipse at top, #270a3b, transparent),
+		radial-gradient(ellipse at bottom, #000, transparent);
 }
 
 #feedback-dialog::backdrop {
 	background-color: rgba(0, 0, 0, 9);
 	opacity: 0.7;
 }
-
 
 #feedback-dialog {
 	box-shadow: 0 4px 5px rgb(0 0 0 / 90%);
