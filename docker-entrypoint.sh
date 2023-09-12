c60cd3554809d9e6a2db001627e3a7693815b7c8
Satont <satontworldwide@gmail.com>
2023-09-12 23:43:14 +0300

    fix(stack): correct docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-cat /run/secrets/tsuwari_doppler_token | doppler configure set token --scope / > /dev/null
+cat /run/secrets/twir_doppler_token | doppler configure set token --scope / > /dev/null
 
 exec doppler run -- "$@"
