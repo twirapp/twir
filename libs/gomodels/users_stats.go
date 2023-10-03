03b64d8742e3c6f534a502bcac6855efb451b3c2
Satont <satontworldwide@gmail.com>
2023-10-03 16:29:32 +0300

    fix(parser): add missed destinations for missed `usersStats` model

--- libs/gomodels/users_stats.go
+++ libs/gomodels/users_stats.go
@@ -6,10 +6,10 @@ type UsersStats struct {
 	ChannelID         string `gorm:"column:channelId;type:TEXT;"                     json:"channelId" db:"channelId"`
 	Messages          int32  `gorm:"column:messages;type:INT4;default:0;"            json:"messages" db:"messages"`
 	Watched           int64  `gorm:"column:watched;type:INT8;default:0;"             json:"watched" db:"watched"`
-	UsedChannelPoints int64  `gorm:"column:usedChannelPoints;type:INT8;default:0;"       json:"usedChannelPoints" db:"usedChannelPoints"`
-	IsMod             bool   `gorm:"column:is_mod;type:BOOL;default:false;"           json:"isMod" db:"isMod"`
-	IsVip             bool   `gorm:"column:is_vip;type:BOOL;default:false;"           json:"isVip" db:"isVip"`
-	IsSubscriber      bool   `gorm:"column:is_subscriber;type:BOOL;default:false;"    json:"isSubscriber" db:"isSubscriber"`
+	UsedChannelPoints int64  `gorm:"column:usedChannelPoints;type:INT8;default:0;"   json:"usedChannelPoints" db:"usedChannelPoints"`
+	IsMod             bool   `gorm:"column:is_mod;type:BOOL;default:false;"           json:"isMod" db:"is_mod"`
+	IsVip             bool   `gorm:"column:is_vip;type:BOOL;default:false;"           json:"isVip" db:"is_vip"`
+	IsSubscriber      bool   `gorm:"column:is_subscriber;type:BOOL;default:false;"    json:"isSubscriber" db:"is_subscriber"`
 
 	// internal, not db
 	Emotes int `gorm:"-" json:"emotes" db:"emotes"`
