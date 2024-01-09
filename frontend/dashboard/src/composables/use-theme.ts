cd6614b2c12b0de89cfde7ec2397f3456e6a46ec
Satont <satontworldwide@gmail.com>
2024-01-09 17:23:02 +0300

    feat(dashboard): add optional key for `useTheme` hook

--- frontend/dashboard/src/composables/use-theme.ts
+++ frontend/dashboard/src/composables/use-theme.ts
@@ -2,8 +2,8 @@ import { useLocalStorage } from '@vueuse/core';
 
 export type Theme = 'light' | 'dark'
 
-export const useTheme = () => {
-	const theme = useLocalStorage<Theme>('twirTheme', 'dark');
+export const useTheme = (key?: string) => {
+	const theme = useLocalStorage<Theme>(key ?? 'twirTheme', 'dark');
 
 	return {
 		theme,
