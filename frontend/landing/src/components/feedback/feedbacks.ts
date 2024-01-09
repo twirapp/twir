9650d0faddfe043a4409beaed3163a575e8254e6
Vitalij Ryndin <crashmax.dev@gmail.com>
2024-01-09 16:39:42 +0800

    refactor(landing): improve folder structure and code

---
+++ frontend/landing/src/components/feedback/feedbacks.ts
@@ -0,0 +1,59 @@
+interface Feedback {
+	id: string;
+	username: string;
+	roles: string[];
+	comment: string;
+	avatarUrl: string;
+	rating: number;
+}
+
+export const feedbacks: Feedback[] = [
+	{
+		id: '139336353',
+		username: '7ssk7',
+		roles: ['Streamer', 'pro player'],
+		comment: `I've been using Twir for a few years now. There are useful integrations with Valorant, Spotify. I am pleased with its stability and functionality.`,
+		avatarUrl:
+			'https://static-cdn.jtvnw.net/jtv_user_pictures/66cb7060-1a8a-4fca-9ccd-f760b70af636-profile_image-70x70.png',
+		rating: 5,
+	},
+	{
+		id: '104435562',
+		username: 'qrushcsgo',
+		roles: ['Streamer', 'player'],
+		comment: `Good, handy bot for streaming. Easy to set all the settings and everything works clearly. Recommended 👍`,
+		avatarUrl:
+			'https://static-cdn.jtvnw.net/jtv_user_pictures/a477bccc-9b23-44d7-a379-fe64f67898c3-profile_image-70x70.png',
+		rating: 4,
+	},
+	{
+		id: '48385787',
+		username: 'promotive',
+		roles: ['Streamer'],
+		comment:
+			'Started used Twir in 2017, now it\'s a robust, feature-rich bot for streamers — complete automation for advanced chat functionality.',
+		avatarUrl:
+			'https://static-cdn.jtvnw.net/jtv_user_pictures/6808c622-2cf0-4319-a2ac-d91ae5212928-profile_image-70x70.png',
+		rating: 5,
+	},
+	{
+		id: '155644238',
+		username: 'le_xot',
+		roles: ['Streamer'],
+		comment:
+			'Twir combines a simple and clear interface, extensive customization and integration options, making this bot an indispensable assistant on my broadcasts.',
+		avatarUrl:
+			'https://static-cdn.jtvnw.net/jtv_user_pictures/423e40e6-9534-46ac-9ed8-5714657dd03b-profile_image-70x70.png',
+		rating: 5,
+	},
+	{
+		id: '189703483',
+		username: 'daetojekara',
+		roles: ['Streamer', 'viewer'],
+		comment:
+			'Streamline broadcasts: bot controls, configures & monitors. Integrated 3rd-party tools enable dashboard-based basic setup.',
+		avatarUrl:
+			'https://static-cdn.jtvnw.net/jtv_user_pictures/b73f81e7-3fe1-415b-a543-4fe164d16e56-profile_image-70x70.png',
+		rating: 5,
+	},
+];
---
+++ frontend/landing/src/components/streamers/streamers-list.vue
@@ -0,0 +1,100 @@
+<script lang="ts" setup>
+import { AutoPlay } from '@egjs/flicking-plugins';
+import Flicking from '@egjs/vue3-flicking';
+import type { GetTwirStreamersResponse_Streamer } from '@twir/grpc/generated/api/api/stats';
+import { ref } from 'vue';
+
+defineProps<{
+	streamers: GetTwirStreamersResponse_Streamer[][]
+}>();
+
+const wrapper = ref<HTMLElement>(null);
+</script>
+
+<template>
+	<div class="flicking-viewport">
+		<div ref="wrapper" class="wrapper">
+			<Flicking
+				v-if="wrapper"
+				ref="flickingRef"
+				:options="{
+					renderOnlyVisible: true,
+					moveType: 'snap',
+					circularFallback: 'linear',
+					circular: false,
+					align: 'prev',
+					bound: true
+				}"
+				:plugins="[new AutoPlay({ stopOnHover: true, duration: 2500 })]"
+			>
+				<div
+					v-for="(item, idx) in streamers"
+					:key="idx"
+					class="slider-review-card gap-5 p-6 rounded-[12px] bg-[#24242780] border-[#393A3E] inline-flex flex-col border select-none"
+				>
+					<div
+						v-for="(streamer, streamerIdx) of item" :key="streamerIdx"
+						class="flex gap-3 items-center"
+					>
+						<div class="relative">
+							<img
+								:src="streamer.avatar" class="rounded-full w-10 h-10" draggable="false"
+								:alt="`streamers-list-${streamer.userDisplayName}`"
+							/>
+							<span v-if="streamer.isLive" class="absolute inline-block bg-red-600 text-white text-xs font-semibold uppercase px-1 rounded-sm top-8 left-1">
+								LIVE
+							</span>
+						</div>
+						<a
+							class="streamer-link flex flex-col gap-1"
+							:href="`https://twitch.tv/${streamer.userLogin}`" target="_blank"
+						>
+							<div class="flex items-center">
+								<span>{{ streamer.userDisplayName }}</span>
+								<svg v-if="streamer.isPartner" class="fill-[#a970ff] ml-1" width="16" height="16" viewBox="0 0 16 16" aria-label="Verified Partner"><path fill-rule="evenodd" d="M12.5 3.5 8 2 3.5 3.5 2 8l1.5 4.5L8 14l4.5-1.5L14 8l-1.5-4.5ZM7 11l4.5-4.5L10 5 7 8 5.5 6.5 4 8l3 3Z" clip-rule="evenodd"></path></svg>
+							</div>
+							<span class="text-xs uppercase">{{ streamer.followersCount }} followers</span>
+						</a>
+					</div>
+				</div>
+			</Flicking>
+		</div>
+	</div>
+</template>
+
+<style>
+@import "@egjs/vue3-flicking/dist/flicking.css";
+@import "@egjs/flicking-plugins/dist/flicking-plugins.css";
+@import "@egjs/flicking-plugins/dist/arrow.css";
+@import "@egjs/flicking-plugins/dist/pagination.css";
+
+.wrapper {
+	display: flex;
+	width: 50dvw;
+	align-items: center;
+	justify-content: center;
+	padding-bottom: 40px;
+	margin: auto;
+	height: 300px;
+}
+
+.slider-review-card {
+	width: auto;
+	margin: 0 12px;
+	height: auto;
+	opacity: 1 !important;
+	user-select: none;
+
+	@media screen and (max-width: 565.98px) {
+		width: calc(100vw - 24px * 2);
+	}
+}
+
+:deep(.flicking-pagination-bullet) {
+	background-color: #fff !important;
+}
+
+.streamer-link {
+	user-select: none;
+}
+</style>
