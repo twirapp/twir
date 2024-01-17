a56c26f8e1ba184014e6b71e3b7f5253151e036b
Satont <satontworldwide@gmail.com>
2024-01-17 20:09:00 +0300

    uuh

--- apps/eval/src/index.js
+++ apps/eval/src/index.js
@@ -1,5 +1,5 @@
 import { PORTS } from '@twir/grpc/constants/constants';
-import * as Eval from '@twir/grpc/generated/eval/eval';
+import * as Eval from '@twir/grpc/eval/eval';
 import _ from 'lodash';
 import { createServer } from 'nice-grpc';
 import { VM } from 'vm2';
