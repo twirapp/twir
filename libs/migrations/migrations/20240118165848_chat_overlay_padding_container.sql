0cbe57a02fbc59d2b6c08f8d24aad07bae578a2e
Vitalij Ryndin <crashmax.dev@gmail.com>
2024-01-18 17:09:23 +0800

    chore: update migration

--- libs/migrations/migrations/20240118165848_chat_overlay_padding_container.sql
+++ libs/migrations/migrations/20240118165848_chat_overlay_padding_container.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 SELECT 'up SQL query';
-ALTER TABLE "channels_overlays_chat" ADD COLUMN "padding_container" smallint;
+ALTER TABLE "channels_overlays_chat" ADD COLUMN "padding_container" smallint NOT NULL DEFAULT 0;
 -- +goose StatementEnd
 
 -- +goose Down
