964e5d2138aa84ec6c5aea67343c21fa0f098456
Vitalij Ryndin <crashmax.dev@gmail.com>
2024-01-22 22:30:22 +0800

    feat(dashboard): not found page

---
+++ frontend/dashboard/src/global.d.ts
@@ -0,0 +1,16 @@
+// This can be directly added to any of your `.ts` files like `router.ts`
+// It can also be added to a `.d.ts` file. Make sure it's included in
+// project's tsconfig.json "files"
+import 'vue-router';
+import type { PermissionsType } from '@/api';
+
+// To ensure it is treated as a module, add at least one `export` statement
+export {};
+
+declare module 'vue-router' {
+  interface RouteMeta {
+		neededPermission?: PermissionsType;
+		noPadding?: boolean;
+		fullScreen?: boolean;
+  }
+}
