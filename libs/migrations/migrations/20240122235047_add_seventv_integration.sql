77775bb706a730e0f6c96401305d2a56375ef33f
Satont <satontworldwide@gmail.com>
2024-01-25 18:04:54 +0300

    use text for rewards ids, add indexes

--- libs/migrations/migrations/20240122235047_add_seventv_integration.sql
+++ libs/migrations/migrations/20240122235047_add_seventv_integration.sql
@@ -5,10 +5,14 @@ SELECT 'up SQL query';
 CREATE TABLE "channels_integrations_seventv" (
 		"id" uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
 		"channel_id" text NOT NULL,
-		"reward_id_for_add_emote" uuid,
-		"reward_id_for_remove_emote" uuid,
+		"reward_id_for_add_emote" text,
+		"reward_id_for_remove_emote" text,
 		CONSTRAINT "channels_integrations_seventv_channel_id_fk" FOREIGN KEY ("channel_id") REFERENCES "channels" ("id") ON DELETE CASCADE
 );
+
+CREATE INDEX "channels_integrations_seventv_channel_id_idx" ON "channels_integrations_seventv" ("channel_id");
+CREATE INDEX "channels_integrations_seventv_reward_id_for_add_emote_idx" ON "channels_integrations_seventv" ("reward_id_for_add_emote");
+CREATE INDEX "channels_integrations_seventv_reward_id_for_remove_emote_idx" ON "channels_integrations_seventv" ("reward_id_for_remove_emote");
 
 ALTER TYPE channels_events_operations_type_enum ADD VALUE 'SEVENTV_ADD_EMOTE';
 ALTER TYPE channels_events_operations_type_enum ADD VALUE 'SEVENTV_REMOVE_EMOTE';
