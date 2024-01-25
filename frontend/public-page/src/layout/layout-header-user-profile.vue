2233e94d16db248f38ff60c885533b79d153a9d9
Satont <satontworldwide@gmail.com>
2024-01-25 05:13:23 +0300

    fix directions on user profile skeletons

--- frontend/public-page/src/layout/layout-header-user-profile.vue
+++ frontend/public-page/src/layout/layout-header-user-profile.vue
@@ -10,11 +10,11 @@ const { data, isLoading, isError } = useUserProfile();
 
 <template>
 	<div v-if="isLoading" class="flex items-center space-x-4">
-		<Skeleton class="h-12 w-12 rounded-full" />
 		<div class="space-y-2">
-			<Skeleton class="h-4 w-[250px]" />
-			<Skeleton class="h-4 w-[200px]" />
+			<Skeleton class="h-4 w-[50px]" />
+			<Skeleton class="h-4 w-[50px]" />
 		</div>
+		<Skeleton class="h-12 w-12 rounded-full" />
 	</div>
 	<!--	use !data for test login button -->
 	<div v-else-if="!isError && data" class="flex items-center gap-2">
