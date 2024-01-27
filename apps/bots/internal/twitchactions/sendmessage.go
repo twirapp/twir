5293a19389161d135adb737d417cfab92ca73773
Satont <satontworldwide@gmail.com>
2024-01-27 05:49:37 +0300

    kruto

--- apps/bots/internal/twitchactions/sendmessage.go
+++ apps/bots/internal/twitchactions/sendmessage.go
@@ -3,6 +3,7 @@ package twitchactions
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/nicklaw5/helix/v2"
 	"github.com/satont/twir/libs/twitch"
@@ -15,17 +16,31 @@ type SendMessageOpts struct {
 	ReplyParentMessageID string
 }
 
+func validateResponseSlashes(response string) string {
+	if strings.HasPrefix(response, "/me") || strings.HasPrefix(response, "/announce") {
+		return response
+	} else if strings.HasPrefix(response, "/") {
+		return "Slash commands except /me and /announce is disallowed. This response wont be ever sended."
+	} else if strings.HasPrefix(response, ".") {
+		return `Message cannot start with "." symbol.`
+	} else {
+		return response
+	}
+}
+
 func (c *TwitchActions) SendMessage(ctx context.Context, opts SendMessageOpts) error {
 	twitchClient, err := twitch.NewBotClientWithContext(ctx, opts.SenderID, c.Config, c.TokensGrpc)
 	if err != nil {
 		return err
 	}
 
+	text := strings.ReplaceAll(opts.Message, "\n", " ")
+
 	resp, err := twitchClient.SendChatMessage(
 		&helix.SendChatMessageParams{
 			BroadcasterID:        opts.BroadcasterID,
 			SenderID:             opts.SenderID,
-			Message:              opts.Message,
+			Message:              validateResponseSlashes(text),
 			ReplyParentMessageID: opts.ReplyParentMessageID,
 		},
 	)
