c4bde6c5f8cf9bd2952b4c1b56ff7f7d0c0bf754
Satont <satontworldwide@gmail.com>
2024-02-25 04:35:00 +0300

    feat(dashboard): add new dudes command to form

--- frontend/dashboard/src/pages/overlays/dudes/dudes-settings.vue
+++ frontend/dashboard/src/pages/overlays/dudes/dudes-settings.vue
@@ -122,7 +122,11 @@ const addable = computed(() => {
 			</div>
 		</div>
 		<div style="width: 30%;">
-			<command-button name="jump" />
+			<div style="display: flex; gap: 8px; flex-wrap: wrap">
+				<command-button title="Jump command" name="jump" />
+				<command-button title="Color command" name="dudes color" />
+				<command-button title="Grow command" name="dudes grow" />
+			</div>
 			<n-tabs
 				v-model:value="openedTab"
 				type="card"
