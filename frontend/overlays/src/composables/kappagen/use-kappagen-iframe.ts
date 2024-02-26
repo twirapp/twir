939b68caa00df22f2cf74258c20914aeb130ec78
Vitalij Ryndin <crashmax.dev@gmail.com>
2024-02-26 21:18:52 +0800

    feat(overlays): update `@twirapp/kappagen` and refactor

--- frontend/overlays/src/composables/kappagen/use-kappagen-iframe.ts
+++ frontend/overlays/src/composables/kappagen/use-kappagen-iframe.ts
@@ -1,40 +1,38 @@
+import type { KappagenMethods } from '@twirapp/kappagen/types';
+
 import { animations } from './kappagen-animations.js';
 import { twirEmote } from './use-kappagen-builder.js';
 import { useKappagenSettings } from './use-kappagen-settings.js';
 
-import type { KappagenSpawnAnimatedEmotesFn, KappagenSpawnEmotesFn, KappagenSettings } from '@/types.js';
+import type { KappagenSettings } from '@/types.js';
 
-type Options = {
-	kappagenCallback: KappagenSpawnAnimatedEmotesFn
-	spawnCallback: KappagenSpawnEmotesFn
-	clearCallback?: () => void;
-}
+type Options = KappagenMethods
 
 export const useKappagenIframe = (options: Options) => {
-	const { setSettings } = useKappagenSettings();
+	const { updateSettings } = useKappagenSettings();
 
 	const onWindowMessage = (msg: MessageEvent<string>) => {
 		const parsedData = JSON.parse(msg.data) as { key: string, data?: any };
 
 		if (parsedData.key === 'settings' && parsedData.data) {
 			const settings = parsedData.data as KappagenSettings;
-			setSettings(settings);
+			updateSettings(settings);
 		}
 
 		if (parsedData.key === 'kappa') {
-			options.kappagenCallback([twirEmote], animations[Math.floor(Math.random() * animations.length)]);
+			options.playAnimation([twirEmote], animations[Math.floor(Math.random() * animations.length)]);
 		}
 
 		if (parsedData.key === 'kappaWithAnimation') {
-			options.kappagenCallback([twirEmote], parsedData.data.animation);
+			options.playAnimation([twirEmote], parsedData.data.animation);
 		}
 
 		if (parsedData.key === 'spawn') {
-			options.spawnCallback([twirEmote]);
+			options.showEmotes([twirEmote]);
 		}
 
 		if (parsedData.key === 'clear') {
-			options.clearCallback?.();
+			options.clear();
 		}
 	};
 
--- frontend/overlays/src/composables/kappagen/use-kappagen-settings.ts
+++ frontend/overlays/src/composables/kappagen/use-kappagen-settings.ts
@@ -1,17 +1,89 @@
+import type { KappagenConfig } from '@twirapp/kappagen/types';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 import type { KappagenSettings } from '@/types.js';
 
 export const useKappagenSettings = defineStore('kappagen-settings', () => {
-	const settings = ref<KappagenSettings>();
+	const kappagenSettings = ref<Required<KappagenConfig>>({
+		max: 0,
+		time: 5,
+		queue: 0,
+		cube: {
+			speed: 6,
+		},
+		animation: {
+			fade: {
+				in: 8,
+				out: 9,
+			},
+			zoom: {
+				in: 17,
+				out: 8,
+			},
+		},
+		in: {
+			fade: true,
+			zoom: true,
+		},
+		out: {
+			fade: true,
+			zoom: true,
+		},
+		size: {
+			min: 1,
+			max: 256,
+			ratio: {
+				normal: 1 / 12,
+				small: 1 / 24,
+			},
+		},
+	});
 
-	function setSettings(newOptions: KappagenSettings): void {
-		settings.value = newOptions;
+	const overlaySettings = ref<KappagenSettings>();
+
+	function updateOverlaySettings(settings: KappagenSettings): void {
+		overlaySettings.value = settings;
+
+		if (settings.emotes) {
+			kappagenSettings.value.max = settings.emotes.max;
+			kappagenSettings.value.time = settings.emotes.time;
+			kappagenSettings.value.queue = settings.emotes.queue;
+		}
+
+		if (settings.size) {
+			kappagenSettings.value.size = {
+				min: settings.size.min,
+				max: settings.size.max,
+				ratio: {
+					normal: settings.size.ratioNormal,
+					small: settings.size.ratioSmall,
+				},
+			};
+		}
+
+		if (settings.cube) {
+			kappagenSettings.value.cube = {
+				speed: settings.cube.speed,
+			};
+		}
+
+		if (settings.animation) {
+			kappagenSettings.value.in = {
+				fade: settings.animation.fadeIn,
+				zoom: settings.animation.zoomIn,
+			};
+
+			kappagenSettings.value.out = {
+				fade: settings.animation.fadeOut,
+				zoom: settings.animation.zoomOut,
+			};
+		}
 	}
 
 	return {
-		settings,
-		setSettings,
+		overlaySettings,
+		kappagenSettings,
+		updateSettings: updateOverlaySettings,
 	};
 });
