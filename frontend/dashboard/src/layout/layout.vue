15b68f5d88e5c47d77703266cac8e6a0858cae6b
Satont <satontworldwide@gmail.com>
2024-04-07 19:34:24 +0300

    upd

--- frontend/dashboard/src/layout/layout.vue
+++ frontend/dashboard/src/layout/layout.vue
@@ -19,6 +19,7 @@ import { RouterView, useRouter } from 'vue-router';
 
 import { Toaster as Sonner } from '@/components/ui/sonner';
 import { Toaster } from '@/components/ui/toast';
+import { useLayout } from '@/composables/use-layout';
 import { useTheme } from '@/composables/use-theme.js';
 import Header from '@/layout/header.vue';
 import Sidebar from '@/layout/sidebar.vue';
