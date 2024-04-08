90e2b9871da15b08921590975068a997d9b23210
Vitalij Ryndin <crashmax.dev@gmail.com>
2024-04-08 19:18:57 +0800

    upd

--- frontend/dashboard/src/pages/admin-panel.vue
+++ frontend/dashboard/src/pages/admin-panel.vue
@@ -2,22 +2,22 @@
 import { useI18n } from 'vue-i18n';
 
 import AdminManageBadges from '@/features/admin-panel/manage-badges/manage-badges.vue';
+import AdminManageNotifications from '@/features/admin-panel/manage-notifications/manage-notifications.vue';
 import AdminManageUsers from '@/features/admin-panel/manage-users/manage-users.vue';
-import AdminNotifications from '@/features/admin-panel/notifications/notifications.vue';
 import PageLayout, { type PageLayoutTab } from '@/layout/page-layout.vue';
 
 const { t } = useI18n();
 
 const pageTabs: PageLayoutTab[] = [
 	{
-		name: 'notifications',
-		title: t('adminPanel.notifications.title'),
-		component: AdminNotifications,
-	},
-	{
 		name: 'users',
 		title: t('adminPanel.manageUsers.title'),
 		component: AdminManageUsers,
+	},
+	{
+		name: 'notifications',
+		title: t('adminPanel.notifications.title'),
+		component: AdminManageNotifications,
 	},
 	{
 		name: 'badges',
