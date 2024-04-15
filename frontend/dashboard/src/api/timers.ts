b63953dfe1424e1667910d8d50a27c08ffe0977a
Vitalij Ryndin <crashmax.dev@gmail.com>
2024-04-15 18:27:28 +0800

    feat(dashboard): timers gql

---
+++ frontend/dashboard/src/api/timers.ts
@@ -0,0 +1,61 @@
+import { useQuery } from '@urql/vue';
+import { defineStore } from 'pinia';
+
+import { useMutation } from '@/composables/use-mutation.js';
+import { graphql } from '@/gql';
+import type { GetAllTimersQuery } from '@/gql/graphql';
+
+export type TimerResponse = GetAllTimersQuery['timers'][0];
+
+const invalidationKey = 'TimersInvalidateKey';
+
+export const useTimersApi = defineStore('api/timers', () => {
+	const useQueryTimers = () => useQuery({
+		variables: {},
+		context: { additionalTypenames: [invalidationKey] },
+		query: graphql(`
+			query GetAllTimers {
+				timers {
+					id
+					name
+					enabled
+					timeInterval
+					messageInterval
+					responses {
+						text
+						isAnnounce
+					}
+				}
+			}
+		`),
+	});
+
+	const useMutationCreateTimer = () => useMutation(graphql(`
+		mutation CreateTimer($opts: TimerCreateInput!) {
+			timersCreate(opts: $opts) {
+				id
+			}
+		}
+	`), [invalidationKey]);
+
+	const useMutationUpdateTimer = () => useMutation(graphql(`
+		mutation UpdateTimer($id: String!, $opts: TimerUpdateInput!) {
+			timersUpdate(id: $id, opts: $opts) {
+				id
+			}
+		}
+	`), [invalidationKey]);
+
+	const useMutationRemoveTimer = () => useMutation(graphql(`
+		mutation RemoveTimer($id: String!) {
+			timersRemove(id: $id)
+		}
+	`), [invalidationKey]);
+
+	return {
+		useQueryTimers,
+		useMutationCreateTimer,
+		useMutationUpdateTimer,
+		useMutationRemoveTimer,
+	};
+});
