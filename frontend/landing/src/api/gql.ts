847edeff82e7da689fd18fff498cbac3214f120b
Satont <satontworldwide@gmail.com>
2024-04-27 19:45:28 +0300

    fix(landing): correct gql api addr

--- frontend/landing/src/api/gql.ts
+++ frontend/landing/src/api/gql.ts
@@ -2,6 +2,6 @@ import { config } from '@twir/config';
 
 const host = config.SITE_BASE_URL ?? 'localhost:3005';
 const isDev = config.NODE_ENV === 'development';
-const apiAddr = isDev ? `${host}/api-new` : 'api:3009';
+const apiAddr = isDev ? `${host}/api-new` : 'api-gql:3009';
 
 export const gqlUrl = `http://${apiAddr}/query`;
