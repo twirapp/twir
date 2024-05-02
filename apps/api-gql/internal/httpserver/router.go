fe3e1096d292b2a5f7d6a72d434f2ad57c23e85b
Satont <satontworldwide@gmail.com>
2024-05-03 01:22:47 +0300

    chore(api-gql): change remoteIpheaders slice to only cf-connection-ip

--- apps/api-gql/internal/httpserver/router.go
+++ apps/api-gql/internal/httpserver/router.go
@@ -45,7 +45,7 @@ func New(opts Opts) *Server {
 	)
 
 	r.ForwardedByClientIP = true
-	r.RemoteIPHeaders = append(r.RemoteIPHeaders, "X-Forwarded-For", "X-Real-IP", "Cf-Connecting-IP")
+	r.RemoteIPHeaders = append(r.RemoteIPHeaders, "Cf-Connecting-IP")
 
 	r.Use(opts.Sessions.Middleware())
 
