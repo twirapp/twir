0a819cd81309767d48839efc9b10515476d0969d
Vitalij Ryndin <crashmax.dev@gmail.com>
2024-05-10 18:13:24 +0800

    wires

--- frontend/dashboard/src/features/admin-panel/manage-users/components/users-action-selector.vue
+++ frontend/dashboard/src/features/admin-panel/manage-users/components/users-action-selector.vue
@@ -4,12 +4,13 @@ import { useI18n } from 'vue-i18n'
 
 import { useUsers } from '../composables/use-users.js'
 
+import { useProfile } from '@/api/auth.js'
 import { Button } from '@/components/ui/button'
 import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
-	DropdownMenuTrigger
+	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 
 defineProps<{
@@ -20,10 +21,11 @@ defineProps<{
 
 const { t } = useI18n()
 const { switchBan, switchAdmin } = useUsers()
+const { data: profile } = useProfile()
 </script>
 
 <template>
-	<DropdownMenu>
+	<DropdownMenu v-if="userId !== profile?.id">
 		<DropdownMenuTrigger as-child>
 			<Button variant="secondary" size="icon">
 				<WrenchIcon class="size-4" />
