b58bafd7fb4db10ca1ddd387961896feb04c9736
Vitalij Ryndin <crashmax.dev@gmail.com>
2024-05-10 17:34:49 +0800

    wires

--- frontend/dashboard/src/features/alerts/composables/use-alerts-information.ts
+++ frontend/dashboard/src/features/alerts/composables/use-alerts-information.ts
@@ -1,11 +1,8 @@
 import { computed, ref } from 'vue'
-import { useI18n } from 'vue-i18n'
 
 import { useProfile } from '@/api'
 
 export function useAlertsInformation() {
-	const { t } = useI18n()
-
 	const { data: profile } = useProfile()
 	const overlayLink = computed(() => {
 		return `${window.location.origin}/overlays/${profile.value?.apiKey}/alerts`
@@ -16,12 +13,9 @@ export function useAlertsInformation() {
 		isShowOverlayLink.value = !isShowOverlayLink.value
 	}
 
-	const copyButtonText = ref(t('alerts.copyOverlayLink'))
 	async function copyOverlayLink() {
 		try {
 			await navigator.clipboard.writeText(overlayLink.value)
-			copyButtonText.value = t('sharedTexts.copied')
-			setTimeout(() => (copyButtonText.value = t('alerts.copyOverlayLink')), 1000)
 		} catch (error) {
 			console.error(error)
 		}
@@ -31,8 +25,6 @@ export function useAlertsInformation() {
 		overlayLink,
 		isShowOverlayLink,
 		toggleShowOverlayLink,
-
-		copyButtonText,
 		copyOverlayLink,
 	}
 }
