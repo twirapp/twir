d00540a5b9fc4e194d7ac912456352492ebc7cc7
Satont <satontworldwide@gmail.com>
2024-05-31 06:36:54 +0300

    fix(migrations): correct migration for eventsub
    [skip ci]

--- libs/migrations/migrations/20240531032351_eventsub_unique_index.sql
+++ libs/migrations/migrations/20240531032351_eventsub_unique_index.sql
@@ -2,7 +2,7 @@
 -- +goose StatementBegin
 SELECT 'up SQL query';
 
-CREATE UNIQUE INDEX "eventsub_subscriptions_user_id_idx" ON "eventsub_subscriptions" ("topic_id", "user_id");
+CREATE UNIQUE INDEX "eventsub_subscriptions_topic_id_user_id_idx" ON "eventsub_subscriptions" ("topic_id", "user_id");
 -- +goose StatementEnd
 
 -- +goose Down
