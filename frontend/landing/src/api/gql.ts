8a76048c17c67fd199d0f6b8f10dea476cd856ba
Satont <satontworldwide@gmail.com>
2024-05-31 10:46:32 +0300

    fix(landing): correct gql addr

--- frontend/landing/src/api/gql.ts
+++ frontend/landing/src/api/gql.ts
@@ -3,6 +3,6 @@ import { config } from '@twir/config'
 const host = config.SITE_BASE_URL ?? 'localhost:3005'
 const isDev = config.NODE_ENV === 'development'
 const apiAddr = isDev ? `${host}/api` : 'api-gql:3009'
-const protocol = isDev && !config.USE_WSS ? 'http' : 'https'
+const protocol = config.USE_WSS ? 'https' : 'http'
 
 export const gqlUrl = process.env.API_GQL_ADDR || `${protocol}://${apiAddr}/query`
