2007bacc5ab3839f1596b1f4669d4b0306f3267c
Vitalij Ryndin <crashmax.dev@gmail.com>
2024-07-10 00:40:16 +0800

    feat: ffz settings in the chat widget

--- frontend/dashboard/src/components/dashboard/chat.vue
+++ frontend/dashboard/src/components/dashboard/chat.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
-import { IconMoon, IconSun } from '@tabler/icons-vue'
-import { NButton } from 'naive-ui'
-import { computed } from 'vue'
+import { IconDog, IconMoon, IconSun } from '@tabler/icons-vue'
+import { NButton, NTooltip } from 'naive-ui'
+import { computed, ref } from 'vue'
 
 import Card from './card.vue'
 
@@ -12,6 +12,7 @@ const { data: profile } = useProfile()
 
 const { theme: chatTheme, toggleTheme } = useTheme('twirTwitchChatTheme')
 
+const openFrankerFaceZ = ref(false)
 const selectedTwitchId = computed(() => profile.value?.selectedDashboardId ?? '')
 const selectedDashboardTwitchUser = useTwitchGetUsers({ ids: selectedTwitchId })
 
@@ -26,6 +27,10 @@ const chatUrl = computed(() => {
 		url += '&darkpopout'
 	}
 
+	if (openFrankerFaceZ.value) {
+		url += '&ffz-settings'
+	}
+
 	return url
 })
 </script>
@@ -33,6 +38,16 @@ const chatUrl = computed(() => {
 <template>
 	<Card :content-style="{ 'margin-bottom': '10px', 'padding': '0px' }">
 		<template #header-extra>
+			<NTooltip trigger="hover" placement="bottom">
+				<template #trigger>
+					<NButton size="small" text @click="openFrankerFaceZ = !openFrankerFaceZ">
+						<IconDog />
+					</NButton>
+				</template>
+
+				FrankerFaceZ Control Center
+			</NTooltip>
+
 			<NButton size="small" text @click="toggleTheme">
 				<IconSun v-if="chatTheme === 'dark'" color="orange" />
 				<IconMoon v-else />
