a0ab1b0289edc466a9b0a40513d4b65e6c6f7114
kvizyx <kvzxplugg4@gmail.com>
2024-10-16 07:00:17 +0300

    kruto

--- libs/baseapp/gorm-audit-hooks.go
+++ libs/baseapp/gorm-audit-hooks.go
@@ -1,7 +1,6 @@
 package baseapp
 
 import (
-	"context"
 	"log/slog"
 
 	"github.com/google/uuid"
@@ -12,33 +11,33 @@ import (
 	"gorm.io/gorm"
 )
 
-type auditHook func(tx *gorm.DB) model.AuditLog
+type auditHook func(tx *gorm.DB) *model.AuditLog
 
 type gormAuditHooks struct {
 	logger          logger.Logger
 	auditLogsPubSub auditlogs.PubSub
 }
 
-func (c *gormAuditHooks) create(tx *gorm.DB) model.AuditLog {
+func (c *gormAuditHooks) create(tx *gorm.DB) *model.AuditLog {
 	if tx.Statement.Schema != nil && tx.Statement.Schema.Table == "audit_logs" || tx.Error != nil {
-		return model.AuditLog{}
+		return nil
 	}
 
 	recordMap, err := getDataBeforeOperation(tx)
 	if err != nil {
-		return model.AuditLog{}
+		return nil
 	}
 
 	objId := getKeyFromData("id", recordMap)
 	if objId == "" {
-		return model.AuditLog{}
+		return nil
 	}
 
 	ctx := tx.Statement.Context
 	userID := getUserIDFromContext(ctx)
 	dashboardID := getDashboardIDFromContext(ctx)
 
-	audit := model.AuditLog{
+	audit := &model.AuditLog{
 		ID:            uuid.New(),
 		Table:         tx.Statement.Schema.Table,
 		OperationType: model.AuditOperationCreate,
@@ -48,39 +47,39 @@ func (c *gormAuditHooks) create(tx *gorm.DB) model.AuditLog {
 		ChannelID:     null.StringFromPtr(dashboardID),
 	}
 
-	if err := tx.Session(
+	if err = tx.Session(
 		&gorm.Session{
 			SkipHooks: true,
 			NewDB:     true,
 		},
-	).Create(&audit).Error; err != nil {
+	).Create(audit).Error; err != nil {
 		c.logger.Error("error in audit log creation", slog.Any("err", err))
-		return model.AuditLog{}
+		return nil
 	}
 
 	return audit
 }
 
-func (c *gormAuditHooks) delete(tx *gorm.DB) model.AuditLog {
+func (c *gormAuditHooks) delete(tx *gorm.DB) *model.AuditLog {
 	if tx.Statement.Schema != nil && tx.Statement.Schema.Table == "audit_logs" || tx.Error != nil {
-		return model.AuditLog{}
+		return nil
 	}
 
 	recordMap, err := getDataBeforeOperation(tx)
 	if err != nil {
-		return model.AuditLog{}
+		return nil
 	}
 	objId := getKeyFromData("id", recordMap)
 
 	if objId == "" {
-		return model.AuditLog{}
+		return nil
 	}
 
 	ctx := tx.Statement.Context
 	userID := getUserIDFromContext(ctx)
 	dashboardID := getDashboardIDFromContext(ctx)
 
-	audit := model.AuditLog{
+	audit := &model.AuditLog{
 		ID:            uuid.New(),
 		Table:         tx.Statement.Schema.Table,
 		OperationType: model.AuditOperationDelete,
@@ -90,39 +89,39 @@ func (c *gormAuditHooks) delete(tx *gorm.DB) model.AuditLog {
 		ChannelID:     null.StringFromPtr(dashboardID),
 	}
 
-	if err := tx.Session(
+	if err = tx.Session(
 		&gorm.Session{
 			SkipHooks: true,
 			NewDB:     true,
 		},
-	).Create(&audit).Error; err != nil {
+	).Create(audit).Error; err != nil {
 		c.logger.Error("error in audit log creation", slog.Any("err", err))
-		return model.AuditLog{}
+		return nil
 	}
 
 	return audit
 }
 
-func (c *gormAuditHooks) update(tx *gorm.DB) model.AuditLog {
+func (c *gormAuditHooks) update(tx *gorm.DB) *model.AuditLog {
 	if tx.Statement.Schema != nil && tx.Statement.Schema.Table == "audit_logs" || tx.Error != nil {
-		return model.AuditLog{}
+		return nil
 	}
 
 	recordMap, err := getDataBeforeOperation(tx)
 	if err != nil {
-		return model.AuditLog{}
+		return nil
 	}
 
 	objId := getKeyFromData("id", recordMap)
 	if objId == "" {
-		return model.AuditLog{}
+		return nil
 	}
 
 	ctx := tx.Statement.Context
 	userID := getUserIDFromContext(ctx)
 	dashboardID := getDashboardIDFromContext(ctx)
 
-	audit := model.AuditLog{
+	audit := &model.AuditLog{
 		ID:            uuid.New(),
 		Table:         tx.Statement.Schema.Table,
 		OperationType: model.AuditOperationUpdate,
@@ -132,14 +131,14 @@ func (c *gormAuditHooks) update(tx *gorm.DB) model.AuditLog {
 		ChannelID:     null.StringFromPtr(dashboardID),
 	}
 
-	if err := tx.Session(
+	if err = tx.Session(
 		&gorm.Session{
 			SkipHooks: true,
 			NewDB:     true,
 		},
-	).Create(&audit).Error; err != nil {
+	).Create(audit).Error; err != nil {
 		c.logger.Error("error in audit log creation", slog.Any("err", err))
-		return model.AuditLog{}
+		return nil
 	}
 
 	return audit
@@ -148,9 +147,12 @@ func (c *gormAuditHooks) update(tx *gorm.DB) model.AuditLog {
 func (c *gormAuditHooks) withPublisher(hook auditHook) func(tx *gorm.DB) {
 	return func(tx *gorm.DB) {
 		auditLog := hook(tx)
+		if auditLog == nil {
+			return
+		}
 
 		err := c.auditLogsPubSub.Publish(
-			context.Background(), auditlogs.AuditLog{
+			tx.Statement.Context, auditlogs.AuditLog{
 				ID:            auditLog.ID,
 				Table:         auditLog.Table,
 				OperationType: auditlogs.AuditOperationType(auditLog.OperationType),
