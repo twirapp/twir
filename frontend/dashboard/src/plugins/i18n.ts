9250e07a517e6415df7f3e4cc5f5197f82558756
Satont <satontworldwide@gmail.com>
2024-11-26 20:49:59 +0300

    fix(dashboard): back available locales

--- frontend/dashboard/src/plugins/i18n.ts
+++ frontend/dashboard/src/plugins/i18n.ts
@@ -52,6 +52,26 @@ export const AVAILABLE_LOCALES = [
 		code: 'en',
 		name: 'English',
 	},
+	{
+		code: 'ru',
+		name: 'Russian',
+	},
+	{
+		code: 'uk',
+		name: 'Українська',
+	},
+	{
+		code: 'de',
+		name: 'Deutsch',
+	},
+	{
+		code: 'ja',
+		name: '日本語',
+	},
+	{
+		code: 'sk',
+		name: 'Slovenčina',
+	},
 ]
 
 function setupI18n(): I18n {
