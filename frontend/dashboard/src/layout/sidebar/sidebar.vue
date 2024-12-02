7c20aefd598fd3760a029b3108cfba69d9f56a1a
Satont <satontworldwide@gmail.com>
2024-12-02 15:10:51 +0300

    feat(dashboard): add persisting of collapsed state for sidebar

--- frontend/dashboard/src/layout/sidebar/sidebar.vue
+++ frontend/dashboard/src/layout/sidebar/sidebar.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { useLocalStorage } from '@vueuse/core'
+
 import SidebarDashboardSelector from './sidebar-dashboard-selector.vue'
 import SidebarFooter from './sidebar-footer.vue'
 import SidebarHeader from './sidebar-header.vue'
@@ -11,10 +13,12 @@ import {
 	SidebarSeparator,
 } from '@/components/ui/sidebar'
 import SidebarNavigation from '@/layout/sidebar/sidebar-navigation.vue'
+
+const open = useLocalStorage('twirSidebarIsOpen', true)
 </script>
 
 <template>
-	<SidebarProvider>
+	<SidebarProvider v-model:open="open">
 		<Sidebar collapsible="icon">
 			<SidebarHeader />
 
