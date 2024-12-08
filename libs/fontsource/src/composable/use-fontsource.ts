b47c111d466fa361b149f11c28d5f753b8a70adb
Satont <satontworldwide@gmail.com>
2024-12-08 07:58:36 +0300

    fix: correct iterate through fonts in firefox

--- libs/fontsource/src/composable/use-fontsource.ts
+++ libs/fontsource/src/composable/use-fontsource.ts
@@ -1,46 +1,64 @@
-import { ref, onMounted } from 'vue';
+import { onMounted, ref } from 'vue'
 
-import { loadFontList, loadFont as loadFontById, generateFontKey } from '../api.js';
-import type { Font, FontItem } from '../types.js';
+import { generateFontKey, loadFont as loadFontById, loadFontList } from '../api.js'
+
+import type { Font, FontItem } from '../types.js'
+
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1729089
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1780657
+// https://sidneyliebrand.io/blog/fixing-font-face-set-entries-not-iterable-in-firefox
+function firefoxWorkaroundIterFonts(target: FontFaceSet) {
+	const iterable = target.entries()
+	const results = []
+	let iterator = iterable.next()
+	while (iterator.done === false) {
+		results.push(iterator.value[0])
+		iterator = iterable.next()
+	}
+	return results
+}
 
 export function useFontSource(preloadFonts = true) {
-	const loading = ref(true);
-	const fontList = ref<FontItem[]>([]);
-	const fonts = ref<Font[]>([]);
+	const loading = ref(true)
+	const fontList = ref<FontItem[]>([])
+	const fonts = ref<Font[]>([])
 
 	onMounted(async () => {
 		try {
-			if (!preloadFonts) return;
-			fontList.value = await loadFontList();
+			if (!preloadFonts) return
+			fontList.value = await loadFontList()
 		} catch (err) {
-			console.error(err);
+			console.error(err)
 		} finally {
-			loading.value = false;
+			loading.value = false
 		}
-	});
+	})
 
 	async function loadFont(
 		fontId: string,
 		fontWeight: number,
 		fontStyle: string,
 	): Promise<Font | undefined> {
-		const fontKey = generateFontKey(fontId, fontWeight, fontStyle);
-		for (const fontFace of document.fonts.values()) {
-			if (fontFace.family === fontKey) return getFont(fontId);
+		const fontKey = generateFontKey(fontId, fontWeight, fontStyle)
+
+		console.log(firefoxWorkaroundIterFonts(document.fonts))
+
+		for (const fontFace of firefoxWorkaroundIterFonts(document.fonts)) {
+			if (fontFace.family === fontKey) return getFont(fontId)
 		}
 
 		try {
-			const font = await loadFontById(fontId, fontWeight, fontStyle);
-			if (!font) return;
-			fonts.value.push(font);
-			return font;
+			const font = await loadFontById(fontId, fontWeight, fontStyle)
+			if (!font) return
+			fonts.value.push(font)
+			return font
 		} catch (err) {
-			console.error(err);
+			console.error(err)
 		}
 	}
 
 	function getFont(fontId: string): Font | undefined {
-		return fonts.value.find((font) => font.id === fontId);
+		return fonts.value.find((font) => font.id === fontId)
 	}
 
 	return {
@@ -50,5 +68,5 @@ export function useFontSource(preloadFonts = true) {
 		loadFont,
 		getFont,
 		generateFontKey,
-	};
+	}
 }
