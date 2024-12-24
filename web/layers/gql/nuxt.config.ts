16cbaaf555d0a30abcb05dc7e2cabab6047dbc5e
Satont <satontworldwide@gmail.com>
2024-12-24 17:24:22 +0300

    fix(web): correct urql endpoints

--- web/layers/gql/nuxt.config.ts
+++ web/layers/gql/nuxt.config.ts
@@ -14,13 +14,12 @@ export default defineNuxtConfig({
 	modules: ['@bicou/nuxt-urql'],
 
 	urql: {
-		endpoint: `${https ? 'https' : 'http'}://${config.SITE_BASE_URL}/api/query`,
+		endpoint: `/api/query`,
 		client: path.join(currentDir, 'urql.ts'),
 		ssr: {
 			endpoint: process.env.NODE_ENV !== 'production'
 				? `${https ? 'https' : 'http'}://${config.SITE_BASE_URL}/api/query`
 				: 'http://api-gql:3009/query',
-			key: 'urql-ssr'
 		},
 	},
 })
