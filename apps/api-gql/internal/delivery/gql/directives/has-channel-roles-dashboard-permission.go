912d1edba6e6a6b5d4b3f4a8bdfefb8ce26abfeb
Satont <satontworldwide@gmail.com>
2025-01-25 08:13:12 +0300

    fix(api-gql): check on user stats in `HasChannelRolesDashboardPermission`

--- apps/api-gql/internal/delivery/gql/directives/has-channel-roles-dashboard-permission.go
+++ apps/api-gql/internal/delivery/gql/directives/has-channel-roles-dashboard-permission.go
@@ -38,6 +38,34 @@ func (c *Directives) HasChannelRolesDashboardPermission(
 		return nil, fmt.Errorf("cannot get channel roles: %w", err)
 	}
 
+	var userStat model.UsersStats
+	if err := c.gorm.
+		WithContext(ctx).
+		Where(`"userId" = ? AND "channelId" = ?`, user.ID, dashboardId).
+		First(&userStat).
+		Error; err != nil {
+		return nil, fmt.Errorf("cannot get user stats: %w", err)
+	}
+
+	roleToStats := map[model.ChannelRoleEnum]bool{
+		model.ChannelRoleTypeModerator:  userStat.IsMod,
+		model.ChannelRoleTypeVip:        userStat.IsVip,
+		model.ChannelRoleTypeSubscriber: userStat.IsSubscriber,
+	}
+
+	for _, role := range channelRoles {
+		if roleToStats[role.Type] {
+			role.Users = append(
+				role.Users,
+				&model.ChannelRoleUser{
+					ID:     "", // not needed
+					UserID: user.ID,
+					RoleID: role.ID,
+				},
+			)
+		}
+	}
+
 	for _, role := range channelRoles {
 		// we do not check does role.Users contains request author user
 		// because we are doing preload by user id
