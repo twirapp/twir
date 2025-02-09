9809f672150738fff0ae5e2807e510f1c5798289
Satont <satontworldwide@gmail.com>
2025-02-10 00:33:39 +0300

    upd workflow

--- apps/eval/src/index.ts
+++ apps/eval/src/index.ts
@@ -1,32 +1,35 @@
+import vm from 'node:vm'
+
 import { newBus } from '@twir/bus-core'
 import _ from 'lodash'
-import { connect as natsConnect } from 'nats'
-import { VM } from 'vm2'
+import { connect } from 'nats'
 
-const vm = new VM({
-	sandbox: {
-		fetch,
-		URLSearchParams,
-		_,
-	},
-	timeout: 1000,
-	wasm: false,
-	eval: false,
-})
-
-const nc = await natsConnect({
-	servers: Bun.env.NODE_ENV === 'production' ? 'nats://nats:4222' : 'nats://127.0.0.1:4222',
+const nc = await connect({
+	servers: Bun.env.NODE_ENV === 'production'
+		? 'nats://nats:4222'
+		: 'nats://127.0.0.1:4222',
 })
 const bus = newBus(nc)
 
 bus.Eval.Evaluate.subscribeGroup('eval.evaluate', async (request) => {
 	let resultOfExecution
 	try {
-		const toEval = `(async function () { ${request.expression} })()`.split(';\n').join(';')
-		resultOfExecution = await vm.run(toEval)
+		const toEval = `(async function () { ${request.expression} })()`
+			.split(';\n')
+			.join(';')
+
+		const script = new vm.Script(toEval)
+		const context = {
+			fetch,
+			URLSearchParams,
+			_,
+			result: undefined,
+		}
+
+		resultOfExecution = await script.runInContext(vm.createContext(context))
 	} catch (error) {
 		console.error(error)
-		resultOfExecution = (error as any).message ?? 'unexpected error'
+		resultOfExecution = (error as any).message || 'unexpected error'
 	}
 
 	return {
