50151116f56ab6f71f95e1193de170e6833012ba
Satont <satontworldwide@gmail.com>
2025-03-18 20:18:12 +0300

    feat(parser): add support of commands into spam command

--- apps/parser/internal/commands/spam/spam.go
+++ apps/parser/internal/commands/spam/spam.go
@@ -2,6 +2,7 @@ package spam
 
 import (
 	"context"
+	"strings"
 
 	"github.com/guregu/null"
 	"github.com/lib/pq"
@@ -9,19 +10,21 @@ import (
 	command_arguments "github.com/satont/twir/apps/parser/internal/command-arguments"
 	"github.com/satont/twir/apps/parser/internal/types"
 	model "github.com/satont/twir/libs/gomodels"
+	channelscommandsprefixmodel "github.com/twirapp/twir/libs/repositories/channels_commands_prefix/model"
 )
 
 const (
 	spamCountArgName   = "count"
-	spamMessageArgName = "message"
+	spamMessageArgName = "message_or_command"
 )
 
 var Command = &types.DefaultCommand{
 	ChannelsCommands: &model.ChannelsCommands{
-		Name:        "spam",
-		Description: null.StringFrom("Spam into chat. Example usage: <b>!spam 5 Follow me on twitter"),
-		RolesIDS:    pq.StringArray{model.ChannelRoleTypeModerator.String()},
-		Module:      "MODERATION",
+		Name:               "spam",
+		Description:        null.StringFrom("Spam into chat. Example usage: !spam 5 Follow me on twitter or !spam 5 !tg"),
+		RolesIDS:           pq.StringArray{model.ChannelRoleTypeModerator.String()},
+		Module:             "MODERATION",
+		KeepResponsesOrder: false,
 	},
 	Args: []command_arguments.Arg{
 		command_arguments.Int{
@@ -42,8 +45,56 @@ var Command = &types.DefaultCommand{
 		count := parseCtx.ArgsParser.Get(spamCountArgName).Int()
 		text := parseCtx.ArgsParser.Get(spamMessageArgName).String()
 
+		var commandsPrefix string
+		commandPrefixEntity, _ := parseCtx.Services.CommandsPrefixCache.Get(ctx, parseCtx.Channel.ID)
+		if commandPrefixEntity != channelscommandsprefixmodel.Nil {
+			commandsPrefix = commandPrefixEntity.Prefix
+		} else {
+			commandsPrefix = "!"
+		}
+
+		// if not command
+		if !strings.HasPrefix(text, commandsPrefix) {
+			for i := 0; i < count; i++ {
+				result.Result = append(result.Result, text)
+			}
+
+			return result, nil
+		}
+
+		cmds, err := parseCtx.Services.CommandsCache.Get(ctx, parseCtx.Channel.ID)
+		if err != nil {
+			return nil, err
+		}
+
+		var foundCmd *model.ChannelsCommands
+
+		for _, cmd := range cmds {
+			if cmd.Name == strings.TrimPrefix(text, commandsPrefix) {
+				foundCmd = &cmd
+				break
+			}
+
+			for _, alias := range cmd.Aliases {
+				if alias == strings.TrimPrefix(text, commandsPrefix) {
+					foundCmd = &cmd
+					break
+				}
+			}
+		}
+
+		if foundCmd == nil {
+			return nil, nil
+		}
+
 		for i := 0; i < count; i++ {
-			result.Result = append(result.Result, text)
+			for _, r := range foundCmd.Responses {
+				if !r.Text.Valid {
+					continue
+				}
+
+				result.Result = append(result.Result, r.Text.String)
+			}
 		}
 
 		return result, nil
