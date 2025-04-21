08dd834f2561169ef7516ebc534398b67892ff4d
Satont <satontworldwide@gmail.com>
2025-04-21 22:45:52 +0300

    chore(integrations): add otel for 7tv

--- libs/integrations/seventv/client_transport.go
+++ libs/integrations/seventv/client_transport.go
@@ -2,6 +2,9 @@ package seventv
 
 import (
 	"net/http"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type headerTransport struct {
@@ -9,8 +12,28 @@ type headerTransport struct {
 	token string
 }
 
-func (t *headerTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	req.Header.Set("Authorization", "Bearer "+t.token)
+func (t *headerTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	span := trace.SpanFromContext(r.Context())
+	defer span.End()
+	r.Header.Set("Authorization", "Bearer "+t.token)
 
-	return t.base.RoundTrip(req)
+	resp, err := t.base.RoundTrip(r)
+	if err != nil {
+		return nil, err
+	}
+
+	span.SetAttributes(
+		attribute.String(
+			"7tv.x-ratelimit-global-limit",
+			resp.Header.Get("X-RateLimit-Global-Limit"),
+		),
+		attribute.String(
+			"7tv.x-ratelimit-global-remaining",
+			resp.Header.Get("X-RateLimit-Global-Remaining"),
+		),
+		attribute.String("7tv.x-ratelimit-global-reset", resp.Header.Get("X-RateLimit-Global-Reset")),
+		attribute.String("7tv.x-ratelimit-global-used", resp.Header.Get("X-RateLimit-Global-Used")),
+	)
+
+	return resp, nil
 }
