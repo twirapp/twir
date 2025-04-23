0e9b010fd2f64a2fd16443b7f314feb8a527411c
Satont <satontworldwide@gmail.com>
2025-04-24 02:12:27 +0300

    feat(dashboard): chat messages virtual scroll

--- frontend/dashboard/src/features/community-chat-messages/ui/chat-messages.vue
+++ frontend/dashboard/src/features/community-chat-messages/ui/chat-messages.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue'
+import { useVirtualizer } from '@tanstack/vue-virtual'
+import { computed, onMounted, ref, useTemplateRef, watch } from 'vue'
 
 import ChatMessage from './message.vue'
 import { useChatMessagesFilters } from '../composables/use-filters'
@@ -18,35 +19,98 @@ const { data, executeQuery } = useChatMessages(filters.computedFilters)
 const messages = ref<ChatMessageType[]>([])
 
 watch(data, (v) => {
-	messages.value = v?.chatMessages ?? []
+	messages.value = v?.chatMessages?.reverse() ?? []
 }, {
 	immediate: true,
 })
 
 const subscription = useChatMessagesSubscription()
-
 watch(subscription.data, (v) => {
 	if (v?.chatMessages) {
-		messages.value.unshift(v.chatMessages)
+		messages.value.push(v.chatMessages)
 	}
 })
 
 onMounted(() => {
 	executeQuery({ requestPolicy: 'cache-and-network' })
 })
+
+const messagesCount = computed(() => {
+	return messages.value.length ?? 0
+})
+
+const scrollRef = useTemplateRef<HTMLElement | null>('scrollRef')
+
+const rowVirtualizer = useVirtualizer({
+	get count() {
+		return messagesCount.value
+	},
+	getScrollElement: () => scrollRef.value,
+	estimateSize: () => 20,
+})
+
+const virtualRows = computed(() => rowVirtualizer.value.getVirtualItems())
+const totalSize = computed(() => rowVirtualizer.value.getTotalSize())
+
+function measureElement(el: Element) {
+	if (!el) {
+		return
+	}
+
+	rowVirtualizer.value.measureElement(el)
+
+	return undefined
+}
+
+watch(messages, (v) => {
+	rowVirtualizer.value.scrollToIndex(v.length - 1)
+}, {
+	immediate: true,
+})
 </script>
 
 <template>
 	<Card>
-		<CardContent class="p-2 flex flex-col gap-2 overflow-y-auto">
-			<div v-if="data?.chatMessages.length === 0">
-				No data
+		<CardContent class="p-0">
+			<div
+				ref="scrollRef"
+				class="overflow-auto h-[75dvh] w-full"
+				style="contain: strict"
+			>
+				<div
+					:style="{
+						height: `${totalSize}px`,
+						width: '100%',
+						position: 'relative',
+					}"
+				>
+					<div
+						:style="{
+							position: 'absolute',
+							top: 0,
+							left: 0,
+							width: '100%',
+							transform: `translateY(${virtualRows[0]?.start ?? 0}px)`,
+						}"
+						class="flex flex-col gap-2"
+					>
+						<div
+							v-for="virtualRow in virtualRows"
+							:key="virtualRow.key as PropertyKey"
+							:ref="measureElement"
+							:data-index="virtualRow.index"
+
+							:class="{
+								'bg-zinc-800/40': virtualRow.index % 2,
+							}"
+						>
+							<ChatMessage
+								:message="messages[virtualRow.index]"
+							/>
+						</div>
+					</div>
+				</div>
 			</div>
-			<ChatMessage
-				v-for="message of data?.chatMessages"
-				:key="message.id"
-				:message="message"
-			/>
 		</CardContent>
 	</Card>
 </template>
