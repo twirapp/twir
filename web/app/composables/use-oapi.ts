bb992baf248c2d40a22a13952a0cfe08cca7daad
Satont <satontworldwide@gmail.com>
2025-05-03 12:23:24 +0300

    fix(web): correct `useRequestHeaders` in hastebin raw №2

--- web/app/composables/use-oapi.ts
+++ web/app/composables/use-oapi.ts
@@ -2,14 +2,12 @@ import process from 'node:process'
 
 import { Api, HttpClient } from '@twir/api/openapi'
 
-import { useRequestHeaders } from '#imports'
-
 export function useOapi() {
 	const apiUrl = import.meta.server
 		? process.env.NODE_ENV === 'production' ? 'http://api-gql:3009' : 'http://localhost:3009'
 		: `${window.location.origin}/api`
 
-	const headers = useRequestHeaders(['cookie', 'session'])
+	const headers = import.meta.server ? useRequestHeaders(['cookie', 'session']) : {}
 
 	return new Api(new HttpClient({
 		baseUrl: apiUrl,
