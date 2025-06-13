e276c559c3b72118e5486f55e69c5b6f274e50db
Satont <satontworldwide@gmail.com>
2025-06-13 07:54:03 +0300

    feat(api-gql): cache channel files for 10 minutes to prevent requests to s3

--- apps/api-gql/internal/services/channels_files/channels_files.go
+++ apps/api-gql/internal/services/channels_files/channels_files.go
@@ -1,16 +1,19 @@
 package channels_files
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io"
 	"strings"
+	"time"
 	"unicode/utf8"
 
 	"github.com/avito-tech/go-transaction-manager/trm/v2"
 	"github.com/google/uuid"
 	"github.com/minio/minio-go/v7"
 	config "github.com/satont/twir/libs/config"
+	"github.com/satont/twir/libs/logger"
 	"github.com/twirapp/twir/apps/api-gql/internal/entity"
 	"github.com/twirapp/twir/libs/repositories/channels_files"
 	"github.com/twirapp/twir/libs/repositories/channels_files/model"
@@ -19,20 +22,63 @@ import (
 
 type Opts struct {
 	fx.In
+	LC fx.Lifecycle
 
 	FilesRepo  channels_files.Repository
 	TrmManager trm.Manager
 	S3Client   *minio.Client
 	Config     config.Config
+	Logger     logger.Logger
+}
+
+type cachedFile struct {
+	content []byte
+	addedAt time.Time
 }
 
 func New(opts Opts) *Service {
-	return &Service{
-		config:     opts.Config,
-		filesRepo:  opts.FilesRepo,
-		trmManager: opts.TrmManager,
-		s3Client:   opts.S3Client,
+	s := &Service{
+		config:            opts.Config,
+		filesRepo:         opts.FilesRepo,
+		trmManager:        opts.TrmManager,
+		s3Client:          opts.S3Client,
+		filesContentCache: make(map[string]*cachedFile),
 	}
+
+	cacheTime := 10 * time.Minute
+	contentClearTicker := time.NewTicker(cacheTime)
+	contentClearTickerDone := make(chan struct{})
+
+	opts.LC.Append(
+		fx.Hook{
+			OnStart: func(_ context.Context) error {
+				go func() {
+					for {
+						select {
+						case <-contentClearTickerDone:
+							contentClearTicker.Stop()
+							return
+						case <-contentClearTicker.C:
+							for k, v := range s.filesContentCache {
+								if v.addedAt.Add(cacheTime).Before(time.Now()) {
+									delete(s.filesContentCache, k)
+								}
+							}
+						}
+					}
+				}()
+
+				return nil
+			},
+			OnStop: func(ctx context.Context) error {
+				contentClearTickerDone <- struct{}{}
+				close(contentClearTickerDone)
+				return nil
+			},
+		},
+	)
+
+	return s
 }
 
 type Service struct {
@@ -40,6 +86,8 @@ type Service struct {
 	filesRepo  channels_files.Repository
 	trmManager trm.Manager
 	s3Client   *minio.Client
+
+	filesContentCache map[string]*cachedFile
 }
 
 // 10MB
@@ -209,6 +257,12 @@ func (c *Service) GetFileContent(ctx context.Context, channelID string, fileID u
 	io.Reader,
 	error,
 ) {
+	if f, ok := c.filesContentCache[fileID.String()]; ok {
+		c.filesContentCache[fileID.String()].addedAt = time.Now()
+
+		return bytes.NewReader(f.content), nil
+	}
+
 	object, err := c.s3Client.GetObject(
 		ctx,
 		c.config.S3Bucket,
@@ -219,5 +273,15 @@ func (c *Service) GetFileContent(ctx context.Context, channelID string, fileID u
 		return nil, fmt.Errorf("cannot get file: %w", err)
 	}
 
-	return object, nil
+	content, err := io.ReadAll(object)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read file: %w", err)
+	}
+
+	c.filesContentCache[fileID.String()] = &cachedFile{
+		content: content,
+		addedAt: time.Now(),
+	}
+
+	return bytes.NewReader(content), nil
 }
