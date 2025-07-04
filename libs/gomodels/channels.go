03ea0953ea8735d9fdff4fc778786b58d2b64046
Satont <satontworldwide@gmail.com>
2025-07-04 22:55:46 +0300

    feat: add users and channels created_at columns

--- libs/gomodels/channels.go
+++ libs/gomodels/channels.go
@@ -16,11 +16,12 @@ var (
 )
 
 type Channels struct {
-	ID             string `gorm:"primaryKey;column:id;type:TEXT;"               json:"id"`
-	IsEnabled      bool   `gorm:"column:isEnabled;type:BOOL;"       json:"isEnabled"`
-	IsTwitchBanned bool   `gorm:"column:isTwitchBanned;type:BOOL;" json:"isTwitchBanned"`
-	IsBotMod       bool   `gorm:"column:isBotMod;type:BOOL;" json:"isBotMod"`
-	BotID          string `gorm:"column:botId;type:TEXT;"                        json:"botId"`
+	ID             string    `gorm:"primaryKey;column:id;type:TEXT;"               json:"id"`
+	IsEnabled      bool      `gorm:"column:isEnabled;type:BOOL;"       json:"isEnabled"`
+	IsTwitchBanned bool      `gorm:"column:isTwitchBanned;type:BOOL;" json:"isTwitchBanned"`
+	IsBotMod       bool      `gorm:"column:isBotMod;type:BOOL;" json:"isBotMod"`
+	BotID          string    `gorm:"column:botId;type:TEXT;"                        json:"botId"`
+	CreatedAt      time.Time `gorm:"column:created_at;type:TIMESTAMPTZ;default:now()" json:"createdAt"`
 
 	Commands []ChannelsCommands `gorm:"foreignKey:ChannelID" json:"commands"`
 	Roles    []*ChannelRole     `gorm:"foreignKey:ChannelID" json:"roles"`
