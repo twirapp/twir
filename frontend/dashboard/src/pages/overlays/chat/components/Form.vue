22571772e70ecd42aa72490798597b7225f1b18f
Satont <satontworldwide@gmail.com>
2025-07-21 18:37:56 +0300

    fix(dashboard): add ability to copy chat overlay link

--- frontend/dashboard/src/pages/overlays/chat/components/Form.vue
+++ frontend/dashboard/src/pages/overlays/chat/components/Form.vue
@@ -17,11 +17,7 @@ import { useI18n } from 'vue-i18n'
 import { defaultChatSettings } from './default-settings'
 import { useChatOverlayForm } from './form.js'
 
-import {
-	useChatOverlayApi,
-	useProfile,
-	useUserAccessFlagChecker,
-} from '@/api'
+import { useChatOverlayApi, useProfile, useUserAccessFlagChecker } from '@/api'
 import { useCopyOverlayLink } from '@/components/overlays/copyOverlayLink.js'
 import { useNaiveDiscrete } from '@/composables/use-naive-discrete'
 import { ChannelRolePermissionEnum } from '@/gql/graphql'
@@ -35,10 +31,6 @@ const userCanEditOverlays = useUserAccessFlagChecker(ChannelRolePermissionEnum.M
 const { data: profile } = useProfile()
 const { data: formValue, reset } = useChatOverlayForm()
 
-const canCopyLink = computed(() => {
-	return profile?.value?.selectedDashboardId === profile.value?.id && userCanEditOverlays
-})
-
 const styleSelectOptions = [
 	{ label: 'Clean', value: 'clean' },
 	{ label: 'Boxed', value: 'boxed' },
@@ -51,10 +43,14 @@ const directionOptions = computed(() => {
 })
 
 const fontData = ref<Font | null>(null)
-watch(() => fontData.value, (font) => {
-	if (!font) return
-	formValue.value.fontFamily = font.id
-}, { immediate: true })
+watch(
+	() => fontData.value,
+	(font) => {
+		if (!font) return
+		formValue.value.fontFamily = font.id
+	},
+	{ immediate: true }
+)
 
 const fontWeightOptions = computed(() => {
 	if (!fontData.value) return []
@@ -96,17 +92,13 @@ async function save() {
 <template>
 	<div v-if="formValue" class="card">
 		<div class="flex flex-wrap justify-start gap-2">
-			<NButton
-				secondary
-				type="error"
-				@click="reset"
-			>
+			<NButton secondary type="error" @click="reset">
 				{{ t('sharedButtons.setDefaultSettings') }}
 			</NButton>
 			<NButton
 				secondary
 				type="info"
-				:disabled="!formValue.id || !canCopyLink"
+				:disabled="!formValue.id || !userCanEditOverlays"
 				@click="copyOverlayLink({ id: formValue.id! })"
 			>
 				{{ t('overlays.copyOverlayLink') }}
@@ -148,16 +140,17 @@ async function save() {
 
 				<div v-if="formValue.preset === 'boxed'" class="switch">
 					<span>{{ t('overlays.chat.showAnnounceBadge') }}</span>
-					<NSwitch
-						v-model:value="formValue.showAnnounceBadge"
-						:disabled="!formValue.showBadges"
-					/>
+					<NSwitch v-model:value="formValue.showAnnounceBadge" :disabled="!formValue.showBadges" />
 				</div>
 
 				<div class="slider">
-					<span>{{ t('overlays.chat.paddingContainer') }} ({{ formValue.paddingContainer }}px)</span>
+					<span
+						>{{ t('overlays.chat.paddingContainer') }} ({{ formValue.paddingContainer }}px)</span
+					>
 					<NSlider
-						v-model:value="formValue.paddingContainer" :min="0" :max="256"
+						v-model:value="formValue.paddingContainer"
+						:min="0"
+						:max="256"
 						:marks="{ 0: '0', 256: '256' }"
 					/>
 				</div>
@@ -186,24 +179,20 @@ async function save() {
 
 				<div>
 					<span>{{ t('overlays.chat.fontWeight') }}</span>
-					<NSelect
-						v-model:value="formValue.fontWeight"
-						:options="fontWeightOptions"
-					/>
+					<NSelect v-model:value="formValue.fontWeight" :options="fontWeightOptions" />
 				</div>
 
 				<div>
 					<span>{{ t('overlays.chat.fontStyle') }}</span>
-					<NSelect
-						v-model:value="formValue.fontStyle"
-						:options="fontStyleOptions"
-					/>
+					<NSelect v-model:value="formValue.fontStyle" :options="fontStyleOptions" />
 				</div>
 
 				<div class="slider">
 					<span>{{ t('overlays.chat.fontSize') }} ({{ formValue.fontSize }}px)</span>
 					<NSlider
-						v-model:value="formValue.fontSize" :min="12" :max="80"
+						v-model:value="formValue.fontSize"
+						:min="12"
+						:max="80"
 						:marks="{ 12: '12', 80: '80' }"
 					/>
 				</div>
@@ -212,7 +201,9 @@ async function save() {
 					<div class="flex justify-between mb-1">
 						<span>{{ t('overlays.chat.backgroundColor') }}</span>
 						<NButton
-							size="tiny" secondary type="success"
+							size="tiny"
+							secondary
+							type="success"
 							@click="formValue.chatBackgroundColor = defaultChatSettings.chatBackgroundColor"
 						>
 							<IconReload class="h-4 w-4" />
@@ -227,10 +218,7 @@ async function save() {
 
 				<div class="slider">
 					<span>{{ t('overlays.chat.textShadow') }}({{ formValue.textShadowSize }}px)</span>
-					<NColorPicker
-						v-model:value="formValue.textShadowColor"
-						default-value="rgba(0,0,0,1)"
-					/>
+					<NColorPicker v-model:value="formValue.textShadowColor" default-value="rgba(0,0,0,1)" />
 					<NSlider v-model:value="formValue.textShadowSize" :min="0" :max="30" />
 				</div>
 
@@ -238,18 +226,12 @@ async function save() {
 
 				<div class="slider">
 					<span>{{ t('overlays.chat.hideTimeout') }}({{ formValue.messageHideTimeout }}s)</span>
-					<NSlider
-						v-model:value="formValue.messageHideTimeout" :max="60"
-						:marks="sliderMarks"
-					/>
+					<NSlider v-model:value="formValue.messageHideTimeout" :max="60" :marks="sliderMarks" />
 				</div>
 
 				<div class="slider">
 					<span>{{ t('overlays.chat.showDelay') }}({{ formValue.messageShowDelay }}s)</span>
-					<NSlider
-						v-model:value="formValue.messageShowDelay" :max="60"
-						:marks="sliderMarks"
-					/>
+					<NSlider v-model:value="formValue.messageShowDelay" :max="60" :marks="sliderMarks" />
 				</div>
 			</div>
 		</div>
