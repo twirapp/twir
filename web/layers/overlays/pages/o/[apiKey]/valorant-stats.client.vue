e6e98f844f757eb14b48b842ab70191dd84f7418
Misha Gusev <majikx.prod0@gmail.com>
2025-08-29 15:03:29 +0500

    chore(overlays): add inter font in overlay page

--- web/layers/overlays/pages/o/[apiKey]/valorant-stats.client.vue
+++ web/layers/overlays/pages/o/[apiKey]/valorant-stats.client.vue
@@ -26,6 +26,24 @@ const settings = computed<Settings>(() => {
 })
 
 const apiKey = route.params.apiKey as string
+
+useHead({
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com'
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com',
+      crossorigin: ''
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap'
+    }
+  ]
+})
 </script>
 
 <template>
