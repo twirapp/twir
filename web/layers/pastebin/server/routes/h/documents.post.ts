beb3837e61538f18a1b8ca29fd13d3978cdd071b
Satont <satontworldwide@gmail.com>
2025-09-10 03:03:19 +0300

    feat(web): add support of post to /h/

---
+++ web/layers/pastebin/server/routes/h/documents.post.ts
@@ -0,0 +1,31 @@
+import { useOapi } from '~/composables/use-oapi'
+
+export default defineEventHandler(async (event) => {
+	if (event.node.req.method !== 'POST') {
+		throw createError({
+			statusCode: 405,
+			statusMessage: 'Method not allowed',
+		})
+	}
+	const url = getRequestURL(event)
+
+	const api = useOapi({ headers: event.node.req.headers })
+	const req = await api.v1.pastebinCreate({
+		content: await readBody(event),
+	})
+	if (req.error) {
+		throw createError({
+			statusCode: req.status || 500,
+			statusMessage: req.error,
+		})
+	}
+
+	if (!req.data?.id) {
+		throw createError({
+			statusCode: 500,
+			statusMessage: 'No ID returned from api, contact administrator',
+		})
+	}
+
+	return sendRedirect(event, `${url.origin}/h/${req.data.id}`)
+})
