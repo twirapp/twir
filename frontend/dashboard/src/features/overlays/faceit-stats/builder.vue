dbf34c27003363db4c04f0ae88700346b2836139
Misha Gusev <majikx.prod0@gmail.com>
2025-09-13 07:01:59 +0500

    feat(dashboard): add translation for faceit stats builder

--- frontend/dashboard/src/features/overlays/faceit-stats/builder.vue
+++ frontend/dashboard/src/features/overlays/faceit-stats/builder.vue
@@ -2,6 +2,7 @@
 import { FaceitStatsWidget } from '@twir/frontend-faceit-stats'
 import { Label } from 'radix-vue'
 
+import { useI18n } from 'vue-i18n'
 import { useFaceitStats } from './composables/use-faceit-stats'
 import DisplaySwitcher from './ui/display-switcher.vue'
 
@@ -11,71 +12,96 @@ import Separator from '@/components/ui/separator/Separator.vue'
 import PageLayout from '@/layout/page-layout.vue'
 
 const { settings, copyOverlayLink } = useFaceitStats()
+
+const { t } = useI18n()
 </script>
 
 <template>
 	<PageLayout cleanBody>
 		<template #title>
-			Faceit Stats Builder
+			{{ t('overlays.faceit.title') }}
 		</template>
 
 		<template #content>
 			<div class="flex flex-col-reverse lg:flex-row">
 				<div class="overflow-auto bg-card lg:max-w-[400px] w-full flex flex-col p-4 shadow-md">
 					<div class="flex flex-col gap-2">
-						<span class="text-xs mb-2">General</span>
+						<span class="text-xs mb-2">{{ t('overlays.faceit.settings.general.title') }}</span>
 
 						<div class="flex flex-col gap-2">
-							<Label for="nickname">Faceit nickname</Label>
+							<Label for="nickname">
+								{{ t('overlays.faceit.settings.general.faceitNickname') }}
+							</Label>
 							<Input id="nickname" v-model:modelValue="settings.nickname" placeholder="s1mple" />
 						</div>
 
 						<div class="flex flex-col gap-2">
-							<Label for="game">Game</Label>
+							<Label for="game">{{ t('overlays.faceit.settings.general.game') }}</Label>
 							<Input id="game" v-model:modelValue="settings.game" placeholder="cs2" disabled />
 						</div>
 
 						<Separator class="my-2" />
 
-						<span class="text-xs mb-2">Appearance</span>
+						<span class="text-xs mb-2">{{ t('overlays.faceit.settings.appearance.title') }}</span>
 
 						<div class="flex flex-col gap-2">
-							<Label for="backgroundColor">Background color</Label>
+							<Label for="backgroundColor">
+								{{ t('overlays.faceit.settings.appearance.background') }}
+							</Label>
 							<Input id="backgroundColor" v-model:modelValue="settings.bgColor" type="color" />
 						</div>
 
 						<div class="flex flex-col gap-2">
-							<Label for="textColor">Text color</Label>
+							<Label for="textColor">
+								{{ t('overlays.faceit.settings.appearance.textColor') }}
+							</Label>
 							<Input id="textColor" v-model:modelValue="settings.textColor" type="color" />
 						</div>
 
 						<div class="flex flex-col gap-2">
-							<Label for="borderRadius">Border radius</Label>
+							<Label for="borderRadius">
+								{{ t('overlays.faceit.settings.appearance.borderRadius') }}
+							</Label>
 							<Input id="borderRadius" v-model:modelValue="settings.borderRadius" type="number" />
 						</div>
 
 						<Separator class="my-2" />
 
-						<span class="text-xs mb-2">Display</span>
+						<span class="text-xs mb-2">{{ t('overlays.faceit.settings.display.title') }}</span>
 
 						<div class="flex flex-col gap-2">
-							<Label for="showAvarageKdr">Show avarage kdr</Label>
-							<DisplaySwitcher id="showAvarageKdr" v-model:modelValue="settings.displayAvarageKdr" />
+							<Label for="showAvarageKdr">
+								{{ t('overlays.faceit.settings.display.showAvarageKdr') }}
+							</Label>
+							<DisplaySwitcher
+								id="showAvarageKdr"
+								v-model:modelValue="settings.displayAvarageKdr"
+							/>
 						</div>
 
 						<div class="flex flex-col gap-2">
-							<Label for="showWorldRanking">World ranking</Label>
-							<DisplaySwitcher id="showWorldRanking" v-model:modelValue="settings.displayWorldRanking" />
+							<Label for="showWorldRanking">
+								{{ t('overlays.faceit.settings.display.worldRanking') }}
+							</Label>
+							<DisplaySwitcher
+								id="showWorldRanking"
+								v-model:modelValue="settings.displayWorldRanking"
+							/>
 						</div>
 
 						<div class="flex flex-col gap-2">
-							<Label for="lastTwentyatches">Last 20 matches stats</Label>
-							<DisplaySwitcher id="lastTwentyatches" v-model:modelValue="settings.displayLastTwentyMatches" />
+							<Label for="last20MatchesStats">
+								{{ t('overlays.faceit.settings.display.last20MatchesStats') }}
+							</Label>
+							<DisplaySwitcher
+								id="lastTwentyatches"
+								v-model:modelValue="settings.displayLastTwentyMatches"
+							/>
 						</div>
 					</div>
 
 					<Button class="mt-4" @click="copyOverlayLink">
-						Generate obs link
+						{{ t('overlays.faceit.settings.generateObsLink') }}
 					</Button>
 				</div>
 
