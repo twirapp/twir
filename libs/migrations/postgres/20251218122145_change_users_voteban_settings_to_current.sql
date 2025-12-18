c5943c32fccf424c0f107f05c18ce14e625fd982
Kirill Sudakov <kvzxeverywhere@gmail.com>
2025-12-18 16:35:03 +0300

    refactor: simplify change voteban settings migration

--- libs/migrations/postgres/20251218122145_change_users_voteban_settings_to_current.sql
+++ libs/migrations/postgres/20251218122145_change_users_voteban_settings_to_current.sql
@@ -2,17 +2,8 @@
 -- +goose StatementBegin
 SELECT 'up SQL query';
 
-UPDATE channels_games_voteban
-SET vote_duration = CASE
-											WHEN vote_duration = 1 THEN 10
-											ELSE vote_duration
-										END,
-needed_votes  = CASE
- 							  	WHEN needed_votes = 1 THEN 2
-									ELSE needed_votes
-								END
-WHERE vote_duration = 1
-	 OR needed_votes = 1;
+UPDATE channels_games_voteban SET vote_duration = 10 WHERE vote_duration < 10;
+UPDATE channels_games_voteban SET needed_votes = 2 WHERE needed_votes < 2;
 -- +goose StatementEnd
 
 -- +goose Down
