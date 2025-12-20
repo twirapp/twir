2b158cd4a9a21e1ac3ddc46e4a7ba8c5773845c4
Satont <satontworldwide@gmail.com>
2025-12-20 10:57:21 +0300

    fix(dashboard): correct events separator and paddings

--- frontend/dashboard/src/components/dashboard/events/base.vue
+++ frontend/dashboard/src/components/dashboard/events/base.vue
@@ -28,26 +28,19 @@ const date = computed(() => new Date(Number(props.createdAt)))
 </script>
 
 <template>
-	<div class="flex min-h-[50px] gap-2.5 px-2.5 select-text border-b-(--n-border-color) border-b border-solid">
+	<div
+		class="flex min-h-12.5 gap-2.5 px-2.5 select-text border-b-border border-b border-solid py-2"
+	>
 		<div class="flex justify-between items-center w-full">
 			<div class="flex gap-2.5 items-center">
-				<component
-					:is="icon"
-					class="flex items-center min-h-9 min-w-9"
-					:style="{ color }"
-				/>
+				<component :is="icon" class="flex items-center min-h-9 min-w-9" :style="{ color }" />
 				<div class="flex flex-col">
 					<slot name="leftContent" />
 				</div>
 			</div>
 
 			<div class="flex items-end text-xs h-full py-2 shrink-0">
-				<UseTimeAgo
-					v-slot="{ timeAgo }"
-					:time="date"
-					:update-interval="1000"
-					show-second
-				>
+				<UseTimeAgo v-slot="{ timeAgo }" :time="date" :update-interval="1000" show-second>
 					<span class="tooltip" :data-utc="date.toLocaleString(locale)">
 						{{ timeAgo }}
 					</span>
@@ -59,24 +52,24 @@ const date = computed(() => new Date(Number(props.createdAt)))
 
 <style scoped>
 .tooltip {
-  position: relative;
+	position: relative;
 }
 
 .tooltip:after {
-  content: attr(data-utc);
-  position: absolute;
-  top: 0px;
-  right: 0;
-  left: -100px;
-  display: none;
-  text-align: center;
-  background-color: #000;
-  border-radius: 4px;
-  padding: 2px;
+	content: attr(data-utc);
+	position: absolute;
+	top: 0px;
+	right: 0;
+	left: -100px;
+	display: none;
+	text-align: center;
+	background-color: #000;
+	border-radius: 4px;
+	padding: 2px;
 	white-space: nowrap;
 }
 
 .tooltip:hover:after {
-  display: block;
+	display: block;
 }
 </style>
