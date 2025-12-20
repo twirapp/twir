b23596e000b052391252d8ac260d5894487dc52b
Satont <satontworldwide@gmail.com>
2025-12-20 11:21:57 +0300

    feat: upgrade to vite 8.0.0 beta

--- web/app/composables/use-oapi.ts
+++ web/app/composables/use-oapi.ts
@@ -1,10 +1,8 @@
-import process from 'node:process'
-
 import { Api, HttpClient } from '@twir/api/openapi'
 
 export function useOapi(opts?: { headers?: Record<string, any> }) {
 	const apiUrl = import.meta.server
-		? process.env.NODE_ENV === 'production'
+		? import.meta.env.NODE_ENV === 'production'
 			? 'http://api-gql:3009'
 			: 'http://localhost:3009'
 		: `${window.location.origin}/api`
