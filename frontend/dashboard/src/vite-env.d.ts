fcdddeffe442eb5db21b53e5073b1d257fb08961
Satont <satontworldwide@gmail.com>
2026-01-13 10:06:34 +0300

    chore(dashboard): add vut stab for bun and vue-tsc
    [skip ci]

--- frontend/dashboard/src/vite-env.d.ts
+++ frontend/dashboard/src/vite-env.d.ts
@@ -1,3 +1,8 @@
 /// <reference types="vite/client" />
 /// <reference types="@twirapp/vite-plugin-svg-spritemap/client" />
-declare module '*.vue';
+
+declare module '*.vue' {
+	import type { DefineComponent } from 'vue'
+	const component: DefineComponent<object, object, unknown>
+	export default component
+}
