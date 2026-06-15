194e3d5015661bd3db138532b8b5f9e24a79e055
Satont <satontworldwide@gmail.com>
2026-06-15 04:24:00 +0300

    fix(web): exclude from ssr for languages

--- web/layers/dashboard/nuxt.config.ts
+++ web/layers/dashboard/nuxt.config.ts
@@ -2,5 +2,7 @@ export default defineNuxtConfig({
 	routeRules: {
 		'/dashboard': { ssr: false },
 		'/dashboard/**': { ssr: false },
+		'/**/dashboard': { ssr: false },
+		'/**/dashboard/**': { ssr: false },
 	},
 })
