d5fbed413c0f955c729f6b1863d06ca5b7eb4270
Satont <satontworldwide@gmail.com>
2026-07-20 19:19:55 +0300

    fix(migrations): correct hide_on_pause

--- libs/migrations/postgres/20260706041740_channel_api_key_and_hide_on_paus.sql
+++ libs/migrations/postgres/20260706041740_channel_api_key_and_hide_on_paus.sql
@@ -1,8 +1,8 @@
 -- +goose Up
 -- +goose StatementBegin
 SELECT 'up SQL query';
-ALTER TABLE channels ADD COLUMN api_key TEXT DEFAULT uuidv7();
-CREATE UNIQUE INDEX channels_api_key_idx ON channels(api_key) WHERE api_key IS NOT NULL;
+ALTER TABLE channels ADD COLUMN IF NOT EXISTS  api_key TEXT DEFAULT uuidv7();
+CREATE UNIQUE INDEX IF NOT EXISTS channels_api_key_idx ON channels(api_key) WHERE api_key IS NOT NULL;
 
 -- Hide on pause setting for song requests
 ALTER TABLE channels_song_requests_settings ADD COLUMN hide_on_pause BOOL DEFAULT true;
